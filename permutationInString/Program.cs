ff5bca78982337212e6838f2da72fc14636600bf
Ning <xiaoningning@yahoo.com>
2017-08-09 23:49:20 +0000

    permutation pattern in string

---
+++ permutationInString/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace permutationInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string p = args[0];
+            string s = args[1];
+            Console.WriteLine("input pattern: " + p);
+            Console.WriteLine("search string: " + s);
+            Console.WriteLine("find pattern: " + permutationInString(p, s));
+        }
+
+        static bool permutationInString(string p, string s)
+        {
+            int[] map = new int[256];
+            int count = p.Length;
+            for (int i = 0; i < p.Length; i++)
+            {
+                map[p[i]]++;
+            }
+
+            int j = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (map[s[i]]-- > 0) count--;
+                while (count == 0)
+                {
+                    if (i - j + 1 == p.Length) return true;
+                    if (++map[s[j++]] > 0) count++;
+                }
+            }
+            return false;
+        }
+    }
+}
