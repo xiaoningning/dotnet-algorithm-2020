a9a9164c2add0b16148b8c122f7980b1a3306c5c
Ning <xiaoningning@yahoo.com>
2017-08-09 23:06:56 +0000

    longest substring without any repeat

---
+++ longestStringNoRepeat/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace longestStringNoRepeat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input string: " + args[0]);
+            int r = lengthOfLongestSubstring(args[0]);
+            Console.WriteLine("result: " + r);
+        }
+
+        static int lengthOfLongestSubstring(string s){
+            HashSet<char> map = new HashSet<char>();
+            int result = 0;
+            int i = 0, j = 0;
+            while(i < s.Length){
+                if(!map.Contains(s[i])){
+                    map.Add(s[i]);
+                    result = Math.Max(result, map.Count);
+                    i++;
+                }
+                else{
+                    map.Remove(s[j]);
+                    j++;
+                }
+            }
+            
+            return map.Count;
+        }
+    }
+}
