e9844c428d16c328eff3472040fece71ae727792
Ning@VM <xiaoningning@yahoo.com>
2017-10-25 16:42:01 -0700

    longest string no repeat passes OJ

--- longestStringNoRepeat/Program.cs
+++ longestStringNoRepeat/Program.cs
@@ -10,6 +10,8 @@ namespace longestStringNoRepeat
             Console.WriteLine("Input string: " + args[0]);
             int r = lengthOfLongestSubstring(args[0]);
             Console.WriteLine("result: " + r);
+            r = lengthOfLongestSubstring1(args[0]);
+            Console.WriteLine("result1: " + r);
         }
 
         static int lengthOfLongestSubstring(string s){
@@ -26,6 +28,24 @@ namespace longestStringNoRepeat
                     map.Remove(s[j]);
                     j++;
                 }
+            }
+            
+            return result;
+        }
+
+        static int lengthOfLongestSubstring1(string s){
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            int result = 0;
+            
+            for (int i = 0, j = 0; i < s.Length; i++){
+                if(map.ContainsKey(s[i]))
+                {
+                    j = Math.Max(map[s[i]], j);
+                    map[s[i]] = i + 1;
+                }else{
+                    map.Add(s[i], i + 1);
+                }
+                result = Math.Max(result, i - j + 1);
             }
             
             return result;
