d7b42296dbfda6a82b65fe90cceb84fe3e998fd6
Ning <xiaoningning@yahoo.com>
2017-12-12 09:31:01 -0800

    hamming dist update OJ

--- hammingDistance/Program.cs
+++ hammingDistance/Program.cs
@@ -6,12 +6,22 @@ namespace hammingDistance
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("input numbers 0 <= x, y <= 2^31" );
             Console.WriteLine("input numbers: {0}, {1}", args[0], args[1] );
             Console.WriteLine("Hamming Distance: {0}", 
                             HammingDistance(Int32.Parse(args[0]), Int32.Parse(args[1])));
         }
 
         public static int HammingDistance(int x, int y) {
+            int dist = 0;
+            
+            for (int i = 0; i < 32; i++){
+                dist += ((x >> i) ^ (y >> i)) & 1;
+            }
+
+            return dist;
+        }
+        public static int HammingDistance1(int x, int y) {
             int dist = 0;
             int xor = x ^ y;
             
