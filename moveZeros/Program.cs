1d2b0344da20e65b6110e0f7d91dbc347ee76eb9
Ning Wei <xiaoningning@yahoo.com>
2017-12-22 19:29:43 +0000

    move zeros OJ

---
+++ moveZeros/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace moveZeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("input num list: {0}", args[0]);
+            int[] nums = Array.ConvertAll(args[0].Split(','), s => int.Parse(s)); 
+            MoveZeroes(nums);
+            Console.WriteLine("output: {0}",  string.Join(",", nums.Select(i => i.ToString()).ToArray()));
+        }
+
+        static void MoveZeroes(int[] nums) {
+            for (int lastNonZero = 0, i = 0; i < nums.Length; i++){
+                if(nums[i] != 0){
+                    swapArray(nums, lastNonZero, i);
+                    lastNonZero++;
+                }
+            }        
+        }
+        static void swapArray(int[] arr, int i, int j){
+            int t = arr[i];
+            arr[i] = arr[j];
+            arr[j] = t;
+        }
+    }
+}
