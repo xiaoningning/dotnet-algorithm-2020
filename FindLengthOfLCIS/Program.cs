01230ae8433615f526019e16e0d1876a65240c97
Ning Wei <xiaoningning@yahoo.com>
2018-01-09 19:17:11 +0000

    find the length of longest continue increasing sequence OJ

---
+++ FindLengthOfLCIS/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FindLengthOfLCIS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("array: {0}", args[0]);
+            int[] nums = Array.ConvertAll(args[0].Split(','), s => int.Parse(s));
+            Console.WriteLine("the length of LCIS: {0}", FindLengthOfLCIS(nums));
+        }
+        
+        static int FindLengthOfLCIS(int[] nums) {
+            int res = 0, cnt = 0;
+            for (int i = 0; i < nums.Length; i++){
+                if (i ==0 || nums[i] > nums[i - 1]){
+                    res = Math.Max(res, ++cnt);
+                }
+                else
+                {
+                    cnt = 1;
+                }
+            }
+            return res;
+        }
+    }
+}
