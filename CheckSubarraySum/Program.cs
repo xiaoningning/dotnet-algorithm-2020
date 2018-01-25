d329872ad96f6345ca614290b3df4de50d901313
Ning Wei <xiaoningning@yahoo.com>
2018-01-25 01:54:58 +0000

    check sub array sum OJ

---
+++ CheckSubarraySum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CheckSubarraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("nums array: {0}", args[0]);
+            int[] nums = Array.ConvertAll(args[0].Split(','), s => int.Parse(s));
+            int k = int.Parse(args[1]);
+            Console.WriteLine("target value: {0}", k);            
+            Console.WriteLine("find the target: {0}", CheckSubarraySum(nums, k) );
+        }
+
+        static bool CheckSubarraySum(int[] nums, int k) {
+            // put the modulus of k into set
+            HashSet<int> set = new HashSet<int>();
+            int sum = 0;
+            int pre = 0; // at least two nums
+            foreach (var n in nums)
+            {   
+                sum += n;
+                int res = (k==0) ? sum : (sum % k);
+                if(set.Contains(res)) return true;
+                set.Add(pre);
+                pre = res;
+            }
+            return false;
+        }
+    }
+}
