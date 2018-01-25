61c641b0b88f86b94cc62e6865d5b8f4c5df668b
Ning Wei <xiaoningning@yahoo.com>
2018-01-25 00:47:21 +0000

    diameter of binary tree OJ

---
+++ DiameterOfBinaryTree/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DiameterOfBinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The tree input is hard coded.");            
+                        TreeNode root = new TreeNode(5);
+            root.left = new TreeNode(3);
+            root.right = new TreeNode(6);
+            root.left.left = new TreeNode(2);
+            root.left.right = new TreeNode(4);
+            root.right.right = new TreeNode(7);
+            Console.WriteLine("diameter of tree:{0}", DiameterOfBinaryTree(root));
+        }
+
+        static int res;
+        static int DiameterOfBinaryTree(TreeNode root) {
+            res = 0;
+            GetDepth(root);
+            return res;
+        }
+
+        static int GetDepth(TreeNode t){
+            if (t == null) return 0;
+            int left = GetDepth(t.left);
+            int right = GetDepth(t.right);
+            res = Math.Max(res, left + right + 1);
+            return Math.Max(left, right) + 1;
+        }
+
+    }
+    public class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+}
