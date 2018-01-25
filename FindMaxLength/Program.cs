93e337430eb333feb2a2f85f59aeed4a828f3e5b
Ning Wei <xiaoningning@yahoo.com>
2018-01-25 01:39:29 +0000

    find max length of 0/1 array OJ

---
+++ FindMaxLength/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindMaxLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("0/1 array: {0}", args[0]);                        
+            int[] nums = Array.ConvertAll(args[0].Split(','), s => int.Parse(s));
+            Console.WriteLine("max length of equal of 0, 1: {0}", FindMaxLength(nums));
+        }
+
+        static int FindMaxLength(int[] nums) {
+            // key: sum, value: index
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            int sum = 0;
+            int maxLen = 0;
+            map[0] = -1; // initial position of equal of 0/1
+            for(int i = 0; i < nums.Length; i++){
+                sum += nums[i] == 1 ? 1 : -1;
+                if (map.ContainsKey(sum)){
+                    maxLen = Math.Max(maxLen, i - map[sum]);
+                }
+                else{
+                    map[sum] = i;
+                }
+            }
+            return maxLen;
+        }
+    }
+}
