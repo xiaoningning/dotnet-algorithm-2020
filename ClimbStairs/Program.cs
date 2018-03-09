fdc44e8a61280bd229259eb165d4352322c9714f
Ning Wei <xiaoningning@yahoo.com>
2018-03-09 01:51:44 +0000

    climbstairs minCostClimbStairs OJ

---
+++ ClimbStairs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClimbStairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("stairs: {0}", args[0]);
+            int n = int.Parse(args[0]);
+            Console.WriteLine("the ways of climb stairs: {0}", ClimbStairs(n));            
+        }
+        
+        static int ClimbStairs(int n){
+            if (n <= 1) return 1;
+            
+            int[] dp = new int[n];
+            dp[0] = 1; dp[1] = 2;
+            for (int i = 2; i < n; i++){
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+            return dp[n-1];
+        }
+    }
+}
---
+++ MinCostClimbingStairs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MinCostClimbingStairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("stair cost: {0}", args[0]);
+            int[] cost = Array.ConvertAll(args[0].Split(','), s => int.Parse(s));
+            Console.WriteLine("min cost climbing stairs: {0}", MinCostClimbingStairs(cost));
+        }
+        static int MinCostClimbingStairs(int[] cost) {
+            int n = cost.Length;
+            int[] dp = new int[n];
+            dp[0] = cost[0];
+            dp[1] = cost[1];
+            for (int i = 2; i < n; i++){
+                dp[i] = cost[i] + Math.Min(dp[i-1], dp[i-2]);
+            }
+            return Math.Min(dp[n-2], dp[n-1]);
+        }
+    }
+}
