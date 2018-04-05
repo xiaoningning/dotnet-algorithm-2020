f742c8acd34f00c061af1de2562a1261b6f7729a
Ning Wei <xiaoningning@yahoo.com>
2018-04-05 22:29:54 +0000

    fix small bug on num of decoding II

--- NumDecodingsII/Program.cs
+++ NumDecodingsII/Program.cs
@@ -26,7 +26,7 @@ namespace NumDecodingsII
                         dp[i] += 2 * dp[i - 2];
                     }
                 }
-                else if (s[i-1] >= '1' || s[i-1] <= 9){
+                else if (s[i-1] >= '1'){
                     dp[i] += dp[i - 1];
                     if (s[i - 2] == '1' || (s[i - 2] == '2' && s[i-1] <= '6')) {
                         dp[i] += dp[i - 2];
