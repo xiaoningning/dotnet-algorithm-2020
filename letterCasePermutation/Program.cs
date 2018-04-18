a68d694b266b39842f4a8e302d3d0b421f438ad4
Ning Wei <xiaoningning@yahoo.com>
2018-04-18 00:05:12 +0000

    letter case permutation OJ

---
+++ letterCasePermutation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace letterCasePermutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = args[0];
+            Console.WriteLine("string: " + args[0]);
+            Console.WriteLine("Letter Case permutation: {0}", string.Join(',', LetterCasePermutation(S)));
+        }
+
+        static IList<string> LetterCasePermutation(string S) {
+            List<string> res = new List<string>();
+            res.Add(S);
+            int n = S.Length;
+            for (int i = 0; i < n; i++){
+                if (S[i] >= '0' && S[i] <= '9') continue;
+                List<string> temp = new List<string>();
+                foreach(string s in res){
+                    char[] c = s.ToCharArray();
+                    c[i] = char.ToLower(c[i]);
+                    temp.Add(new string(c));
+                    c[i] = char.ToUpper(c[i]);
+                    temp.Add(new string(c));
+                }
+                res = temp;
+            }        
+            return res;
+        }
+    }
+}
