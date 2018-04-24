8fa7f65065dfb7b9ec3b8054f11566272321215a
Ning Wei <xiaoningning@yahoo.com>
2018-04-24 18:49:55 +0000

    combination sum 4 OJ

---
+++ CombinationSum4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CombinationSum4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[]{1, 2, 3};
+            int target = 4;
+            Console.WriteLine("CombinationSum4: {0}", CombinationSum4(nums, target));
+        }
+        static int CombinationSum4(int[] nums, int target) {
+            int[] dp = new int[target+1];
+            dp[0] = 1;
+            for(int i = 1; i <= target; i++){
+                for(int j = 0; j < nums.Length; j++){
+                    if (i >= nums[j]) dp[i] += dp[i - nums[j]];
+                }
+            }
+            return dp[target];
+        }
+    }
+}
