c08dad250dd32f9fa1fe5e42b6c0fd7df13c95de
Ning Wei <xiaoningning@yahoo.com>
2018-04-24 00:00:05 +0000

    sum of left leaves OJ

---
+++ sumLeftLeaves/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace sumLeftLeaves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var root = new TreeNode(3);
+            root.left = new TreeNode(9);
+            root.right = new TreeNode(20);
+            root.right.left = new TreeNode(15);
+            root.right.right = new TreeNode(7);
+            Console.WriteLine("sum of left leaves: {0}", SumOfLeftLeaves(root));            
+        }
+        static int SumOfLeftLeaves(TreeNode root) {        
+            int res = Helper(root, false);
+            return res;
+        }
+        static int Helper(TreeNode root, bool isLeft){
+            if(root == null) return 0;
+            if(root.left == null && root.right == null && isLeft) return root.val;
+            return Helper(root.left, true) + Helper(root.right, false);        
+        }
+    }
+    public class TreeNode {
+       public int val;
+       public TreeNode left;
+       public TreeNode right;
+       public TreeNode(int x) { val = x; }
+    }
+}
