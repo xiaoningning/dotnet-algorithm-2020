040955ef8f7dce75d96fd9be999ccd07f3ac2c6b
Ning Wei <xiaoningning@yahoo.com>
2018-04-24 00:37:10 +0000

    random pick index OJ

---
+++ randomPickIndex/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace randomPickIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] {1,2,3,3,3};
+            var obj = new Solution(nums);
+            Console.WriteLine("random pick index:{0}", obj.Pick(3));
+        }
+    }
+    public class Solution {
+        int[] nums;
+        Random rnd;
+
+        public Solution(int[] nums) {
+            this.nums = nums;
+            this.rnd = new Random();
+        }
+        
+        public int Pick(int target) {
+            int result = -1;
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++) {
+                if (nums[i] != target) continue;
+                if (rnd.Next(++count) == 0) result = i;
+            }        
+            return result;
+        }
+    }
+}
