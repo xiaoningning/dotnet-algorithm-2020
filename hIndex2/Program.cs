8b55b95bf484d89bf1c1cde86624f6c4bbf862a9
Ning Wei <xiaoningning@yahoo.com>
2018-04-27 23:18:12 +0000

    h index of citations 2 OJ

---
+++ hIndex2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace hIndex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Solution();
+            int[] citations = new int[]{6,5,1,3,0};
+            Console.WriteLine("H index of citiations: {0}", obj.HIndex(citations));   
+        }
+    }
+    public class Solution {
+        public int HIndex(int[] citations) {
+            int len = citations.Length;
+            int left = 0;
+            int right = len - 1;
+            while(left <= right){
+                int mid = left + (right -left)/2;
+                if(citations[mid] == (len-mid)) return citations[mid];
+                else if(citations[mid] > len - mid) right = mid - 1;
+                else left = mid + 1;
+            }
+            return len - left;
+        }
+    }
+}
