723b9eecf9a35823d5001dff9747b12711517ce7
Ning Wei <xiaoningning@yahoo.com>
2018-04-27 00:48:45 +0000

    first bad vesion OJ

---
+++ firstBadVersion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace firstBadVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Solution();
+            Console.WriteLine("First Bad Version in 5: {0}", obj.FirstBadVersion(5));                  
+        }
+    }
+    public class Solution {
+        public int FirstBadVersion(int n) {
+            int left = 1, right = n;
+            while(left < right) {
+                int mid = left + (right-left) /2;
+                if(IsBadVersion(mid)) right = mid;
+                else left =  mid + 1;
+            }
+            return left;
+        }
+        bool IsBadVersion(int version){
+           if (version == 3)  return true;
+           else return false;
+        }
+    }
+}
