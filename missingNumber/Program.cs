b9acb602fbd6206db510ac855ed60b05af5769bf
Ning Wei <xiaoningning@yahoo.com>
2018-04-30 17:43:58 +0000

    missing number OJ

---
+++ missingNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace missingNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Solution();
+            int[] nums = new int[]{3, 0, 1};
+            Console.WriteLine("find missing number: {0}", obj.MissingNumber(nums));            
+        }
+    }
+    public class Solution {
+        public int MissingNumber(int[] nums) {
+            int sum = 0;
+            int n = nums.Length;
+            foreach(int i in nums){
+                sum += i;
+            }
+            int targetSum = 0;
+            for(int i = 1; i <= n; i++){
+                targetSum += i;
+            }
+            return targetSum - sum;
+        }
+    }
+}
