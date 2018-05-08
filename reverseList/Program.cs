2883fbe0f71e0665fc459f60093b9d5edebce45d
Ning Wei <xiaoningning@yahoo.com>
2018-05-08 20:42:18 +0000

    reverse list OJ

---
+++ reverseList/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace reverseList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode head = new ListNode(1);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(3);
+            head.next.next.next = new ListNode(4);
+            var obj = new Solution();
+            var res = obj.ReverseList(head);
+            Console.WriteLine("reverse List");
+            while(res != null){
+                Console.WriteLine(res.val);
+                res = res.next;
+            }
+        }
+    }
+    public class ListNode {
+       public int val;
+       public ListNode next;
+       public ListNode(int x) { val = x; }
+    }
+    public class Solution {
+        public ListNode ReverseList(ListNode head) {
+            ListNode prev = null;
+            while(head != null){
+                ListNode tmp = head.next;
+                head.next = prev;
+                prev = head;
+                head = tmp;
+            }      
+            return prev;
+        }
+    }
+}
