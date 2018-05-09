ae09503a0992d81fcd35851b2ccc546f6f63513d
Ning Wei <xiaoningning@yahoo.com>
2018-05-09 00:17:38 +0000

    graph valid tree with Union Find OJ

--- graphValidTree/Program.cs
+++ graphValidTree/Program.cs
@@ -9,10 +9,33 @@ namespace graphValidTree
         {
             var obj = new Solution(); 
             Console.WriteLine("Graph is valid tree:{0}", obj.ValidTree(1, new int[,]{}));
-            Console.WriteLine("Graph is valid tree:{0}", obj.ValidTree(3, new int[,]{{0,1},{0,2}}));
+            Console.WriteLine("Graph is valid tree:{0}", obj.ValidTree1(3, new int[,]{{0,1},{0,2}}));
         }
     }
     public class Solution {
+        public bool ValidTree1(int n, int[,] edges) {
+            // tree is no cycle connected of graph
+            // Union Find the common root of two nodes
+            int[] nodeRoots = new int[n];
+            for(int i = 0; i < n; i++) nodeRoots[i] = -1;
+            for(int e = 0; e < edges.GetLength(0); e++){
+                int x = FindRoot(nodeRoots, edges[e,0]);
+                int y = FindRoot(nodeRoots, edges[e,1]);
+                // nodes have the same root
+                // they are cyclely connected.
+                if (x == y) return false;
+                // x and y is connected
+                nodeRoots[x] = y;
+            }
+            return edges.GetLength(0) == n - 1;
+        }
+
+        int FindRoot(int[] roots, int i){
+            // it has a root
+            while (roots[i] != -1) i = roots[i];
+            return i;
+        }
+        
         public bool ValidTree(int n, int[,] edges) {
             // tree is no cycle connected of graph
             Dictionary<int, HashSet<int>> g = new Dictionary<int, HashSet<int>>();
@@ -42,6 +65,6 @@ namespace graphValidTree
                 }
             }
             return visited.Count == n;
-        }
+        }        
     }
 }
