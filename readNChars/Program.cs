c0d3a711d0d678a6f96def4e4627f67a4fd57991
Ning Wei <xiaoningning@yahoo.com>
2018-05-11 23:27:01 +0000

    read n chars OJ

---
+++ readNChars/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace readNChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Solution();
+            string s = "abcde";
+            int n = obj.Read(s.ToCharArray(), 5);
+            Console.WriteLine("Read N chars: {0}", s.Substring(0,n));
+        }
+    }
+    public class Solution {
+        /**
+        * @param buf Destination buffer
+        * @param n   Maximum number of characters to read
+        * @return    The number of characters read
+        */
+        public int Read(char[] buf, int n) {
+            int t = Read4(buf);
+            if (t >=n) return n;
+            else if (t < 4) return t;
+            else{
+                char[] tmp = new char[buf.Length-4];
+                Array.Copy(buf, 4, tmp, 0, buf.Length-4);            
+                return 4 + Read(tmp, n-4);
+            }
+        }
+        int Read4(char[] buf){
+            int n = buf.Length;
+            if(n >= 4) return 4;
+            else return n;
+        }
+    }
+}
