3d37334d26227f69c058e08eb201abfeea5be44a
Ning Wei <xiaoningning@yahoo.com>
2018-05-14 21:57:02 +0000

    word break 2 and longest consecutive seq OJ

---
+++ longestConsecutive/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace longestConsecutive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Solution();
+            int[] nums = new int[]{1,3,100,2,300,4};
+            Console.WriteLine("longest consecutive sequence");
+            Console.WriteLine("{0}", string.Join(",", obj.LongestConsecutive(nums)));
+        }
+    }
+    public class Solution {
+        public int LongestConsecutive(int[] nums) {
+            int res = 0;
+            HashSet<int> s = new HashSet<int>();
+            foreach (int num in nums) s.Add(num);
+            foreach (int num in nums) {
+                if (s.Remove(num)) {
+                    int pre = num - 1, next = num + 1;
+                    while (s.Remove(pre)) --pre;
+                    while (s.Remove(next)) ++next;
+                    res = Math.Max(res, next - pre - 1);
+                }
+            }
+            return res;    
+        }
+    }
+}
---
+++ wordBreak2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace wordBreak2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Solution();
+            List<string> dict = new List<string>(){"leet", "code"};
+            var res = obj.WordBreak("leetcode", dict);
+            Console.WriteLine("word break 2 {0}", string.Join(",", res));
+        }
+    }
+    public class Solution {
+        public IList<string> WordBreak(string s, IList<string> wordDict) {
+            Dictionary<string, List<string>> map = new  Dictionary<string, List<string>>();
+            return DFS(s, wordDict, map);
+        }
+        List<string> DFS(string s, IList<string> wordDict, Dictionary<string, List<string>> map){
+            if(map.ContainsKey(s)) return map[s];
+            if(string.IsNullOrEmpty(s)) return new List<string>(){""};
+            List<string> res = new List<string>();
+            foreach(string w in wordDict){
+                if(s.StartsWith(w)){
+                    string nextStr = s.Substring(w.Length);
+                    List<string> remains = DFS(nextStr, wordDict, map);
+                    foreach(string r in remains){
+                        res.Add(w + (r.Length == 0 ? "" : " ") + r);
+                    }
+                }
+            }
+            map[s] = res;
+            return res;
+        }
+    }
+}
