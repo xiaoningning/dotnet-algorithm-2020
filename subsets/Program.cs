b6d88a1f4561fa69f64e9c11ccbb5714c3bafc21
Ning Wei <xiaoningning@yahoo.com>
2018-05-16 23:24:24 +0000

    subsets OJ

---
+++ subsets/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace subsets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Solution();
+            int[] nums = new int[]{1,2,3};
+            var res = obj.Subsets(nums);
+            Console.WriteLine("Subsets");
+            foreach(var r in res) Console.WriteLine(string.Join(",",r));
+        }
+    }
+    public class Solution {
+        public IList<IList<int>> Subsets(int[] nums) {
+            List<IList<int>> res = new List<IList<int>>();
+            GetSubsets(nums, 0, new List<int>(), res);
+            return res;
+        }
+        void GetSubsets(int[] nums, int pos, List<int> tmp, List<IList<int>> res){
+            res.Add(new List<int>(tmp));
+            for (int i = pos; i < nums.Length; ++i) {
+                tmp.Add(nums[i]);
+                GetSubsets(nums, i+1, tmp, res);
+                tmp.Remove(nums[i]);
+            }
+        }
+    }
+}
