6fbed832b6b4f8a40728e503bc323ecb49375046
Ning Wei <xiaoningning@yahoo.com>
2018-05-16 22:05:38 +0000

    best time buy sell stock OJ

---
+++ bestTimeBuySellStock/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace bestTimeBuySellStock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Solution();
+            int[] prices = new int[]{7,1,5,3,6,4};
+            Console.WriteLine("best time buy sell stock {0}", string.Join(",", obj.MaxProfit(prices)));
+        }        
+    }
+    public class Solution {
+        public int MaxProfit(int[] prices) {
+            int res = 0, minPrice = Int32.MaxValue;
+            foreach (int price in prices) {
+                minPrice = Math.Min(minPrice, price);
+                res = Math.Max(res, price - minPrice);
+            }
+            return res;
+        }
+    }
+}
