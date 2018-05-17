24daf6ea820dbfb90c6d90e7afe1b4278c2ec003
Ning Wei <xiaoningning@yahoo.com>
2018-05-17 00:27:31 +0000

    largest rectangle OJ

---
+++ largestRectangle/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace largestRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n1 = new int[]{2,1,5,6,2,3};
+            var obj = new Solution();
+            Console.WriteLine("largest rectangle [2,1,5,6,2,3] {0}", obj.LargestRectangleArea(n1));
+            n1 = new int[]{1};
+            Console.WriteLine("largest rectangle [1] {0}", obj.LargestRectangleArea(n1));
+        }
+    }
+    public class Solution {
+        public int LargestRectangleArea(int[] heights) {
+            Stack<int> st = new Stack<int>();
+            int res = 0;
+            for(int i = 0; i <= heights.Length; i++){
+                // handle only only heights.Length = 1, such as [1]
+                int tmp = i < heights.Length ? heights[i] : 0;
+                while (st.Count != 0 && heights[st.Peek()] >= tmp) {
+                    int cur = st.Pop();
+                    res = Math.Max(res, heights[cur] * (st.Count == 0 ? i : (i - st.Peek() - 1)));
+                }
+                st.Push(i);
+            }
+            return res;
+        }
+    }
+}
