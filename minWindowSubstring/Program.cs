9643454d6adb68ec50fa91882c32d5b8cca32215
Ning Wei <xiaoningning@yahoo.com>
2018-05-24 01:32:21 +0000

    min window substring OJ

---
+++ minWindowSubstring/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace minWindowSubstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Solution();
+            string s = "ADOBECODEBANC";
+            string t = "ABC";
+            Console.WriteLine("min windows substr {0}", obj.MinWindow(s,t));
+        }
+    }
+    public class Solution {
+        public string MinWindow(string s, string t) {
+            int min = Int32.MaxValue;
+            int cnt = t.Length;
+            int begin = 0, end = 0, head = 0;
+            int[] map = new int[128];
+            foreach(char c in t) map[c-'A']++;
+            while(end < s.Length){
+                if(map[s[end++] -'A']-- > 0) cnt--;
+                while(cnt == 0){
+                    if(end - begin < min){
+                        min = end - begin; 
+                        head = begin;
+                    }  
+                    if(map[s[begin++] - 'A']++ == 0) cnt++; 
+                }
+            }
+            return min == Int32.MaxValue ? "" : s.Substring(head, min);
+        }
+    }
+}
