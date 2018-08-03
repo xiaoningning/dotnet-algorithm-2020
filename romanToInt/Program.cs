564f9ef7ec7c8b1b54d877a9bb507954b8d24fe2
Ning Wei <xiaoningning@yahoo.com>
2018-08-03 16:58:06 -0700

    roman to int OJ

---
+++ romanToInt/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace romanToInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Solution();
+            Console.WriteLine("roman to int : {0}", obj.RomanToInt("MCMXCIV"));
+        }
+    }
+    public class Solution {
+        public int RomanToInt(string s) {
+            Dictionary<char, int> m = new Dictionary<char, int> {
+                {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
+            };
+            int res = 0;
+            for (int i = 0; i < s.Length; ++i) {
+                int val = m[s[i]];
+                if (i == s.Length - 1 || m[s[i+1]] <= m[s[i]]) res += val;
+                else res -= val;
+            }
+            return res;
+        }
+    }
+}
