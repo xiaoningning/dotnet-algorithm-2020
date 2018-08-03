2332cf61c5373250f05bb9a25f412ca994e71de0
Ning Wei <xiaoningning@yahoo.com>
2018-08-03 12:05:00 -0700

    BST to double linked list OJ

---
+++ treeToDoublyList/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace treeToDoublyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Solution();
+            var left = new Node(3, null, null);
+            var right = new Node(5, null, null);
+            var root = new Node(4, left, right);
+            obj.TreeToDoublyList(root);
+        }
+    }
+    public class Node {
+        public int val;
+        public Node left;
+        public Node right;
+
+        public Node(){}
+        public Node(int _val, Node _left, Node _right) {
+            val = _val;
+            left = _left;
+            right = _right;
+        }
+    }
+    public class Solution {
+        Node prev = null;
+        public Node TreeToDoublyList(Node root) {
+            if (root == null) return null;
+            var head = new Node(0, null, null);
+            prev = head;
+            Helper(root);
+            //connect head and tail
+            prev.right = head.right;
+            head.right.left = prev;
+            return head.right;
+        }
+        private void Helper (Node cur) {
+            if (cur == null) return;
+            Helper(cur.left);
+            prev.right = cur;
+            cur.left = prev;
+            prev = cur;
+            Helper(cur.right);
+        }
+    }
+}
