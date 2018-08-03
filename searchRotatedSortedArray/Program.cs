455849665bb2ecd5097141ddf53cff9bf61b5824
Ning Wei <xiaoningning@yahoo.com>
2018-08-03 15:29:21 -0700

    search in rotated sorted array OJ

---
+++ searchRotatedSortedArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace searchRotatedSortedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Solution();
+            int t = 3;
+            int[] nums = new int[]{4,5,6,7,0,1,2,3};
+            Console.WriteLine("search in rotated sorted array {0} : {1}", t, obj.Search(nums, t));
+        }
+    }
+    public class Solution {
+        public int Search(int[] nums, int target) {
+            int n = nums.Length;
+            if (n == 0) return -1;
+            int left = 0, right = n - 1;
+            while (left <= right) {
+                int mid = (left + right) / 2;
+                if (nums[mid] == target) return mid;
+                else if (nums[mid] < nums[right]) {
+                    if (nums[mid] < target && nums[right] >= target) left = mid + 1;
+                    else right = mid - 1;
+                } else {
+                    if (nums[left] <= target && nums[mid] > target) right = mid - 1;
+                    else left = mid + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
