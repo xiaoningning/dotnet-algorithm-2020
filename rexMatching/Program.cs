8b7054d450da16a56eadd0382c3272c76c30557d
Ning Wei <xiaoningning@yahoo.com>
2018-08-09 10:29:44 -0700

    rex expression matching with DP OJ

--- rexMatching/Program.cs
+++ rexMatching/Program.cs
@@ -7,7 +7,7 @@ namespace rexMatching
         static void Main(string[] args)
         {
             var obj = new Solution();
-            Console.WriteLine("regular expression match:{0}", obj.IsMatch("aab", "a.b*"));
+            Console.WriteLine("regular expression match:{0}", obj.IsMatch1("aab", "a.b*"));
         }
 
         //'.' Matches any single character.
@@ -22,6 +22,22 @@ namespace rexMatching
                     return !string.IsNullOrEmpty(s) 
                         && (s[0] == p[0] || p[0] == '.') && IsMatch(s.Substring(1), p.Substring(1));
                 }
+            }
+
+            public bool IsMatch1(string s, string p){
+                bool[,] dp = new bool[s.Length + 1, p.Length + 1];
+                dp[0,0] = true;
+                for (int i = 0; i <= s.Length; i++){
+                    for (int j = 1; j <= p.Length; j++){
+                        if (j > 1 && p[j-1] == '*'){
+                            dp[i,j] = dp[i, j-2] || (i > 0 && (s[i-1] == p[j-2] || p[j-2] == '.' ) && dp[i-1,j]);
+                        }
+                        else {
+                            dp[i,j] = i > 0 && dp[i-1, j-1] && (s[i-1] == p[j-1] || p[j-1] == '.');
+                        }
+                    }
+                }
+                return dp[s.Length, p.Length];
             }
         }
     }
