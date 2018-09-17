6c8eec493305d0a906b6ce396c9e22423bfcccc7
Ning Wei <xiaoningning@yahoo.com>
2018-09-17 16:21:36 -0700

    monotonic array OJ

---
+++ monotonicArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace monotonicArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Solution();
+            int[] A = new int[]{1,2,2,4};
+            Console.WriteLine("Is Monotonic Array: {0}", obj.IsMonotonic(A));
+        }
+    }
+    public class Solution {
+        public bool IsMonotonic(int[] A) {
+            bool inc = true, dec = true;
+            for (int i = 1; i < A.Length; i++){
+                inc &= A[i -1] <= A[i];
+                dec &= A[i -1] >= A[i];
+            }
+            return inc || dec;
+        }
+    }
+}
