ed66aba9838bee746ceccf504737e773358f86f4
Ning Wei <xiaoningning@yahoo.com>
2018-09-20 10:21:19 -0700

    intersection two arrays ii +1 OJ

--- intersectionTwoArraysII/Program.cs
+++ intersectionTwoArraysII/Program.cs
@@ -26,6 +26,23 @@ namespace intersectionTwoArraysII
                 }
                 return res.ToArray();
             }
+
+            public int[] Intersect1(int[] nums1, int[] nums2) {
+                Array.Sort(nums1);
+                Array.Sort(nums2);
+                List<int> res = new List<int>();
+                int i = 0, j = 0;
+                while (i < nums1.Length && j < nums2.Length) {
+                    if (nums1[i] == nums2[j]) {
+                        res.Add(nums1[i]);
+                        i++;
+                        j++;
+                    }
+                    else if (nums1[i] > nums2[j]) j++;
+                    else i++;                
+                }
+                return res.ToArray();
+            }
         }
     }
 }
