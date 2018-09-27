1d6ca740268b34a7d91d086d944a2f38edcb77b8
Ning Wei <xiaoningning@yahoo.com>
2018-09-27 12:04:49 -0700

    remove Nth node from the end  OJ

---
+++ removeNthFromEnd/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace removeNthFromEnd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Solution();
+            var head = new ListNode(5);
+            head.next = new ListNode(3);
+            head.next.next = new ListNode(2);
+            head.next.next.next = new ListNode(1);
+            var res = obj.RemoveNthFromEnd(head, 1);
+            Console.WriteLine("Remove Nth From End");
+        }
+    }
+    /**
+    * Definition for singly-linked list.
+    */
+    public class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+    public class Solution {
+        public ListNode RemoveNthFromEnd(ListNode head, int n) {
+            ListNode dummy = new ListNode(0);
+            dummy.next = head;
+            ListNode slow = dummy, fast = dummy;
+            
+            //Move fast in front so that the gap between slow and fast becomes n
+            for (int i = 0; fast != null && i <= n; i++) {
+                fast = fast.next;
+            }
+            
+            //Move fast to the end, maintaining the gap
+            while (fast != null) {
+                fast = fast.next;
+                slow = slow.next;
+            }
+            slow.next = slow.next.next;
+            return dummy.next;
+        }
+    }
+}
