3e118812d511184873ca94ce8833f48bd3084f1d
Ning Wei <xiaoningning@yahoo.com>
2018-10-01 10:12:54 -0700

    get intersection two linked list OJ

---
+++ intersectionTwoLinkedList/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace intersectionTwoLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Solution();
+            Console.WriteLine("Get Intersection Node 1");
+            ListNode headA = new ListNode(1);
+            ListNode headB = new ListNode(2);
+            headB.next = new ListNode(1);
+            obj.GetIntersectionNode(headA, headB);
+        }
+    }
+    /**
+    * Definition for singly-linked list.
+    */
+    public class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+    public class Solution {
+        public ListNode GetIntersectionNode1(ListNode headA, ListNode headB) {
+            if(headA == null || headB == null) return null;
+            
+            int la = GetLength(headA);
+            int lb = GetLength(headB);
+            while (la < lb) {
+                headB = headB.next;
+                lb--;
+            }
+            while (lb < la) {
+                headA = headA.next;
+                la--;
+            }
+            while (headA != headB){
+                headA = headA.next;
+                headB = headB.next;
+            }
+            return headA;
+        }
+        
+        private int GetLength(ListNode root) {
+            int res = 0;
+            while (root != null) {
+                root = root.next;
+                res++;
+            }
+            return res;
+        }
+        
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB) {
+            if(headA == null || headB == null) return null;
+            var a = headA;
+            var b = headB;
+            while (a != b){
+                // if both lengths different, 
+                // link them as a loop and then meet at the intersection
+                a = a == null? headB : a.next;
+                b = b == null? headA : b.next; 
+            }
+            return a;
+        }
+    }
+}
