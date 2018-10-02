693b28b5c9931ca782adf865481ef65f803c7526
Ning Wei <xiaoningning@yahoo.com>
2018-10-02 09:39:07 -0700

    diameter of BT with memory optimization OJ

--- DiameterOfBinaryTree/Program.cs
+++ DiameterOfBinaryTree/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace DiameterOfBinaryTree
 {
     class Program
@@ -13,25 +13,29 @@ namespace DiameterOfBinaryTree
             root.left.left = new TreeNode(2);
             root.left.right = new TreeNode(4);
             root.right.right = new TreeNode(7);
-            Console.WriteLine("diameter of tree:{0}", DiameterOfBinaryTree(root));
+            var obj = new Solution();
+            Console.WriteLine("diameter of tree:{0}", obj.DiameterOfBinaryTree(root));
         }
-
-        static int res;
-        static int DiameterOfBinaryTree(TreeNode root) {
+    }
+    public class Solution {
+        int res;
+        Dictionary<TreeNode, int> map = new Dictionary<TreeNode, int>();
+        public int DiameterOfBinaryTree(TreeNode root) {
             res = 0;
             GetDepth(root);
             return res;
         }
 
-        static int GetDepth(TreeNode t){
+        int GetDepth(TreeNode t){
             if (t == null) return 0;
+            if (map.ContainsKey(t)) return map[t];
             int left = GetDepth(t.left);
             int right = GetDepth(t.right);
-            res = Math.Max(res, left + right + 1);
+            res = Math.Max(res, left + right);
+            map[t] = res;
             return Math.Max(left, right) + 1;
         }
-
-    }
+}
     public class TreeNode {
         public int val;
         public TreeNode left;
