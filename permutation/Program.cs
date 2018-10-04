ae0bbec169417db235183c9986a4e96d537c9bc3
Ning Wei <xiaoningning@yahoo.com>
2018-10-04 11:00:07 -0700

    permutation I/II with backtracking method OJ

---
+++ permutation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace permutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Solution();
+            var res = obj.Permute(new int[]{1,2,3});
+            Console.WriteLine("permutation");
+            foreach(var r in res) {
+                Console.WriteLine(string.Join(",", r));
+            }
+        }
+    }
+    public class Solution {
+        public IList<IList<int>> Permute(int[] nums) {
+            List<IList<int>> res = new List<IList<int>>();
+            Backtracking(nums, new List<int>(), res);
+            return res;
+        }
+        void Backtracking(int[] nums, List<int> tmp, List<IList<int>> res){
+            if (tmp.Count == nums.Length) res.Add(new List<int>(tmp));
+            else {
+                for (int i = 0; i < nums.Length; ++i) {
+                    if (tmp.Contains(nums[i])) continue;
+                    tmp.Add(nums[i]);
+                    Backtracking(nums, tmp, res);
+                    tmp.RemoveAt(tmp.Count - 1);
+                }
+            }
+        }
+    }
+}
---
+++ permutationII/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace permutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Solution();
+            var res = obj.Permute(new int[]{1,2,3});
+            Console.WriteLine("permutation");
+            foreach(var r in res) {
+                Console.WriteLine(string.Join(",", r));
+            }
+        }
+    }
+    public class Solution {
+        public IList<IList<int>> PermuteUnique(int[] nums) {
+            List<IList<int>> res = new List<IList<int>>();
+            Array.Sort(nums);
+            Backtracking(nums, new List<int>(), res, new bool[nums.Length]);
+            return res;
+        }
+        void Backtracking(int[] nums, List<int> tmp, List<IList<int>> res, bool[] used){
+            if (tmp.Count == nums.Length) res.Add(new List<int>(tmp));
+            else {
+                for (int i = 0; i < nums.Length; ++i) {
+                    if (used[i] || i > 0 && nums[i-1] == nums[i] && !used[i-1]) continue;
+                    tmp.Add(nums[i]);
+                    used[i] = true;
+                    Backtracking(nums, tmp, res, used);
+                    used[i] = false;
+                    tmp.RemoveAt(tmp.Count - 1);
+                }
+            }
+        }
+    }    
+}
