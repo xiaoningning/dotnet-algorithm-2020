6ef8bf9ef804c94c0828bf4ea25043bc2e553032
Ning Wei <xiaoningning@yahoo.com>
2018-10-04 17:40:46 -0700

    search rotated sorted array with update OJ

--- searchRotatedSortedArray/Program.cs
+++ searchRotatedSortedArray/Program.cs
@@ -18,13 +18,13 @@ namespace searchRotatedSortedArray
             if (n == 0) return -1;
             int left = 0, right = n - 1;
             while (left <= right) {
-                int mid = (left + right) / 2;
+                int mid = left + (right - left) / 2;
                 if (nums[mid] == target) return mid;
                 else if (nums[mid] < nums[right]) {
-                    if (nums[mid] < target && nums[right] >= target) left = mid + 1;
+                    if (nums[mid] < target && target <= nums[right]) left = mid + 1;
                     else right = mid - 1;
                 } else {
-                    if (nums[left] <= target && nums[mid] > target) right = mid - 1;
+                    if (nums[left] <= target && target < nums[mid]) right = mid - 1;
                     else left = mid + 1;
                 }
             }
