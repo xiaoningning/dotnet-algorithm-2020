098bc72ee7899d064b08accbe08650977eff00df
Ning Wei <xiaoningning@yahoo.com>
2018-10-04 17:28:53 -0700

    merge two sorted list with recursive approach OJ

--- mergeTwoSortedList/Program.cs
+++ mergeTwoSortedList/Program.cs
@@ -29,7 +29,7 @@ namespace mergeTwoSortedList
     }
     
     public class Solution {
-        public ListNode MergeTwoLists(ListNode l1, ListNode l2) {
+        public ListNode MergeTwoLists1(ListNode l1, ListNode l2) {
             ListNode dummy = new ListNode(-1);
             var cur = dummy;
             while (l1 != null && l2 != null) {
@@ -45,6 +45,18 @@ namespace mergeTwoSortedList
             }
             cur.next = (l1 != null) ? l1 : l2;
             return dummy.next;
+        }
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2) {
+            if (l1 == null) return l2;
+            if (l2 == null) return l1;
+            if (l1.val < l2.val) {            
+                l1.next = MergeTwoLists(l1.next, l2);
+                return l1;
+            }
+            else {
+                l2.next = MergeTwoLists(l1, l2.next);
+                return l2;
+            }
         }
     }
 }
