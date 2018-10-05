78a94296c32dc6e71953922bd9fa6df433a62fc2
Ning Wei <xiaoningning@yahoo.com>
2018-10-04 18:01:59 -0700

    search rotated sorted array II with duplicates OJ

--- searchRotatedSortedArrayII/Program.cs
+++ searchRotatedSortedArrayII/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace searchRotatedSortedArray
+namespace searchRotatedSortedArrayII
 {
     class Program
     {
@@ -8,29 +8,35 @@ namespace searchRotatedSortedArray
         {
             var obj = new Solution();
             int t = 3;
-            int[] nums = new int[]{4,5,6,7,0,1,2,3};
-            Console.WriteLine("search in rotated sorted array {0} : {1}", t, obj.Search(nums, t));
+            int[] nums = new int[]{1,3,1,1};
+            Console.WriteLine("search in rotated sorted array II with duplicates {0} : {1}", t, obj.Search(nums, t));
         }
     }
     public class Solution {
-        public int Search(int[] nums, int target) {
+        public bool Search(int[] nums, int target) {
             int n = nums.Length;
-            if (n == 0) return -1;
+            if (n == 0) return false;
             int left = 0, right = n - 1;
             while (left <= right) {
                 int mid = left + (right - left) / 2;
-                if (nums[mid] == target) return mid;
+                // [3 1 1] or [1 1 3 1]
+                // shift the far right to left
+                if (nums[mid] == target) return true;
                 else if (nums[mid] < nums[right]) {
                     if (nums[mid] < target && target <= nums[right]) left = mid + 1;
                     else right = mid - 1;
-                } 
-                // rotated in the right side
-                else {
+                }
+                else if (nums[mid] > nums[right]){
                     if (nums[left] <= target && target < nums[mid]) right = mid - 1;
                     else left = mid + 1;
+                } 
+                // duplicates on the rotation edge
+                // shift the far right to left
+                else {
+                    right--;
                 }
             }
-            return -1;
+            return false;
         }
     }
 }
