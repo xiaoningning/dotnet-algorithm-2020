7612c53403a9a633f0c4b5aea2536a65d2bdc940
Ning Wei <xiaoningning@yahoo.com>
2019-04-18 11:08:06 -0700

    populate next right pointer in each node I && II. OJ

---
+++ nextRightPointersEachNode/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace nextRightPointersEachNode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("populate next right points in each node of perfect bt");
+        }
+    }
+    public class Solution {
+        public Node Connect1(Node root) {
+            if (root == null) return null;
+            Node start = root, cur = null;
+            while (start.left != null) {
+                cur = start;
+                while (cur != null) {
+                    cur.left.next = cur.right;
+                    if (cur.next != null) cur.right.next = cur.next.left;
+                    cur = cur.next;
+                }
+                start = start.left;
+            }
+            return root;
+        }
+        
+        public Node Connect(Node root) {
+            if (root == null) return null;
+            if (root.left != null) root.left.next = root.right;
+            if (root.right != null) root.right.next = root.next != null ? root.next.left : null;
+            Connect(root.left);
+            Connect(root.right);
+            return root;
+        }
+    }
+
+    // Definition for a Node.
+    public class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node(){}
+        public Node(int _val,Node _left,Node _right,Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+}
---
+++ nextRightPointersEachNode2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace nextRightPointersEachNode2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("populate next right points in each node II of unbalanced bt");
+        }
+    }
+    public class Solution {
+        public Node Connect(Node root) {
+            if (root == null) return null;
+            Node p = root.next;
+            while (p != null) {
+                if (p.left != null) {
+                    p = p.left;
+                    break;
+                }
+                if (p.right != null) {
+                    p = p.right;
+                    break;
+                }
+                p = p.next;
+            }
+            if (root.left != null) root.left.next = root.right != null ? root.right : p;
+            if (root.right != null) root.right.next = p;
+            Connect(root.left);
+            Connect(root.right);
+            return root;
+        }
+    }
+
+    // Definition for a Node.
+    public class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node(){}
+        public Node(int _val,Node _left,Node _right,Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+}
