02cbbb289313e6ed58c5271bd9e993470b35adb9
Ning Wei <xiaoningning@yahoo.com>
2019-04-19 18:51:58 -0700

    wiggle sort 2 OJ

---
+++ wiggleSort2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace wiggleSort2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new Solution();
+            int[] nums = new int[]{1,5,1,1,6,4};
+            obj.WiggleSort(nums);
+            Console.WriteLine("wiggle sort: {0}", string.Join(",", nums));
+        }
+    }
+    public class Solution {
+        public void WiggleSort(int[] nums) {
+            int n = nums.Length;
+            int k = (n+1) / 2;
+            int j = n;
+            int[] t = new int[n];
+            nums.CopyTo(t, 0);
+            Array.Sort(t);            
+            for (int i = 0; i < n; i++) {
+                nums[i] = (i % 2) == 1 ? t[--j] : t[--k];
+            }        
+        }
+    }
+}
