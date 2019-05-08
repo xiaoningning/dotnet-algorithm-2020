13e29cfd0e9b2e288a30f9ed5cf68d502aff1906
Ning Wei <xiaoningning@yahoo.com>
2019-05-08 16:15:56 -0700

    quick sort

---
+++ quickSort/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace quickSort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var o = new Solution();
+            int[] n = new int[]{3, 2, 1, 4, 5};
+            o.QuickSort(n, 0, n.Length-1);
+            Console.WriteLine("Quick Sort: {0}", string.Join(',', n));
+        }
+    }
+
+    public class Solution{
+        public void QuickSort(int[] A, int start, int end) {
+            if (start >= end) return;
+            int pos = Partition(A, start, end);
+            QuickSort(A, start, pos - 1);
+            QuickSort(A, pos, end);
+        }
+
+        int Partition(int[] A, int start, int end) {
+            Console.WriteLine("array: {0}", string.Join(',', A));
+            int i = start, j = end, pivot = A[end];
+            while (i <= j) {
+                while (A[i] < pivot) ++i;
+                while (A[j] > pivot) --j;
+                if (i <= j) {
+                    Swap(A, i, j);
+                    ++i; --j;
+                }
+            }            
+            return i-1;
+        }
+        void Swap(int[] A, int i, int j) {
+            int t = A[i];
+            A[i] = A[j];
+            A[j] = t;
+        }
+    }
+}
