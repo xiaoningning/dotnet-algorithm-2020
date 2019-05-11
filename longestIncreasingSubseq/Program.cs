33d872eecfb5bde13d3d48cc8ba42c2bc0df617d
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-05-10 23:20:10 -0700

    longest increasing subsequence  OJ

--- longestIncreasingSubseq/Program.cs
+++ longestIncreasingSubseq/Program.cs
@@ -1,6 +1,24 @@
 public class Solution {
     public int LengthOfLIS(int[] nums) {
         if (nums.Length == 0) return 0;
+        List<int> dp = new List<int>();
+        for (int i = 0; i < nums.Length; i++){
+            int left = 0, right = dp.Count();
+            while (left < right) {
+                int mid = left + (right - left) /2;
+                // binary search to find 
+                // 1st one >= nums[i]
+                if (dp[mid] < nums[i]) left = mid +1;
+                else right = mid;
+            }
+            if (right >= dp.Count()) dp.Add(nums[i]);
+            else dp[right] = nums[i];
+        }
+        // O(logn)
+        return dp.Count();
+    }
+    public int LengthOfLIS1(int[] nums) {
+        if (nums.Length == 0) return 0;
         List<int> ends = new List<int>(){nums[0]};
         foreach (var a in nums) {
             if (a < ends[0]) ends[0] = a;
