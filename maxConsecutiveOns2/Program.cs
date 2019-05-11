33534c50dd8479dc0b5bf0645a35b076bfb42cc9
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-05-11 00:01:15 -0700

    max consecutive ones 2 OJ

---
+++ maxConsecutiveOns2/Program.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int FindMaxConsecutiveOnes(int[] nums) {
+        int res = 0, sum = 0, cur = 0;
+        foreach (int n in nums) {
+            sum += n;
+            // cur: flip 0 sum
+            if (n == 0) cur = sum + 1;
+            sum *= n;
+            res = Math.Max(res, cur + sum);
+        }
+        return res;
+    }
+}
