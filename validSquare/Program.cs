ac8a6c42fc4c27cad54178bd8f53f85184414bce
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-05-14 07:50:01 -0700

    valid square OJ

---
+++ validSquare/Program.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public bool ValidSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
+        HashSet<int> s = new HashSet<int>() {
+            d(p1,p2),
+            d(p1,p3),
+            d(p1,p4),
+            d(p2,p3),
+            d(p2,p4),
+            d(p3,p4),
+        };
+        // edge value and diag value;
+        return !s.Contains(0) && s.Count() == 2;
+    }
+    int d (int[] a, int[] b) {
+        return (a[0] - b[0]) * (a[0] - b[0]) + (a[1] - b[1]) * (a[1] - b[1]);
+    }
+}
