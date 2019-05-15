bf055a892763f146c79c177837125ea3bdaea5f8
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-05-14 23:57:20 -0700

    max product of words with comments

--- maxProductWords/Program.cs
+++ maxProductWords/Program.cs
@@ -3,8 +3,10 @@ public class Solution {
         int[] masks = new int[words.Length];
         int res = 0;
         for (int i = 0; i < words.Length; i++) {
+            // bit 1 -> c-'a' in the word
             foreach (char c in words[i]) masks[i] |= 1 << c - 'a';
             for (int j = 0; j < i; j++) {
+                // no over lapping bit
                 if ((masks[i] & masks[j]) == 0)
                     res = Math.Max(res, words[i].Length * words[j].Length);
             }
