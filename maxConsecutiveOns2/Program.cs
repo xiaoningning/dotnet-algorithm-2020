5d3e98b8521b8a74c6d6121d442815f1a473f480
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-05-24 07:16:29 -0700

    max consecutive ones 2 comment

--- maxConsecutiveOns2/Program.cs
+++ maxConsecutiveOns2/Program.cs
@@ -3,7 +3,7 @@ public class Solution {
         int res = 0, sum = 0, cur = 0;
         foreach (int n in nums) {
             sum += n;
-            // cur: flip 0 tp 1 prev sum
+            // cur: flip 0 to 1 prev sum
             if (n == 0) cur = sum + 1;
             sum *= n;
             res = Math.Max(res, cur + sum);
