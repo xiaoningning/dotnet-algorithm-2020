1d30aa5af108624084475d43949b4606fb760575
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-05-24 07:25:52 -0700

    update max consecutive ones 3 OJ

--- maxConsetutiveOne3/Program.cs
+++ maxConsetutiveOne3/Program.cs
@@ -4,7 +4,7 @@ public class Solution {
         // slide windows
         for (j = 0; j < A.Length; ++j) {
             if (A[j] == 0) K--;
-            if (K < 0 && A[i++] == 0) K++;
+            whie (K < 0) if (A[i++] == 0) K++;
             res = Math.Max(res, j - (i -1));
         }
         return res;
