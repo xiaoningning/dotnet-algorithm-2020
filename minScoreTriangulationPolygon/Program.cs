aa80c77fb63189ddb43c88edea318d94c45b2856
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-05-25 04:13:43 -0700

    min score triangulation polygon with recursive OJ

--- minScoreTriangulationPolygon/Program.cs
+++ minScoreTriangulationPolygon/Program.cs
@@ -1,5 +1,25 @@
 public class Solution {
     public int MinScoreTriangulation(int[] A) {
+        var m = new Dictionary<string, int>();
+        return Helper(A, 0, A.Length -1, m);
+    }
+    
+    int Helper(int[] A, int s, int e, Dictionary<string, int> cache) {
+        if (e - s < 2) return 0;
+        string key = s + "," + e;
+        if (cache.ContainsKey(key)) return cache[key];
+        int res = Int32.MaxValue;
+        for (int m = s+1; m < e; m++) {
+            int sum = Helper(A, s, m, cache);
+            sum += A[s]*A[m]*A[e];
+            sum += Helper(A, m, e, cache);
+            res = Math.Min(res, sum);
+        }
+        cache.Add(key, res);
+        return res;
+    }
+    
+    public int MinScoreTriangulation1(int[] A) {
         int n = A.Length;
         int[,] dp = new int[n,n];
         // triangle
