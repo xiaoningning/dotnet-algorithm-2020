c49782ab5c1052a9b09faf83743d32ae6974ab32
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-05-28 06:16:28 -0700

    last stone weight 2 OJ

---
+++ lastStoneWeight2/Program.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int LastStoneWeightII(int[] stones) {
+        // dp to record the achievable sum of the smaller group
+        // 1 <= stones.length <= 30
+        // 1 <= stones[i] <= 100
+        // total max sum 3000, half is 1501
+        // s1 + s2 = sum
+        // s1 - s2 = diff
+        // => diff = sum - 2* s2
+        bool[] dp = new bool[1501];
+        dp[0] = true;
+        int sum = 0;
+        foreach (var s in stones) {
+            sum += s;
+            for (int i = 1500; i >= s; i--) {
+                dp[i] |= dp[i - s];
+            }
+        }
+        
+        for (int i = sum / 2; i >= 0; i--) {
+            if (dp[i]) return sum - 2 * i;
+        }
+        return 0;
+    }
+}
