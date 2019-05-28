9a24a815f0bd6c111c0532158d908dda49449a49
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-05-28 07:08:09 -0700

    last stone weight 2 with hashes OJ

--- lastStoneWeight2/Program.cs
+++ lastStoneWeight2/Program.cs
@@ -1,5 +1,23 @@
 public class Solution {
     public int LastStoneWeightII(int[] stones) {
+        // similar to target sum #494
+        var s = new HashSet<int>();
+        s.Add(stones[0]);
+        s.Add(-stones[0]);
+        for(int i=1; i<stones.Length; i++) {
+            var set2 = new HashSet<int>();
+            foreach (int item in s) {
+                set2.Add(item + stones[i]);
+                set2.Add(item - stones[i]);
+            }
+            s = set2;
+        }
+        int min = Int32.MaxValue;
+        foreach (int item in s) min = Math.Min(Math.Abs(item), min);
+        return min;
+    }
+    
+    public int LastStoneWeightII1(int[] stones) {
         // dp to record the achievable sum of the smaller group
         // 1 <= stones.length <= 30
         // 1 <= stones[i] <= 100
