c3dcbc0d7d277a81227da58d105e0148d17003c7
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-05-28 06:48:34 -0700

    target sum update OJ

--- TargetSum/Program.cs
+++ TargetSum/Program.cs
@@ -9,14 +9,33 @@ namespace TargetSum
         {
             int[] nums = new int[]{1,1,1,1,1};
             int S = 3;
+            var obj = new Solution();
             Console.WriteLine("Find the ways of target sum: {0}", string.Join(",", nums));
             Console.WriteLine("The target sum: {0}", S);
-            Console.WriteLine("the ways of target sum: {0}", FindTargetSumWays(nums, S));
+            Console.WriteLine("the ways of target sum: {0}", obj.FindTargetSumWays(nums, S));
         }
-        static int FindTargetSumWays(int[] nums, int S) {
+    }
+    
+    public class Solution {
+        public int FindTargetSumWays(int[] nums, int S) {
+            int res = 0;
+            Helper(nums, S, 0, ref res);
+            return res;
+        }
+
+        void Helper(int[] nums, int S, int start, ref int res) {
+            if (start == nums.Length) {
+                if (S == 0) res++;
+                return;
+            }
+            Helper(nums, S + nums[start], start+1, ref res);
+            Helper(nums, S - nums[start], start+1, ref res);
+        }
+
+        public int FindTargetSumWays1(int[] nums, int S) {
             int n = nums.Length;
+            // index of list is ith nums
             List<Dictionary<int, int>> dp = new List<Dictionary<int, int>>();
-            
             // dict: key -> sum, value -> total cnt to get sum
             dp.Add(new Dictionary<int, int>());
             dp[0].Add(0,1);
@@ -28,7 +47,7 @@ namespace TargetSum
                     dp[i + 1][sum - nums[i]] = dp[i+1].ContainsKey(sum - nums[i])? dp[i + 1][sum - nums[i]] + cnt : cnt;
                 }
             }
-            return dp[n].ContainsKey(S) ? dp[n][S] : 0;
+            return dp[n].ContainsKey(S) ? dp[n][S] : 0; 
         }
     }
 }
