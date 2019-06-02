7c999718126b1aed9424939fc36071c615f8ebfe
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-06-01 23:23:33 -0700

    reverse integer with comment

--- reverseInteger/Program.cs
+++ reverseInteger/Program.cs
@@ -2,8 +2,9 @@ public class Solution {
     public int Reverse(int x) {
         int res = 0;
         while (x != 0) {
-            // check if reverse overflow int range
             // another way: use long, after reverse and check overflow
+            // check if reverse overflow int range
+            // res > Int32.MaxValue / 10, res *10 + tail overflow
             if (Math.Abs(res) > Int32.MaxValue / 10) return 0;
             res = res * 10 + x % 10;
             x /= 10;
