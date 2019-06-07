f727acfdacccfe33ee403a9ae3571f0e26aa480a
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-06-07 01:03:55 -0700

    min cost to hire k workers OJ

---
+++ minCostHireKWorkers/Program.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public double MincostToHireWorkers(int[] q, int[] w, int K) {
+        var workers = new List<List<double>>();
+        for (int i = 0; i < q.Length; ++i)
+            workers.Add(new List<double>(){(double)(w[i]) / q[i], (double)q[i]});
+        workers.Sort((a, b) => a[0].CompareTo(b[0]));
+        double res = Double.MaxValue, qsum = 0;
+        var pq = new List<Double>();
+        foreach (var worker in workers) {
+            qsum += worker[1];
+            pq.Add(-worker[1]);
+            pq.Sort();
+            if (pq.Count > K) {
+                qsum += pq.First();
+                pq.RemoveAt(0);
+            }
+            if (pq.Count == K) res = Math.Min(res, qsum * worker[0]);
+        }
+        return res;
+    }
+}
