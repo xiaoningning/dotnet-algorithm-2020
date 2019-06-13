947e8e7f82de0ef7c4259117c3fbd3e0ad617f80
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-06-13 04:39:46 -0700

    max distance to closest person OJ

---
+++ maxDistanceToClosestPerson/Program.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int MaxDistToClosest(int[] seats) {
+        int res = 0, pre = 0, n = seats.Length;
+        // pre is previous 0
+        // j counts 0;
+        for (int j = 0; j < n; j++) {
+            if (seats[j] == 1) {
+                res = pre > 0 ? 
+                    Math.Max(res, (j - pre + 1) / 2) 
+                    : Math.Max(res, j - pre);
+                pre = j + 1;
+            }
+        }
+        // case: [1,0,0,0] seat at the last
+        res = Math.Max(res, n - pre);
+        return res;
+    }
+}
