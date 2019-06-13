d3f84f7f2fd931f7e49d6b93ca1f827630a9935e
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-06-13 02:47:30 -0700

    jump game OJ

---
+++ jumpGame/Program.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public bool CanJump(int[] nums) {
+        int n = nums.Length, reach = 0;
+        for (int i = 0; i < n; ++i) {
+            // if i > reach, it can not jump to i
+            // if reach >= n -1, it already jump to the last
+            if (i > reach || reach >= n - 1) break;
+            reach = max(reach, i + nums[i]);
+        }
+        return reach >= n - 1;
+    }
+}
