310f2e9f631ef88479fc2b9de0d178e9fc9542d1
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-06-19 22:20:03 -0700

    h-index 2 update

--- hIndex2/Program.cs
+++ hIndex2/Program.cs
@@ -15,11 +15,11 @@ namespace hIndex2
         public int HIndex(int[] citations) {
             int len = citations.Length;
             int left = 0;
-            int right = len - 1;
-            while(left <= right){
+            int right = len;
+            while(left < right){
                 int mid = left + (right -left)/2;
                 if(citations[mid] == (len-mid)) return citations[mid];
-                else if(citations[mid] > len - mid) right = mid - 1;
+                else if(citations[mid] > len - mid) right = mid;
                 else left = mid + 1;
             }
             return len - left;
