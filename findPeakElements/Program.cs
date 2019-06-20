25f2f2276e4d2d87319de2d6445a8b0f61f4ac7b
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-06-19 22:11:42 -0700

    find peak element  +comments

--- findPeakElements/Program.cs
+++ findPeakElements/Program.cs
@@ -1,5 +1,7 @@
 public class Solution {
     public int FindPeakElement(int[] nums) {
+        // right must be nums.Length - 1
+        // otherwise mid+1 will out of index
         int left = 0, right = nums.Length - 1;
         // binary search
         // just need to find one of peak elements
