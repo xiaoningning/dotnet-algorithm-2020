8b06e616324781e983717beb101f331c317a1a2a
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-06-26 07:26:23 -0700

    longest valid parentheses DP

--- longestValidParentheses/program.cs
+++ longestValidParentheses/program.cs
@@ -1,5 +1,19 @@
 public class Solution {
     public int LongestValidParentheses(string s) {
+        int res = 0, n = s.Length;
+        var dp = new int[n+1];
+        for (int i = 1; i <= n; i++) {
+            int j = i - 2 - dp[i - 1];
+            if (s[i-1] == '(' || j < 0 || s[j] == ')') 
+                dp[i] = 0;
+            else {
+                dp[i] = dp[i-1] + 2 + dp[j];
+                res = Math.Max(res, dp[i]);
+            }
+        }
+        return res;
+    }
+    public int LongestValidParentheses1(string s) {
         int res = 0, start = 0;
         var st = new Stack<int>();
         for (int i = 0; i < s.Length; i++) {
