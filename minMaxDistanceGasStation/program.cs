ebbe0a518768932fc9486154003034ed45cbd9a3
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-07-16 21:38:34 -0700

    minimize max distance to gas station OJ

---
+++ minMaxDistanceGasStation/program.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public double MinmaxGasDist(int[] stations, int K) {
+        int cnt, N = stations.Length;
+        double l = 0, r = stations[N - 1] - stations[0];
+        double mid;
+        while (l + (1e-6) < r) {
+            mid = l + (r - l) / 2;
+            cnt = 0;
+            for (int i = 0; i < N - 1; ++i) {
+                // calculate cnt of more gas station after distance is mid
+                cnt += (int)Math.Ceiling((stations[i + 1] - stations[i]) / mid) - 1;
+            }
+            if (cnt > K) l = mid; // mid is double, no need of +1 to move left side
+            else r = mid;
+        }
+        return r;
+    }
+}
