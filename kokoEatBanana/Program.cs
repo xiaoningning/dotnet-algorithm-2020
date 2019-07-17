b0767699c4449f2b45a792414a414321de6e4ed7
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-07-16 21:05:42 -0700

    koko eat banana update

--- kokoEatBanana/Program.cs
+++ kokoEatBanana/Program.cs
@@ -1,13 +1,16 @@
 public class Solution {
     public int MinEatingSpeed(int[] piles, int H) {
-        int left = 1, right = Int32.MaxValue;
-        // binary search to find
+        // eat speed: [1, max(piles)]
+        // or [1, Int32.MaxValue]
+        int left = 1, right = piles.Max();
+        // binary search
         while (left < right) {
             int mid = left + (right - left) / 2, cnt = 0;
             foreach (int pile in piles) cnt += pile % mid == 0 ? pile/mid : pile/mid + 1;
             if (cnt > H) left = mid + 1;
             else right = mid;
         }
+        // it can be max(piles) as res
         return right;
     }
 }
