6b1ef415d56e2dbea858dc9324357db8335a0965
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-07-17 23:53:09 -0700

    crack the safe update

--- crackTheSafe/program.cs
+++ crackTheSafe/program.cs
@@ -8,10 +8,12 @@ public class Solution {
         while (--t >= 0) res += "0";
         var visited = new HashSet<string>();
         visited.Add(res);
-        // permutation
         for (int i = 0; i < Math.Pow(k,n); i++) {
+            // last n-1 digits of string
             string pre = res.Substring(res.Length - (n - 1), n - 1);
+            // permutation
             for (int j = k - 1; j >= 0; j--) {
+                // cur is n digits of pwd
                 string cur = pre + j;
                 if (!visited.Contains(cur)){
                     visited.Add(cur);
