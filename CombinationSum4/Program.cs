ef3aa4d8e89cb7ff9662ee50154c8d18a064c51b
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-07-25 23:01:05 -0700

    combination sum 4 update

--- CombinationSum4/Program.cs
+++ CombinationSum4/Program.cs
@@ -21,4 +21,27 @@ namespace CombinationSum4
             return dp[target];
         }
     }
+    public class Solution {
+        // bottom up
+        public int CombinationSum4(int[] nums, int target) {
+            int[] dp = new int[target+1];
+            dp[0] = 1;
+            for(int i = 1; i <= target; i++){
+                for(int j = 0; j < nums.Length; j++){
+                    if (i >= nums[j]) dp[i] += dp[i - nums[j]];
+                }
+            }
+            return dp[target];
+        }
+        // top down
+        public int CombinationSum4_1(int[] nums, int target) {
+            if (target == 0) return 1;
+            int res = 0;
+            for(int  j= 0; j < nums.Length; j++){
+                if (target >= nums[j])
+                    res += CombinationSum4_1(nums, target - nums[j]);
+            }
+            return res;
+        }
+    }
 }
