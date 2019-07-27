10e7146182c742ec2427691268b52b5683051491
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-07-26 23:45:06 -0700

    find kth largest element update quick sort

--- findKthLargest/Program.cs
+++ findKthLargest/Program.cs
@@ -9,10 +9,12 @@ namespace findKthLargest
             Console.WriteLine("int array: {0}", args[0]);
             int k = int.Parse(args[1]);
             int[] nums = Array.ConvertAll(args[0].Split(','), s => int.Parse(s)); 
-            Console.WriteLine("result for {0}: {1}", k, FindKthLargest(nums, k));
+            var o = new Solution();
+            Console.WriteLine("result for {0}: {1}", k, o.FindKthLargest(nums, k));
         }
-
-        static int FindKthLargest(int[] nums, int k) {
+    }
+    public class Solution {
+        public int FindKthLargest(int[] nums, int k) {
             int left = 0, right = nums.Length - 1;
             while (true) {
                 int pos = Partition(nums, left, right);
@@ -22,22 +24,33 @@ namespace findKthLargest
             }
         }
 
-        static int Partition(int[] arr, int left, int right){
-            int pivot = arr[left], l = left + 1, r = right;
+        int Partition1(int[] nums, int left, int right){
+            int pivot = nums[right], l = left, r = right;
             while (l <= r) {
-                if (arr[l] < pivot && arr[r] > pivot) {
-                    swapArray(arr, l, r);
+                while (nums[l] > pivot) l++;
+                while (nums[r] < pivot) r--;
+                if (l <= r) {
+                    Swap(nums, l, r);
                     l++;
                     r--;
                 }
-                if (arr[l] >= pivot) l++;
-                if (arr[r] <= pivot) r--;
             }
-            swapArray(arr, left, r);
-            return r;
+            return l;
         }
 
-        static void swapArray(int[] arr, int i, int j){
+        int Partition(int[] A, int start, int end) {            
+            int i = start - 1, pivot = A[end];
+            for (int j = start; j <= end; ++j) {
+                if (A[j] >= pivot) {
+                    // i need to be start - 1 for the first time
+                    ++i; 
+                    Swap(A, i, j);
+                }
+            }            
+            return i;
+        }
+
+        void Swap(int[] arr, int i, int j){
             int t = arr[i];
             arr[i] = arr[j];
             arr[j] = t;
