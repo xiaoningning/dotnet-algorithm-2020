42b0cc9f8379238bc2fbb337879ee609d30ff08e
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-07-29 05:24:31 -0700

    missing number update

--- missingNumber/Program.cs
+++ missingNumber/Program.cs
@@ -12,16 +12,37 @@ namespace missingNumber
         }
     }
     public class Solution {
-        public int MissingNumber(int[] nums) {
+        public int MissingNumber1(int[] nums) {
             int sum = 0;
             int n = nums.Length;
             foreach(int i in nums){
                 sum += i;
-            }
+            }        
             for(int i = 1; i <= n; i++){
                 sum -= i;
             }
             return -sum;
+        }
+
+        public int MissingNumber2(int[] nums) {
+            Array.Sort(nums);
+            int left = 0, right = nums.Length;
+            while (left < right){
+                int mid = left + (right - left) / 2;
+                if (nums[mid] > mid) right = mid;
+                else left = mid+1;
+            }
+            return left;
+        }
+
+        public int MissingNumber(int[] nums) {
+            // only miss one
+            int xor = nums.Length;
+            for (int i = 0; i < nums.Length; i++) {
+                xor ^= i;
+                xor ^= nums[i];
+            }
+            return xor;
         }
     }
 }
