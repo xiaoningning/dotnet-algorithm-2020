88df0715dae6d1c7fc2b460250e70e46e45431cd
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-08-27 02:49:32 -0700

    remove outmost parentheses with update

--- removeOuterMostParantheses/program.cs
+++ removeOuterMostParantheses/program.cs
@@ -4,6 +4,12 @@ public class Solution {
         //Primitive string will have equal number of opened and closed parenthesis.
         int opened = 0;
         foreach (char c in S.ToCharArray()) {
+            /*
+            opened count the number of opened parenthesis.
+            Add every char to the result,
+            unless the first left parenthesis,
+            and the last right parenthesis.
+            */
             if (c == '(' && opened++ > 0) s.Append(c);
             if (c == ')' && opened-- > 1) s.Append(c);
         }
