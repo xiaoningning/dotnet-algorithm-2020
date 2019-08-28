a766cee3762ccdc60a30532d63dcb63379c1a6a6
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-08-28 02:17:24 -0700

    search 2d matrix II with update

--- search2dMatrixII/program.cs
+++ search2dMatrixII/program.cs
@@ -4,11 +4,10 @@ public class Solution {
         int m = matrix.GetLength(0), n = matrix.GetLength(1);
         if (target < matrix[0,0] || target > matrix[m-1,n-1]) return false;
         int x = matrix.GetLength(0) - 1, y = 0;
-        while (true) {
+        while (x >= 0 && y < matrix.GetLength(1)) {
             if (matrix[x,y] > target) --x;
             else if (matrix[x,y] < target) ++y;
             else return true;
-            if (x < 0 || y >= matrix.GetLength(1)) break;
         }
         return false;
     }
