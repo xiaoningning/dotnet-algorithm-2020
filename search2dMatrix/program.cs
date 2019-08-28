35d9a3a0e026ea21b1a58b9ef571be9fad05c64f
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-08-28 02:09:05 -0700

    search 2D matrix OJ

---
+++ search2dMatrix/program.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public bool SearchMatrix(int[][] matrix, int target) {
+        if (matrix.Length == 0) return false;
+        int m = matrix.GetLength(0), n = matrix[0].Length;
+        if (n == 0 || target < matrix[0][0] || target > matrix[m-1][n-1]) return false;
+        int left = 0, right = m*n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (matrix[mid / n][mid % n] == target) return true;
+            else if (matrix[mid / n][mid % n] < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return false;
+    }
+}
