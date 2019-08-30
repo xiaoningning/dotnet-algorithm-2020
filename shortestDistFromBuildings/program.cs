5dba1fcf34a6b3cf4901666050ed7af272f0bd05
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-08-30 08:35:16 -0700

    shortest distance from all buildings ~oj

--- shortestDistFromBuildings/program.cs
+++ shortestDistFromBuildings/program.cs
@@ -6,32 +6,43 @@ public class Solution {
         dirs[1] = new int[] {1,0};
         dirs[2] = new int[] {0,-1};
         dirs[3] = new int[] {-1,0};
-        int[,] sum = new int[m,n];
+        int[,] dist = new int[m,n];
+        int[,] cnt = new int[m,n];
+        int buildingCnt = 0;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (grid[i][j] == 1) {
-                    res = Int32.MaxValue;
-                    int[,] dist = new int[m,n];
+                    buildingCnt++;
                     bool[,] visited = new bool[m,n];
                     var q = new Queue<int[]>();
                     q.Enqueue(new int[2]{i,j});
+                    int level = 1;
                     while (q.Count != 0) {
-                        var t = q.Dequeue();
-                        int a = t[0], b = t[1];
-                        for (int k = 0; k < dirs.GetLength(0); ++k) {
-                            int x = a + dirs[k][0], y = b + dirs[k][1];
-                            if (x >= 0 && x < m 
-                                && y >= 0 && y < n 
-                                && grid[x][y] == 0
-                                && !visited[x,y]) {
-                                visited[x,y] = true;
-                                dist[x,y] = dist[a,b] + 1;
-                                sum[x,y] += dist[x,y];
-                                q.Enqueue(new int[2]{x, y});
-                                res = Math.Min(res, sum[x,y]);
+                        for (int s = 0; s < q.Count; ++s) {
+                            var t = q.Dequeue();
+                            int a = t[0], b = t[1];
+                            for (int k = 0; k < dirs.GetLength(0); ++k) {
+                                int x = a + dirs[k][0], y = b + dirs[k][1];
+                                if (x >= 0 && x < m 
+                                    && y >= 0 && y < n 
+                                    && grid[x][y] == 0
+                                    && !visited[x,y]) {
+                                    visited[x,y] = true;
+                                    dist[x,y] += level;
+                                    cnt[x,y]++;
+                                    q.Enqueue(new int[2]{x, y});
+                                }
                             }
                         }
+                        level++;
                     }
+                }
+            }
+        }
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 0 && cnt[i,j] == buildingCnt) {
+                    res = Math.Min(res, dist[i,j]);
                 }
             }
         }
