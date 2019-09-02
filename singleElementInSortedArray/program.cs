350aa62aa250f92e7eb16ef1b41c8060657f6045
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-09-02 06:44:43 -0700

    single element in sorted array update

--- singleElementInSortedArray/program.cs
+++ singleElementInSortedArray/program.cs
@@ -4,6 +4,15 @@ public class Solution {
         while (lef < right) {
             int mid = left + (right - left) / 2;
             // find the same element is left or right of mid
+            /*
+            1 2 2 3 3 
+            1 1 2 2 3 
+            5 / 2  = 2
+
+            1 1 2 2 3 3 4
+            1 1 2 3 3 4 4
+            6 / 2 = 3
+            */
             if (mid % 2 == 1) --mid;
             if (nums[mid] == nums[mid + 1]) left = mid + 2;
             else right = mid;
