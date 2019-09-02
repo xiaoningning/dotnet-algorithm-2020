066bc765f0f8736f9b16a46c36d0cb880d3ddab6
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-09-02 07:21:41 -0700

    min moves to make equal in array update

--- minMovesToEqualInArray/program.cs
+++ minMovesToEqualInArray/program.cs
@@ -1,6 +1,9 @@
 public class Solution {
     public int MinMoves(int[] nums) {
         int res = 0, min = Int32.MaxValue;
+        // a move is incrementing n - 1 elements by 1
+        // = a move is decreasing max value element by 1
+        // use max could overflow
         foreach (var n in nums) min = Math.Min(min, n);
         foreach (var n in nums) res += n - min;
         return res;
