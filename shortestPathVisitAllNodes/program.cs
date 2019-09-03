9ec8c224585944d4cf0d0851d11739e57b15a1e4
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-09-03 12:17:42 -0700

    shortest pathes to visit all nodes

---
+++ shortestPathVisitAllNodes/program.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public int ShortestPathLength(int[][] graph) {
+        int n = graph.Length, target = 0, res = 0;
+        var visited = new HashSet<string>();
+        var q = new Queue<int[]>();
+        for (int i = 0; i < n; i++) {
+            int mask = 1 << i;
+            target |= mask;
+            visited.Add(mask.ToString() + "-" + i.ToString());
+            q.Enqueue (new int[]{mask, i});
+        }
+        while (q.Count != 0) {
+            // BFS for each level
+            int size = q.Count;
+            for (int i = 0; i < size; i++) {
+                var cur = q.Dequeue();
+                // path is edge, not node.
+                if (cur[0] == target) return res;
+                foreach (int next in graph[cur[1]]) {
+                    int nMask = cur[0] | 1 << next;
+                    string str = nMask.ToString() + "-" + next.ToString();
+                    if (visited.Contains(str)) continue;
+                    visited.Add(str);
+                    q.Enqueue(new int[]{nMask, next});
+                }
+            }
+            // path cnt = the cnt of edges, not nodes.
+            res++;
+        }
+        return -1;
+    }
+}
