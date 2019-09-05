1b12c51bbdb5c83ccfdf9bdc5577963c883c5f20
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-09-04 23:57:44 -0700

    Capacity To Ship Packages Within D Days OJ

---
+++ capacityToShipPkgWithinDdays/program.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int ShipWithinDays(int[] weights, int D) {
+        // int mn = weights.Max(), mx = weights.Sum();
+        int mn = 0, mx = 0;
+        foreach (int w in weights) {
+            mn = Math.Max(mn, w);
+            mx += w;
+        }
+        
+        while (mn < mx) {
+            int mid = mn + (mx - mn) / 2;
+            int need = 1, cur = 0;
+            foreach (var w in weights) {
+                if (cur + w > mid) {
+                    need += 1; 
+                    cur = 0;
+                }
+                cur += w;
+            }
+            if (need > D) mn = mid + 1;
+            else mx = mid;
+        }
+        return mn;
+    }
+}
