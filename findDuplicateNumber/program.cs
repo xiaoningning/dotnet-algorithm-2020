fb0d18d4bc39f0fe0e272e0f72533846be78abd8
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-09-07 12:42:52 -0700

    find the duplicate number OJ

---
+++ findDuplicateNumber/program.cs
@@ -0,0 +1,33 @@
+public class Solution {
+    public int FindDuplicate1(int[] nums) {
+        // 1 <= nums[i] <= nums.Length - 1
+        int left = 1, right = nums.Length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2, cnt = 0;
+            foreach (int n in nums) {
+                if (n <= mid) cnt++;
+            }
+            if (cnt <= mid) left = mid + 1;
+            else right = mid;
+        }
+        // O(nlogn)
+        return right;
+    }
+    
+    public int FindDuplicate(int[] nums) { 
+        // it is a cycled list b/c of duplicates
+        int slow = 0, fast = 0, t = 0;
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if (slow == fast) break;
+        }
+        while (true) {
+            slow = nums[slow];
+            t = nums[t];
+            if (slow == t) break;
+        }
+        // O(n)
+        return slow;
+    }
+}
