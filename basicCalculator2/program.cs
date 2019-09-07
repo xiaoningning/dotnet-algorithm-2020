bd5c70eac6cbd2d9c8c7568d90458d0194bef5b9
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-09-07 15:48:11 -0700

    basic calculator 2 OJ

---
+++ basicCalculator2/program.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public int Calculate(string s) {
+        int res = 0, curRes = 0, num = 0, n = s.Length;
+        char op = '+';
+        for (int i = 0; i < n; i++) {
+            char c = s[i];
+            if (c >= '0' && c <= '9') {
+                num = 10 * num + (c - '0');
+            }
+            
+            if (c == '+' || c == '-' || c == '*' || c == '/' || i == n - 1) {
+                switch (op) {
+                    case '+': curRes += num; break;
+                    case '-': curRes -= num; break;
+                    case '*': curRes *= num; break;
+                    case '/': curRes /= num; break;
+                }
+                if (c == '+' || c == '-' || i == n - 1) {
+                    res += curRes;
+                    curRes = 0;
+                }
+                num = 0;
+                op = c;
+            }
+        }
+        return res;
+    }
+}
