243dc7b69c6bbf957cb6a8cd07f6b0953952a2fa
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-09-07 13:27:44 -0700

    asteroid collision OJ

---
+++ asteroidCollision/program.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int[] AsteroidCollision(int[] asteroids) {
+        var res = new List<int>();
+        for (int i = 0; i < asteroids.Length; i++) {
+            if (asteroids[i] > 0) res.Add(asteroids[i]);
+            else if (!res.Any() || res.Last() < 0) res.Add(asteroids[i]);
+            // asteroids[i] < 0 
+            else if (res.Last() <= -asteroids[i]) {
+                if (res.Last() < -asteroids[i]) i--;
+                res.RemoveAt(res.Count - 1);
+            }
+            // if res.Last() > --asteroids[i], move next
+        }
+        return res.ToArray();
+    }
+}
