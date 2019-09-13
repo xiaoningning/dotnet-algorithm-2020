0d1e2c86b5ece14be1c7c0f1498d1baabfa438fa
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-09-13 12:51:49 -0700

    missing range overflow & empty case

--- missingRanges/Program.cs
+++ missingRanges/Program.cs
@@ -14,15 +14,19 @@ namespace missingRanges
         public IList<string> FindMissingRanges(int[] nums, int lower, int upper) {
             List<string> res = new List<string>();
 
-            int l = lower;
-            for (int i = 0; i <= nums.Length; ++i) {
-                int r = i < nums.Length ? nums[i] : upper + 1;
-                if (r > l) {
-                    res.Add(r - l == 1 ? l.ToString() : l.ToString() + "->" + (r - 1).ToString()); 
+                int l = lower;
+                for (int i = 0; i < nums.Length; ++i) {
+                    // int r = (i < nums.Length && nums[i] <= upper) ? nums[i] : upper + 1;
+                    int r = nums[i];
+                    if (r > l) {
+                        res.Add(r - l == 1 ? l.ToString() : l.ToString() + "->" + (r - 1).ToString()); 
+                    }
+                    // over flow case
+                    l = r == Int32.MaxValue ? r : r + 1;
                 }
-                l = r + 1;
-            }
-            return res;
+                // do a final check empty array case
+                if (l <= upper && upper != nums[nums.Length - 1]) res.Add(l == upper ? l.ToString() : l.ToString() + "->" + upper.ToString());
+                return res;
         }
-    }
+    }  
 }
