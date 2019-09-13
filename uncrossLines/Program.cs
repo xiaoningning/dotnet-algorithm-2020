92573cc056ed2efa754dee31004b5bfb4991119b
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-09-13 14:42:47 -0700

    uncross lines update

--- uncrossLines/Program.cs
+++ uncrossLines/Program.cs
@@ -4,6 +4,10 @@ public class Solution {
         int[,] dp = new int[n+1,m+1];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
+                /*
+                if (A[i] == B[j]) dp[i+1, j+1] = dp[i, j] + 1;
+                else dp[i+1, j+1] = Math.Max(dp[i+1, j], dp[i, j+1]);
+                */
                 dp[i+1, j+1] = Math.Max(dp[i+1, j], dp[i, j+1]);
                 if (A[i] == B[j]) dp[i+1, j+1] = Math.Max(dp[i+1, j+1], dp[i, j] + 1);
             }
