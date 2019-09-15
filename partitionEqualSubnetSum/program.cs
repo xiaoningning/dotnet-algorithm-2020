17b24847db012413ad12561cbddd88ff6ed2a84f
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-09-15 13:07:03 -0700

    partition equal subset sum update

--- partitionEqualSubnetSum/program.cs
+++ partitionEqualSubnetSum/program.cs
@@ -6,6 +6,10 @@ public class Solution {
         dp[0] = true;
         foreach (int num in nums) {
             // must from target to num, not reverse order
+            /* if num == 1, from num to target, 
+              then all dp is true. 
+              It makes dp invalid for this case
+            */
             // update if sum exists in DP
             for (int i = target; i >= num; --i) {
                 dp[i] = dp[i] || dp[i - num];
