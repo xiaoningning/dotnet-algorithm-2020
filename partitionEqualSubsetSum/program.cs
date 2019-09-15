654a492381c96d2a50cdb6209272ce2c363946a8
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-09-15 13:06:21 -0700

    partition equal subset sum OJ

---
+++ partitionEqualSubsetSum/program.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public bool CanPartition(int[] nums) {
+        int sum = nums.Sum(), target = sum / 2;
+        if (sum % 2 != 0) return false;
+        var dp = new bool[target + 1];
+        dp[0] = true;
+        foreach (int num in nums) {
+            // must from target to num, not reverse order
+            /* if num == 1, from num to target, 
+              then all dp is true. 
+              It makes dp invalid for this case
+            */
+            // update if sum exists in DP
+            for (int i = target; i >= num; --i) {
+                dp[i] = dp[i] || dp[i - num];
+            }
+        }
+        return dp[target];
+    }
+}
