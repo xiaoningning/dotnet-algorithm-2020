e8eff6642123d0f5320970a69eb34d88346e0ca5
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-09-16 07:32:18 -0700

    smallest range 2 update

--- smallestRange2/program.cs
+++ smallestRange2/program.cs
@@ -6,6 +6,7 @@ public class Solution {
         int left = A[0] + K, right = A[n-1] - K;
         for (int i = 0; i < n - 1; ++i)  {
             // find i in which i, i+1 has smallest range
+            // since it is sorted. the smallest range must be i and i+1
             int high = Math.Max(right, A[i] + K);
             int low = Math.Min(left, A[i + 1] - K);
             res = Math.Min(res, high - low);
