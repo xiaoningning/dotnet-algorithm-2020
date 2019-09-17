ecfcb3c426f3693dfdc52a09628485d67d32d961
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-09-16 23:32:08 -0700

    sum subsequence width update

--- sumSubsequenceWidth/program.cs
+++ sumSubsequenceWidth/program.cs
@@ -1,10 +1,24 @@
 public class Solution {
-    public int SumSubseqWidths(int[] A) {
+    public int SumSubseqWidths1(int[] A) {
         Array.Sort(A);
         long res = 0;
         long mod = (long)(Math.Pow(10,9)) + 7, c = 1; 
         for (int i = 0; i < A.Length; ++i, c = (c << 1) % mod)
             res = (res + A[i] * c - A[A.Length - i - 1] * c) % mod;
         return (int)(res % mod);
+    }
+    
+    public int SumSubseqWidths(int[] A) {
+        Array.Sort(A);
+        long res = 0;
+        long mod = (long)(Math.Pow(10,9)) + 7, c = 1; 
+        int leftSum = 0, rightSum = 0, left = 0, right = A.Length -1;
+        while (left < A.Length) {
+            leftSum += A[left++];
+            rightSum += A[right--];
+            res = (res + (rightSum - leftSum) * c) % mod;
+            c = (c << 1) % mod;
+        }
+        return (int)(res);
     }
 }
