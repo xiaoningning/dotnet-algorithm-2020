d6e8ff671b7bb957a1908f4cdc3bda9ba689bfd5
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-09-22 13:25:49 -0700

    repeated string match OJ

--- repeatedStringMatch/Program.cs
+++ repeatedStringMatch/Program.cs
@@ -4,9 +4,15 @@ public class Solution {
         for (int i = 0; i < m; i++) {
             int j = 0;
             while (j < n && A[(i+j) % m] == B[j]) j++;
-            // res : i + (j - 1) b/c j == n
+            // res: i + (j - 1) b/c j == n
             if (j == n) return ((i + j - 1) / m) + 1;
         }
+        return -1;
+    }
+    public int RepeatedStringMatch1(string A, string B) { 
+        string str = A;
+        for (int rep = 1; rep <= B.Length / A.Length + 2; rep++, str += A)
+            if (str.IndexOf(B) != -1) return rep;
         return -1;
     }
 }
