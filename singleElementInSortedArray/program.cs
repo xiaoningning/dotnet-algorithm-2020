71c1a17bc2751815e40a22e2c359f175ecbd913f
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-09-22 17:41:06 -0700

    Single Element in a Sorted Array update

--- singleElementInSortedArray/program.cs
+++ singleElementInSortedArray/program.cs
@@ -1,18 +1,18 @@
 public class Solution {
     public int SingleNonDuplicate(int[] nums) {
-        int left = 0, right = nums.size() - 1, n = nums.Length;
-        while (lef < right) {
+        int left = 0, right = nums.Length - 1, n = nums.Length;
+        while (left < right) {
             int mid = left + (right - left) / 2;
             // nums.Length is always odd
             // find the same element is left or right of mid
             /*
             1 2 2 3 3 
             1 1 2 2 3 
-            5 / 2  = 2
+            (4-0) / 2  = 2
 
             1 1 2 2 3 3 4
             1 1 2 3 3 4 4
-            7 / 2 = 3
+            (6-0) / 2 = 3
             */
             if (mid % 2 == 1) --mid;
             if (nums[mid] == nums[mid + 1]) left = mid + 2;
