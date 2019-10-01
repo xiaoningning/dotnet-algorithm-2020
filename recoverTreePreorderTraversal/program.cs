e2da987b1c7f16d7c7b46a36c785db3b22b69afe
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-10-01 00:57:32 -0700

    recover a tree from preorder traversal update

--- recoverTreePreorderTraversal/program.cs
+++ recoverTreePreorderTraversal/program.cs
@@ -9,6 +9,24 @@
  */
 public class Solution {
     public TreeNode RecoverFromPreorder(string S) {
+        int i = 0;
+        return helper(S, ref i, 0);
+    }
+    TreeNode helper(string S, ref int cur, int level){
+        int cnt = 0, n = S.Length, val = 0;
+        while (cur + cnt < n && S[cur + cnt] == '-') ++cnt;
+        if (cnt != level) return null;
+        cur += cnt;
+        for (; cur < n && S[cur] != '-'; ++cur) {
+            val = 10 * val + (S[cur] - '0');
+        }
+        TreeNode node = new TreeNode(val);
+        node.left = helper(S, ref cur, level + 1);
+        node.right = helper(S, ref cur, level + 1);
+        return node;
+    }
+    
+    public TreeNode RecoverFromPreorder1(string S) {
         var st = new Stack<TreeNode>();
         int i = 0, level = 0, val = 0, n = S.Length;
         while (i < n) {
