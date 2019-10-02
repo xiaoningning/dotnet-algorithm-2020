af7546257050d7ece3e658d513442479a825718e
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-10-02 16:27:02 -0700

    water and jug problem update

--- waterAndJug/program.cs
+++ waterAndJug/program.cs
@@ -1,5 +1,7 @@
 public class Solution {
     public bool CanMeasureWater(int x, int y, int z) {
+        // z = m * x + n * y
+        // z = k * d, d = gcd(x,y)
         return z == 0 || (x + y >= z && z % gcd(x, y) == 0);
     }
     
