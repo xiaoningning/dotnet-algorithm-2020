067ef574cbc853d44cf7ca2e7757a47b159fc374
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-10-02 16:31:51 -0700

    water and jug update example

--- waterAndJug/program.cs
+++ waterAndJug/program.cs
@@ -2,6 +2,8 @@ public class Solution {
     public bool CanMeasureWater(int x, int y, int z) {
         // z = m * x + n * y
         // z = k * d, d = gcd(x,y)
+        // x = 3, y = 5, z = 4
+        // 2 * 5 + (-2) * 3 = 4
         return z == 0 || (x + y >= z && z % gcd(x, y) == 0);
     }
     
