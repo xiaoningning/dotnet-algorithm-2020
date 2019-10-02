8728bb019dbee3e94295a2fd4de6bdf24c5606b0
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-10-02 16:19:12 -0700

    X of Kind in Deck of Cards OJ update

--- xKindInDeckCards/program.cs
+++ xKindInDeckCards/program.cs
@@ -1,5 +1,5 @@
 public class Solution {
-    public bool HasGroupsSizeX(int[] deck) {
+    public bool HasGroupsSizeX1(int[] deck) {
         var cardCnt = new Dictionary<int, int>();
         foreach (int card in deck) {
             if (!cardCnt.ContainsKey(card)) cardCnt.Add(card,0);
@@ -19,5 +19,20 @@ public class Solution {
             if (success) return true;
         }
         return false;
+    }
+    
+    public bool HasGroupsSizeX(int[] deck) {
+        var cardCnt = new Dictionary<int, int>();
+        foreach (int card in deck) {
+            if (!cardCnt.ContainsKey(card)) cardCnt.Add(card,0);
+            ++cardCnt[card];
+        }
+        int res = 0;
+        foreach (var a in cardCnt) res = gcd(a.Value, res);        
+        return res > 1;
+    }
+    
+    int gcd(int a, int b) {
+        return a == 0 ? b : gcd(b % a, a);
     }
 }
