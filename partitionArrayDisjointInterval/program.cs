8e84a369e8938e6713e0128d45f10a2f85355c33
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-10-04 09:54:49 -0700

    Partition Array into Disjoint Intervals OJ

---
+++ partitionArrayDisjointInterval/program.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int PartitionDisjoint(int[] A) {
+        int partitionIdx = 0, curMax = A[0], preMax = curMax;
+        for (int i = 1; i < A.Length; i++){
+            curMax = Math.Max(curMax, A[i]);
+            if (A[i] < preMax) {
+                preMax = curMax;
+                partitionIdx = i;
+            }
+        }
+        return partitionIdx + 1;
+    }
+    
+    public int PartitionDisjoint1(int[] A) {
+        int n = A.Length, curMax = Int32.MinValue;
+        int[] backMin = new int[n];        
+        Array.Fill(backMin,A.Last());
+        for (int i = n - 2; i >= 0; i--) {
+            backMin[i] = Math.Min(backMin[i+1], A[i]);
+        }
+        for (int i = 0; i < n - 1; i++){
+            curMax = Math.Max(curMax, A[i]);
+            if (curMax <= backMin[i+1]) return i + 1;
+        }
+        return -1;
+    }
+}
