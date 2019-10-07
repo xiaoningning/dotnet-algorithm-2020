110c74480a6fadade14b248d2c01eb02616c2611
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-10-06 23:40:56 -0700

    word break update

--- wordBreak/Program.cs
+++ wordBreak/Program.cs
@@ -33,7 +33,7 @@ namespace wordBreak
         }
         public bool WordBreak1(string s, IList<string> wordDict) {
             int n = s.Length;
-            // res[i] is breakable
+            // res[i] is breakable of length i
             bool[] res = new bool[n+1];
             res[0] = true;
             for(int i = 1; i <=n; i++){
