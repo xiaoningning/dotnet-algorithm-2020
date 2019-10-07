309c688e3232a822d72ec32d319e2ee7794594af
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-10-07 00:02:57 -0700

    clone graph Update OJ

--- cloneGraph/Program.cs
+++ cloneGraph/Program.cs
@@ -1,43 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public IList<Node> neighbors;
 
-namespace cloneGraph
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var obj = new Solution();
-            var root = new UndirectedGraphNode(0);
-            root.neighbors.Add(new UndirectedGraphNode(1));
-            root.neighbors.Add(new UndirectedGraphNode(2));
-            var ng = obj.CloneGraph(root);
-            Console.WriteLine("clone graph");
-        }
+    public Node(){}
+    public Node(int _val,IList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+}
+*/
+public class Solution {
+    public Node CloneGraph(Node node) {
+        // record who has been cloned.
+        var map = new Dictionary<int, Node>();
+        return Clone(node, map);
     }
-    /**
-    * Definition for undirected graph.
-    */
-    public class UndirectedGraphNode {
-        public int label;
-        public IList<UndirectedGraphNode> neighbors;
-        public UndirectedGraphNode(int x) { label = x; neighbors = new List<UndirectedGraphNode>(); }
-    }
-    public class Solution {
-        public UndirectedGraphNode CloneGraph(UndirectedGraphNode node) {
-            // record who has been cloned.
-            Dictionary<int, UndirectedGraphNode> map = new Dictionary<int, UndirectedGraphNode>();
-            return Clone(node, map);
-        }
-        UndirectedGraphNode Clone(UndirectedGraphNode node, Dictionary<int, UndirectedGraphNode> map){
-            if(node == null) return node;
-            if(map.ContainsKey(node.label)) return map[node.label];
-            UndirectedGraphNode newNode = new UndirectedGraphNode(node.label);
-            map[node.label] = newNode;
-            foreach(var n in node.neighbors){
-                newNode.neighbors.Add(Clone(n, map));
-            }        
-            return newNode;
-        }
+    Node Clone(Node node, Dictionary<int, Node> map){
+        if(node == null) return node;
+        if(map.ContainsKey(node.val)) return map[node.val];
+        Node newNode = new Node(node.val, new List<Node>());
+        map[node.val] = newNode;
+        foreach(var n in node.neighbors){
+            newNode.neighbors.Add(Clone(n, map));
+        }        
+        return newNode;
     }
 }
