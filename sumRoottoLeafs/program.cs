f7315d7daf55f3c0407104252d604219eabd8afe
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-10-07 00:53:00 -0700

    Sum Root to Leaf Numbers OJ

---
+++ sumRoottoLeafs/program.cs
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    
+    public int SumNumbers(TreeNode root) {
+        return sum(root, 0);
+    }
+    int sum(TreeNode n, int s){
+        if (n == null) return 0;
+        // 0 <= val <= 9
+        if (n.right == null && n.left == null) return s*10 + n.val;
+        return sum(n.left, s*10 + n.val) + sum(n.right, s*10 + n.val);
+    }
+}
