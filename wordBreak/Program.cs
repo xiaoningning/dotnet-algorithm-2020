d09eceae2f12a37febad4549b39c6a1fda7a0c8d
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-10-06 23:34:17 -0700

    word break update

--- wordBreak/Program.cs
+++ wordBreak/Program.cs
@@ -14,6 +14,24 @@ namespace wordBreak
     }
     public class Solution {
         public bool WordBreak(string s, IList<string> wordDict) {
+            var visited = new int[s.Length];
+            Array.Fill(visited, -1);
+            return helper(s, 0, wordDict, visited);
+        }
+        bool helper(string s, int start, IList<string> dict, int[] visited) {
+            if (start >= s.Length) return true;
+            if (visited[start] != -1) return visited[start] == 1;
+            for (int i = start + 1; i <= s.Length; i++) {
+                if (dict.Contains(s.Substring(start, i - start) )
+                    && helper(s, i, dict, visited)) {
+                        visited[start] = 1;
+                        return true;
+                    }
+            }
+            visited[start] = 0;
+            return false;
+        }
+        public bool WordBreak1(string s, IList<string> wordDict) {
             int n = s.Length;
             bool[] res = new bool[n+1];
             res[0] = true;
