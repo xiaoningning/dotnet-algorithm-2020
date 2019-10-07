13e8ff16a3a661a97d5716757ee67b01a6b7f222
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-10-06 23:50:57 -0700

    word break 2 update

--- wordBreak2/Program.cs
+++ wordBreak2/Program.cs
@@ -14,8 +14,22 @@ namespace wordBreak2
         }
     }
     public class Solution {
+        Dictionary<string, List<string>> m = new Dictionary<string, List<string>>();
         public IList<string> WordBreak(string s, IList<string> wordDict) {
-            Dictionary<string, List<string>> map = new  Dictionary<string, List<string>>();
+            if (!s.Any()) return new List<string>(){""};
+            if (m.ContainsKey(s)) return m[s];
+            var res = new List<string>();
+            foreach (string w in wordDict) {
+                if (!s.StartsWith(w)) continue;
+                var remains = WordBreak(s.Substring(w.Length), wordDict);
+                foreach (var r in remains) 
+                    res.Add(w + (r.Length == 0 ? "" : " ") + r);  
+            }
+            m[s] = res;
+            return res;
+        }
+        public IList<string> WordBreak1(string s, IList<string> wordDict) {
+            var map = new  Dictionary<string, List<string>>();
             return DFS(s, wordDict, map);
         }
         List<string> DFS(string s, IList<string> wordDict, Dictionary<string, List<string>> map){
