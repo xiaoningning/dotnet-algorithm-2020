b5a26266b8daa3830ae5d56e20bd5c4d9f29fd33
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-10-10 00:33:37 -0700

    find the first missing positive OJ

--- findMissingPositive/Program.cs
+++ findMissingPositive/Program.cs
@@ -1,5 +1,21 @@
 public class Solution {
     public int FirstMissingPositive(int[] nums) {
+        int n = nums.Length;
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] != i + 1) return i + 1;
+        }
+        // the last positive is missing
+        return n + 1;
+    }
+    void swap(int[] nums, int i, int j) {
+        int t = nums[i]; nums[i] = nums[j]; nums[j] = t;
+    }
+    public int FirstMissingPositive1(int[] nums) {
         HashSet<int> s = new HashSet<int>();
         int max = 0;
         foreach (int n in nums) {
@@ -11,6 +27,7 @@ public class Solution {
         for (int i = 1; i <= max; i++){
             if (!s.Contains(i)) return i;
         }
+        // the last positive is missing
         return max + 1;
     }
 }
