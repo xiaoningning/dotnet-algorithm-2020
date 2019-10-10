197df772faca327b297741da207ad64db2f0d6ab
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-10-10 00:58:18 -0700

    Wildcard Matching update OJ

--- wildcardMatching/Program.cs
+++ wildcardMatching/Program.cs
@@ -9,52 +9,55 @@ namespace wildcardMatching
             var obj = new Solution();
             Console.WriteLine("wild card match:{0}", obj.IsMatch("aab", "?*"));
         }
-        // '?' Matches any single character.
-        // '*' Matches any sequence of characters (including the empty sequence).
-        public class Solution {
-            public bool IsMatch(string s, string p) {
-                int si = 0, pi = 0, sstar = -1, pstar= -1;
-                while (si < s.Length){
-                    if (pi < p.Length && (p[pi] == '?' || s[si] == p[pi])){
-                        si++;
-                        pi++;
-                    }
-                    else if (pi < p.Length && p[pi] == '*'){
-                        pstar = pi;
-                        sstar = si;
-                        pi++;
-                    }
-                    else if (pstar != -1){
-                        pi = pstar + 1;
-                        sstar++;
-                        si = sstar;
-                    }
-                    else return false;
-
-                }
-                while (pi < p.Length && p[pi] == '*') pi++;
-                return pi == p.Length;
+    }
+    public class Solution {
+         // '?' Matches any single character.
+         // '*' Matches any sequence of characters (including the empty sequence).
+         public bool IsMatch(string s, string p){
+            bool[,] dp = new bool[s.Length + 1, p.Length + 1];
+            dp[0,0] = true;                
+            for (int j = 1; j <= p.Length; j++) { 
+                if (p[j-1] == '*') dp[0,j] = dp[0, j-1]; 
             }
-
-            public bool IsMatch1(string s, string p){
-                bool[,] dp = new bool[s.Length + 1, p.Length + 1];
-                dp[0,0] = true;                
-                for (int j = 1; j <= p.Length; j++) { 
-                    if (p[j-1] == '*') dp[0,j] = dp[0, j-1]; 
-                }
-                
-                for (int i = 1; i <= s.Length; i++){
-                    for (int j = 1; j <= p.Length; j++){
-                        if (p[j-1] == '*'){
-                            dp[i,j] = dp[i, j-1] || dp[i-1,j];
-                        }
-                        else {
-                            dp[i,j] = dp[i-1, j-1] && (s[i-1] == p[j-1] || p[j-1] == '?');
-                        }
+            // s = "", p = "*"
+            // i start as 0
+            for (int i = 0; i <= s.Length; i++){
+                for (int j = 1; j <= p.Length; j++){
+                    if (p[j-1] == '*'){
+                        dp[i,j] = dp[i, j-1] || (i > 0 && dp[i-1,j]);
+                    }
+                    else {
+                        dp[i,j] = i > 0 && dp[i-1, j-1] && (s[i-1] == p[j-1] || p[j-1] == '?');
                     }
                 }
-                return dp[s.Length, p.Length];
             }
+            return dp[s.Length, p.Length];
+        }
+
+        public bool IsMatch1(string s, string p) {
+            int si = 0, pi = 0, sstar = -1, pstar= -1;
+            // s = "", then go after while directly
+            while (si < s.Length){
+                if (pi < p.Length && (p[pi] == '?' || s[si] == p[pi])){
+                    si++;
+                    pi++;
+                }
+                else if (pi < p.Length && p[pi] == '*'){
+                    pstar = pi;
+                    sstar = si;
+                    pi++;
+                }
+                else if (pstar != -1){
+                    pi = pstar + 1;
+                    sstar++;
+                    si = sstar;
+                }
+                else return false;
+
+            }
+            // s = "", p = "*"
+            while (pi < p.Length && p[pi] == '*') pi++;
+            return pi == p.Length;
         }
     }
 }
