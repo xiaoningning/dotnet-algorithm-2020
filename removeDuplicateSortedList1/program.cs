bf3263dc076948eac0e5959debce6da43546d0be
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-10-12 08:46:08 -0700

    remove duplicates in sorted list OJ

---
+++ removeDuplicateSortedList1/program.cs
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode DeleteDuplicates(ListNode head) {
+        if (head == null || head.next ==  null) return head;
+        head.next = DeleteDuplicates(head.next);
+        return (head.val == head.next.val) ? head.next : head;
+    }
+}
