a8a248664b34366f0fbc74d00050427cbfb33aba
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-10-15 03:02:56 -0700

    median two sorted array OJ

--- mediaTwoSortedArray/Program.cs
+++ mediaTwoSortedArray/Program.cs
@@ -1,5 +1,24 @@
 public class Solution {
     public double FindMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.Length, n = nums2.Length;
+        if (m < n) return FindMedianSortedArrays(nums2, nums1);
+        if (n == 0) return (nums1[(m - 1) / 2] + nums1[m / 2]) / 2.0;
+        int left = 0, right = 2 * n;
+        while (left <= right) {
+            int mid2 = (left + right) / 2;
+            int mid1 = m + n - mid2;
+            double L1 = mid1 == 0 ? Int64.MinValue : nums1[(mid1 - 1) / 2];
+            double L2 = mid2 == 0 ? Int64.MinValue : nums2[(mid2 - 1) / 2];
+            double R1 = mid1 == m * 2 ? Int64.MaxValue : nums1[mid1 / 2];
+            double R2 = mid2 == n * 2 ? Int64.MaxValue : nums2[mid2 / 2];
+            if (L1 > R2) left = mid2 + 1;
+            else if (L2 > R1) right = mid2 - 1;
+            else return (Math.Max(L1, L2) + Math.Min(R1, R2)) / 2;
+        }
+        return -1;
+    }
+
+    public double FindMedianSortedArrays1(int[] nums1, int[] nums2) {
         int m = nums1.Length, n = nums2.Length, left = (m + n + 1) / 2, right = (m + n + 2) / 2;
         return (FindKth(nums1, 0, nums2, 0, left) + FindKth(nums1, 0, nums2, 0, right)) / 2.0;
     }
