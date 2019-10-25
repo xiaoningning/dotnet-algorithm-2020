0e25815bda990b7d7b4d296f53f0a4e478ff871e
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-10-24 22:50:08 -0700

    valid mountain array OJ

---
+++ validMountainArray/program.cs
@@ -0,0 +1,8 @@
+public class Solution {
+    public bool ValidMountainArray(int[] A) {
+        int n = A.Length, i = 0, j = n - 1;
+        while (i + 1 < n && A[i] < A[i + 1]) i++;
+        while (j > 0 && A[j - 1] > A[j]) j--;
+        return i > 0 && i == j && j < n - 1;
+    }
+}
