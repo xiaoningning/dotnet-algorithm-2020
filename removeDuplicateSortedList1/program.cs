9002b612e8c972d6210837b855119a181be50106
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-10-29 15:50:27 -0700

    Remove Duplicates from Sorted List update

--- removeDuplicateSortedList1/program.cs
+++ removeDuplicateSortedList1/program.cs
@@ -7,9 +7,23 @@
  * }
  */
 public class Solution {
-    public ListNode DeleteDuplicates(ListNode head) {
+    public ListNode DeleteDuplicates1(ListNode head) {
         if (head == null || head.next ==  null) return head;
         head.next = DeleteDuplicates(head.next);
         return (head.val == head.next.val) ? head.next : head;
+    }
+     public ListNode DeleteDuplicates(ListNode head) {
+         if (head == null || head.next == null) return head;
+         var t = new ListNode(-1);
+         t.next = head;
+         while (head != null) {
+            var next = head.next;
+            while (next != null && head.val == next.val) {
+                next = next.next;
+            }
+            if ( next != head.next) head.next = next;
+            else head = head.next;
+        };
+        return t.next;
     }
 }
