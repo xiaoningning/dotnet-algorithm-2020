6053ab93fe2bef85ec3459dbde194aeef1fe95a1
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-10-29 12:56:55 -0700

    Palindromic Substrings Update

--- palindromicSubstr/Program.cs
+++ palindromicSubstr/Program.cs
@@ -56,4 +56,38 @@ namespace palindromicSubstr
         }
 }
     }
+
+    public class Solution {
+        public int CountSubstrings(string s) {
+            int res = 0;
+            int n = s.Length;
+            bool[,] dp = new bool[n,n];
+            for (int i = n - 1; i >= 0; --i) {
+                for (int j = i; j < n; ++j) {
+                    dp[i,j] = (s[i] == s[j]) && (j - i <= 2 || dp[i + 1,j - 1]);
+                    if (dp[i,j]) ++res;
+                }
+            }
+            return res;
+        }
+
+        public int CountSubstrings1(string s) {
+            int res = 0;
+            if (!string.IsNullOrEmpty(s)){
+                for (int i = 0; i < s.Length; i++){
+                    res += Helper(s, i, i);
+                    res += Helper(s, i, i+1);
+                }                
+            }
+            return res;
+        }
+
+         int Helper(string s, int left, int right){
+            int res = 0;
+             while(left >=0 && right < s.Length && s[left] == s[right]){
+                left--; right++; res++;
+            }
+             return res;
+        }
+    }
 }
