55f30c34232644f6c93c21f076743c5d1eb0a271
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-10-31 23:00:17 -0700

    Expression Add Operators update

--- expressionAddOp/Program.cs
+++ expressionAddOp/Program.cs
@@ -18,31 +18,35 @@ namespace expressionAddOp
     }
     public class Solution {
         public IList<string> AddOperators(string num, int target) {
-            List<string> res = new List<string>();
+            var res = new List<string>();
             if (string.IsNullOrEmpty(num)) return res;
             BackTrack(num, target, string.Empty, res, 0, 0); 
             return res;
         }
-        void BackTrack(string num, int target, string path, List<string> res, Int64 eval, Int64 diff){
+        void BackTrack(string num, 
+                       int target, 
+                       string path, 
+                       List<string> res, 
+                       Int64 eval, 
+                       Int64 diff){
             if(0 == num.Length && target == eval) {
                 res.Add(path);
                 return;
             }
-            else {
-                for(int i = 1; i <= num.Length; i++){
-                    string s = num.Substring(0, i);
-                    if(s.Length > 1 && s[0] == '0') return;                    
-                    Int64 cur = Int64.Parse(s);
-                    string next = num.Substring(i);
-                    if(string.IsNullOrEmpty(path)){
-                        BackTrack(next, target, path + s, res, cur, cur);
-                    }
-                    else{
-                        BackTrack(next, target, path + "+" + cur, res, eval + cur, cur);
-                        BackTrack(next, target, path + "-" + cur, res, eval - cur, -cur);
-                        // 1 + 2 -> 1 + 2 * 3 => 1 + 2 - 2 + 2 * 3
-                        BackTrack(next, target, path + "*" + cur, res, eval - diff + diff * cur, diff * cur );
-                    }
+
+            for(int len = 1; len <= num.Length; len++){
+                string s = num.Substring(0, len);
+                if(s.Length > 1 && s[0] == '0') return;                    
+                Int64 cur = Int64.Parse(s);
+                string next = num.Substring(len);
+                if(string.IsNullOrEmpty(path)){
+                    BackTrack(next, target, path + s, res, eval+cur, cur);
+                }
+                else{
+                    BackTrack(next, target, path + "+" + cur, res, eval + cur, cur);
+                    BackTrack(next, target, path + "-" + cur, res, eval - cur, -cur);
+                    // 1+2 -> 1+2*3 => 1-2 + 2*3
+                    BackTrack(next, target, path + "*" + cur, res, eval - diff + diff * cur, diff * cur );
                 }
             }
         }
