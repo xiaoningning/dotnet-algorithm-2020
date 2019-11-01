2d6e979def8b95449ade9f60516df0c785cee604
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-10-31 23:48:46 -0700

    Palindrome Partitioning II OJ

--- PalindromePartition2/Program.cs
+++ PalindromePartition2/Program.cs
@@ -13,9 +13,26 @@ namespace PalindromePartition2
     public class Solution {
         public int MinCut(string s) {
             int n = s.Length;
+            int[] dp = new int[n];
+            bool[,] p = new bool[n, n];
+            for (int i = 0; i < n; ++i) {
+                dp[i] = i;
+                for (int j = 0; j <= i; ++j) {
+                    if (s[i] == s[j] && (i - j < 2 || p[j + 1,i - 1])) {
+                        p[j,i] = true;
+                        // one more cut after j-1
+                        dp[i] = (j == 0) ? 0 : Math.Min(dp[i], dp[j - 1] + 1);
+                    }
+                }
+            }
+            return dp[n-1];
+        }
+
+        public int MinCut1(string s) {
+            int n = s.Length;
             // number of cuts for the first i characters
             int[] dp = new int[n+1];
-            for(int i = 0; i <= n; i++) dp[i] = i-1; // max cuts
+            for(int i = 1; i <=n; i++) dp[i] = i-1; // max cuts
             for(int i = 0; i < n; i++){
                 // odd length palindrome: xyx
                 for(int j = 0; i-j>=0 && i+j < n && s[i+j] == s[i-j]; j++)
