114623f5ad279951912360536110ad00d5cef68b
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-11-03 16:15:40 -0800

    reaching points update

--- reachingPoints/program.cs
+++ reachingPoints/program.cs
@@ -1,7 +1,9 @@
 public class Solution {
     public bool ReachingPoints(int sx, int sy, int tx, int ty) {
         // backward from t to s.
-        // mod => ty -= tx, tx -= ty
+        // backward to root is only one way
+        // from start to target, many possibility to count
+        // using % mod => ty -= tx, tx -= ty
         while (sx < tx && sy < ty)
             if (tx < ty) ty %= tx;
             else tx %= ty;
