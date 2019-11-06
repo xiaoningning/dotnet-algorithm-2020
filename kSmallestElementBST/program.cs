c52bbdc240d93cd74daadb1ceb6c42bb68822824
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-11-06 01:25:53 -0800

    Kth Smallest Element in a BST OJ

---
+++ kSmallestElementBST/program.cs
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int KthSmallest(TreeNode root, int k) {
+        int cnt = countNodes(root.left);
+        if (k <= cnt) return KthSmallest(root.left, k);
+         // 1 is counted as current node
+        else if (k > cnt + 1) return KthSmallest(root.right, k-1-cnt);
+        return root.val;
+    }
+  
+    public int countNodes(TreeNode n) {
+      if (n == null) return 0;
+      return 1 + countNodes(n.left) + countNodes(n.right);
+    }
+}
