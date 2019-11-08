dec227e79015b0935639775286f3a7e122dd2fd9
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-11-08 06:36:43 -0800

    Non-overlapping Intervals OJ

---
+++ nonOverlappingIntervals/program.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int EraseOverlapIntervals(int[][] intervals) {
+        Array.Sort(intervals, (a,b) => (a[0] - b[0]));
+        int res = 0, last = 0; 
+        for (int i = 1; i < intervals.Length; i++) {
+            if (intervals[i][0] < intervals[last][1]) {
+                res++;
+                // remove last
+                if (intervals[i][1] < intervals[last][1]) last = i;
+            }
+            else last = i;
+        }
+        return res;
+    }
+}
