22f2f284b03a05ceb661f63a542840b32877d97d
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-11-08 07:21:01 -0800

    Max Area of Island Update

--- maxAreaIsland/program.cs
+++ maxAreaIsland/program.cs
@@ -1,5 +1,31 @@
 public class Solution {
     public int MaxAreaOfIsland(int[][] grid) {
+        int res = 0, m = grid.Length, n = grid[0].Length;
+        var dirs = new int[4,2]{{0,-1},{-1,0},{0,1},{1,0}};
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if (grid[i][j] != 1) continue;
+                int cnt = 0;
+                var q = new Queue<int[]>();
+                q.Enqueue(new int[]{i,j});
+                grid[i][j] = -1;
+                while (q.Any()) {
+                    var t = q.Dequeue();
+                    res = Math.Max(res, ++cnt);
+                    for(int d = 0; d < dirs.GetLength(0); d++) {
+                        int x = t[0] + dirs[d,0], y = t[1] + dirs[d,1];
+                        if (x >= 0 && x < m && y >=0 && y < n && grid[x][y] == 1) {
+                            grid[x][y] = -1;
+                            q.Enqueue(new int[]{x,y});
+                        }
+                    }
+                }
+            }
+        }
+        return res; 
+    }
+    
+    public int MaxAreaOfIsland1(int[][] grid) {
         int res = 0;
         for(int i = 0; i < grid.Length; i++)
             for(int j = 0; j < grid[0].Length; j++)
