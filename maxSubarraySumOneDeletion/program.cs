8330557a82907654a01239374ca7479e841d54be
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-11-17 15:53:41 -0800

    Maximum Subarray Sum with One Deletion OJ

---
+++ maxSubarraySumOneDeletion/program.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int MaximumSum(int[] arr) {
+        int ignored = 0, notignored = 0, mx = arr.Max(), res = mx;
+        foreach (int a in arr) {
+            // ignored current negative
+            // or add previous negative, ignore the current one
+            ignored = Math.Max(ignored + a, notignored);
+            notignored =+ Math.Max(notignored + a, a);
+            res = Math.Max(res, Math.Max(ignored, notignored));
+        }
+        // all negative cases
+        return mx < 0 ? mx : res;
+    }
+}
