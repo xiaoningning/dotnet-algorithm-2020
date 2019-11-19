a1febac7bd7ecb1ec36dd5f3e866eefeda6c9a0a
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-11-19 01:56:00 -0800

    String Transforms Into Another String update

--- strTransformsAnotherStr/program.cs
+++ strTransformsAnotherStr/program.cs
@@ -9,6 +9,10 @@ public class Solution {
         }
         // transformation of cycle, like a -> b -> c -> a
         // str2 need extra char
+        // "if size(value set)<26, return true". 
+        // When size(value set)=26, it implies that size(key set)=26 too, so we have 26 one-to-one mappings. 
+        // If str1 is not the same as str2, cycle must exist, 
+        // but there is no unused character to break the cycle. So return false.
         return new HashSet<char>(m.Values).Count < 26;
     }
 }
