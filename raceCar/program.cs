b68f0150c99a7a89d3a1f8d5a7d91846ab3a9644
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-11-22 03:56:10 -0800

    race car update

--- raceCar/program.cs
+++ raceCar/program.cs
@@ -8,13 +8,13 @@ public class Solution {
             dp[i] = Int32.MaxValue;
 
             int m = 1, j = 1;
-
+            // before i, acclerate
             for (; j < i; j = (1 << ++m) - 1) {
                 for (int q = 0, p = 0; p < j; p = (1 << ++q) - 1) {
                     dp[i] = Math.Min(dp[i], m + 1 + q + 1 + dp[i - (j - p)]);
                 }
             }
-
+            // by pass i
             dp[i] = Math.Min(dp[i], m + (i == j ? 0 : 1 + dp[j - i]));
         }
 
