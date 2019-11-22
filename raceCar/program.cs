d72026ee3e499d3a279a1e61bdfdaca39bcfdcef
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-11-22 05:05:15 -0800

    race car  update

--- raceCar/program.cs
+++ raceCar/program.cs
@@ -3,21 +3,18 @@ public class Solution {
     // DP
     public int Racecar(int target) {
         int[] dp = new int[target + 1];
-    
         for (int i = 1; i <= target; i++) {
             dp[i] = Int32.MaxValue;
-
-            int m = 1, j = 1;
+            int j = 1, cnt1 = 1;
             // before i, acclerate
-            for (; j < i; j = (1 << ++m) - 1) {
-                for (int q = 0, p = 0; p < j; p = (1 << ++q) - 1) {
-                    dp[i] = Math.Min(dp[i], m + 1 + q + 1 + dp[i - (j - p)]);
+            for (; j < i; j = (1 << ++cnt1) - 1) {
+                for (int k = 0, cnt2 = 0; k < j; k = (1 << ++cnt2) - 1) {
+                    dp[i] = Math.Min(dp[i], cnt1 + 1 + cnt2 + 1 + dp[i - (j - k)]);
                 }
             }
             // by pass i
-            dp[i] = Math.Min(dp[i], m + (i == j ? 0 : 1 + dp[j - i]));
+            dp[i] = Math.Min(dp[i], cnt1 + (i == j ? 0 : 1 + dp[j - i]));
         }
-
         return dp[target];
     }
 }
