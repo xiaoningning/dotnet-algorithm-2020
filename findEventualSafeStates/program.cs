3ea4bcbace5ac7c7458bb52cc981ce8ef2ab6768
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-11-22 04:11:20 -0800

    Find Eventual Safe States OJ

---
+++ findEventualSafeStates/program.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public IList<int> EventualSafeNodes(int[][] graph) {
+        var res = new List<int>();
+        if(graph == null || graph.Length == 0)  return res;
+        
+        int nodeCount = graph.Length;
+        int[] color = new int[nodeCount];
+        
+        for(int i = 0;i < nodeCount;i++){
+            if(dfs(graph, i, color)) res.Add(i);
+        }
+        return res;
+    }
+    bool dfs(int[][] graph, int start, int[] color) {
+        if(color[start] != 0) return color[start] == 1;
+        
+        color[start] = 2; // unsafe
+        foreach (int newNode in graph[start]){
+            if(!dfs(graph, newNode, color)) return false;
+        }
+        // all directed nodes are safe
+        color[start] = 1; // safe
+        
+        return true;
+    }
+}
