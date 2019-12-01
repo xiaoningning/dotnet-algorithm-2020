f1381bb8c329017858ef37d72ceecd88ffa484b9
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-12-01 13:52:01 -0800

    add strings Update OJ

--- addStrings/program.cs
+++ addStrings/program.cs
@@ -1,5 +1,5 @@
 public class Solution {
-    public string AddStrings(string num1, string num2) {
+    public string AddStrings1(string num1, string num2) {
         string res = "";
         int carry = 0;
         // carry == 1 for the last bit result
@@ -14,5 +14,17 @@ public class Solution {
         var arr = res.ToCharArray();
         Array.Reverse(arr);
         return new string(arr);
+    }
+    public string AddStrings(string num1, string num2) {
+        string res = "";
+        int carry = 0, i = num1.Length - 1, j = num2.Length - 1;
+        while (i >= 0 || j >= 0) {
+            int a = i >= 0 ? num1[i--] - '0' : 0;
+            int b = j >= 0 ? num2[j--] - '0' : 0;
+            int sum = a + b + carry;
+            res = res.Insert(0, (sum % 10).ToString());
+            carry = sum / 10;
+        }
+        return carry == 1 ? "1" + res : res;
     }
 }
