a021ea91386ef574b3dca48c8054b72dabfc279c
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-12-02 23:34:02 -0800

    min stack update OJ

--- minStack/Program.cs
+++ minStack/Program.cs
@@ -1,10 +1,33 @@
 public class MinStack {
+    private Stack<int> s1;
+    private Stack<int> s2;
+    
+    public MinStack() {
+        s1 = new Stack<int>();
+        s2 = new Stack<int>();
+    }  
+    public void Push(int x) {
+        s1.Push(x);
+        if (!s2.Any() || s2.Peek() >= x) s2.Push(x);
+    }
+    public void Pop() {
+        int x = s1.Pop();
+        if (s2.Peek() == x) s2.Pop();
+    }   
+    public int Top() {
+        return s1.Peek();
+    }  
+    public int GetMin() {
+        return s2.Peek();
+    }
+}
+public class MinStack1 {
     // use two stack, one for min value
     // another for all value
     private int _min;
     private Stack<int> _s;
     /** initialize your data structure here. */
-    public MinStack() {
+    public MinStack1() {
         _min = Int32.MaxValue;
         _s = new Stack<int>();
     }
