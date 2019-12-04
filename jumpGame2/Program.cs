5024f7b50c4cc7b1474ce7f525b690b21907d72f
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-12-03 22:05:34 -0800

    jump game 2 OJ

--- jumpGame2/Program.cs
+++ jumpGame2/Program.cs
@@ -1,6 +1,22 @@
 public class Solution {
-    public int Jump(int[] nums) {
+    public int Jump(int[] nums) { 
+        int n = nums.Length, res = 0, cur = 0, last = 0;
+        // only check till n - 2 if it can reach n - 1
+        for (int i = 0; i < n - 1; i++) {
+            cur = Math.Max(cur, i + nums[i]);
+            if (i == last) {
+                last = cur;
+                res++;
+                // reach n-1 for min steps
+                if (cur >= n - 1) break;
+            }
+        }
+        return res;
+    }
+    
+    public int Jump1(int[] nums) {
         int n = nums.Length, res = 0, cur = 0, i = 0;
+        // only check till n - 2 if it can reach n - 1
         while (cur < n - 1) {
             res++;
             int pre = cur;
