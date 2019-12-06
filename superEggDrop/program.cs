8574e0ca711a08cfebe6cead2372c4beeeaa66a2
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-12-05 18:13:27 -0800

    Super Egg Drop OJ

---
+++ superEggDrop/program.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int SuperEggDrop(int K, int N) {
+        // # of moves
+        int cnt = 0;
+        // dp[i, j]: # of floors tested by i moves with j eggs 
+        int[,] dp = new int[N+1,K+1];
+        while (dp[cnt,K] < N) {
+            ++cnt;            
+            for (int j = 1; j <= K; j++) {
+                // if egg is broken at the floor, dp[i,j] = dp[i-1, j-1];
+                // if egg is not broken at the floor, this egg can test one more floor.
+                // => dp[i,j] = dp[i-1, j] + 1
+                dp[cnt,j] = dp[cnt - 1,j - 1] + dp[cnt - 1,j] + 1;
+            }
+        }
+        return cnt;
+    }
+}
