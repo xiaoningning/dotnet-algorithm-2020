ab1ed79f8096315583a717b45dab98c5ed57942d
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-12-09 04:57:30 -0800

    Unique Letter String OJ

--- uniqueLetterString/program.cs
+++ uniqueLetterString/program.cs
@@ -1,5 +1,6 @@
 public class Solution {
     public int UniqueLetterString1(string S) {
+        // CACACCAC => CA(CACC)AC, CAC(AC)CAC
         // index of last positions of S[i]
         int[][] index = new int[26][];
         for (int i = 0; i < 26; i++) index[i] = new int[2]{-1,-1};
@@ -16,30 +17,34 @@ public class Solution {
     public int UniqueLetterString(string S) {
         int[] lastPosition = new int[26];
         Array.Fill(lastPosition, -1);
-        int[] cnt = new int[26];
+        int[] dp = new int[26]; // uniq of each char in 26 letters
         int cur = 0, res = 0, mod = (int)Math.Pow(10, 9) + 7;;
         for (int i = 0; i < S.Length; i++) {
             int x = S[i]-'A';
-            cur -= cnt[x] % mod; 
-            cnt[x] = (i - lastPosition[x]);
-            cur += cnt[x] % mod; 
+            // - previous uniq of x
+            cur -= dp[x] % mod; 
+            // current uniq of x
+            dp[x] = (i - lastPosition[x]);
+            cur += dp[x] % mod; 
             lastPosition[x] = i;
             res += cur % mod;
         }   
-        return res;        
+        return res;
+        
     }
-    public int UniqueLetterString2(string S) {
+    public int UniqueLetterString3(string S) {
         int res = 0, dp = 0, mod = (int)Math.Pow(10, 9) + 7;        
         int[] first = new int[26], second = new int[26];
         Array.Fill(first, -1);
         Array.Fill(second, -1);
         for (int i = 0; i < S.Length; i++) {
             int x = S[i]-'A';
-            dp = dp + (i - first[x]) -(first[x] - second[x]);
+            dp = dp + (i - first[x]) - (first[x] - second[x]);
             res = (res + dp) % mod;
             second[x] = first[x];
             first[x] = i;
         }   
-        return res;        
+        return res;
+        
     }
 }
