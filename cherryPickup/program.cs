7931232e4aed79568da0ee27dbb360c524db665c
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-12-09 14:26:38 -0800

    cherry pick update

--- cherryPickup/program.cs
+++ cherryPickup/program.cs
@@ -1,12 +1,16 @@
 public class Solution {
     public int CherryPickup(int[][] grid) {
         int n = grid.Length;
-        int mx = 2 * n - 1; // max steps of (0,0) -> (j,j)
+        int mx = 2 * n - 1; // max steps of (0,0) -> (n-1,n-1)
         int[,] dp = new int[n,n];
         for (int i = 0; i < n*n; i++) dp[i % n, i / n] = -1; 
         dp[0, 0] = grid[0][0];
         // k is step, i,p is the end point
         for (int k = 1; k < mx; ++k) {
+            // k = i + j = p + q
+            // each round k of dp[i,p] needs previous k of dp[i-1,p] and dp[i, p-1]
+            // => from n - 1 to 0
+            // if from 0 to n -1 , then the previous round k is overwritten.
             for (int i = n - 1; i >= 0; --i) {
                for (int p = n - 1; p >= 0; --p) {
                     // i,p right, down
