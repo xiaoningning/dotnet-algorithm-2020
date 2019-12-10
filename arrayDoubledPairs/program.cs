f0a8497288c73873f6d203e79b7968ed01b101d9
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-12-09 22:19:24 -0800

    Array of Doubled Pairs OJ

---
+++ arrayDoubledPairs/program.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public bool CanReorderDoubled(int[] A) {
+        var cnt = new SortedDictionary<int, int>();
+        foreach (int a in A) {
+            if (!cnt.ContainsKey(a)) cnt.Add(a,0);
+            cnt[a]++;
+        }
+        var keys = cnt.Keys.ToList();
+        foreach (int x in keys) {
+            // after t update, skip cnt 0
+            if (cnt[x] == 0) continue;
+            int t = x < 0 ? x / 2 : x * 2;
+            if (x < 0 && x % 2 != 0 
+               || !cnt.ContainsKey(t)
+               || cnt[x] > cnt[t]) return false;
+            cnt[t] -= cnt[x];
+        }
+        return true;
+    }
+}
