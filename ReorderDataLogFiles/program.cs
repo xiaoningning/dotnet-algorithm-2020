5a28c63c3e189d6ab046874528f37d585185b968
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-12-11 12:03:06 -0800

    Reorder Data in Log Files update

--- ReorderDataLogFiles/program.cs
+++ ReorderDataLogFiles/program.cs
@@ -6,7 +6,6 @@ public class Solution {
             if (Char.IsNumber(l.Split(" ")[1][0])) digit.Add(l);
             if (Char.IsLetter(l.Split(" ")[1][0])) letter.Add(l);
         }
-        // letter.Sort((a,b) => (a.Split(" ")[1] == b.Split(" ")[1]) ? string.Compare(a.Split(" ")[0], b.Split(" ")[0]) : string.Compare(a.Split(" ")[1], b.Split(" ")[1]));   
         letter.Sort((a,b) => {
             int la = a.Split(" ")[0].Length;
             int lb = b.Split(" ")[0].Length;
