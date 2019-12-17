d2529aa3498903106372b5eb809c4ee3d1dd3963
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-12-16 16:09:06 -0800

    Web Crawler OJ

---
+++ WebCrawler/program.cs
@@ -0,0 +1,26 @@
+/**
+ * // This is the HtmlParser's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class HtmlParser {
+ *     public List<String> GetUrls(String url) {}
+ * }
+ */
+class Solution {
+    public IList<string> Crawl(string startUrl, HtmlParser htmlParser) {
+        var st = new HashSet<string>();
+        var q = new Queue<string>();
+        var hostname = startUrl.Split("/")[2];
+        st.Add(startUrl);
+        q.Enqueue(startUrl);
+        while (q.Any()) {
+            var n = q.Dequeue();
+            foreach (string url in htmlParser.GetUrls(n)) {
+                if (url.Contains(hostname) && !st.Contains(url)) {
+                    q.Enqueue(url);
+                    st.Add(url);
+                }
+            }
+        }
+        return st.ToList();
+    }
+}
