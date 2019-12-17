f1a86d496e9a1501a9ab4d2f2f0c8cad1982ecfc
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-12-16 17:46:30 -0800

    Number of Valid Words for Each Puzzle OJ

---
+++ NumberOfValidWordsforEachPuzzle/program.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public IList<int> FindNumOfValidWords(string[] words, string[] puzzles) {
+        var map = new Dictionary<int, int>();
+        foreach (string w in words){
+            int mask = 0; // bit mask
+            for(int i = 0; i < w.Length; i++)
+                mask |= 1 << (w[i] - 'a');
+            map[mask] = map.GetValueOrDefault(mask, 0) + 1;
+        }
+        var res = new List<int>();
+        foreach (string p in puzzles){
+            // all combination with 1st letter
+            var t = new List<int>(){1 << (p[0] - 'a')};
+            for(int i = 1; i < p.Length; i++) {
+                var level = t.ToList();
+                foreach(int m in level) {
+                    int m1 = m | 1 << (p[i] - 'a');
+                    t.Add(m1);  
+                } 
+            }
+            int cnt = 0;
+            foreach(var m in t) cnt += map.GetValueOrDefault(m,0);
+            res.Add(cnt);
+        }
+        return res;
+    }
+}
