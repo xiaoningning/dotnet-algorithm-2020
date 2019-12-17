fafbfb9aa1f712bb3d500df7157ed7d5a007d2ed
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-12-16 17:59:22 -0800

    Check If It Is a Good Array OJ

--- CheckIfAGoodArray/program.cs
+++ CheckIfAGoodArray/program.cs
@@ -1,5 +1,7 @@
 public class Solution {
     public bool IsGoodArray(int[] nums) {
+        // all positive nums
+        // If x > 1, making it impossible pa + qb = 1.
         int res = nums[0];
         foreach (int a in nums) {
             res = gcd(res, a);
