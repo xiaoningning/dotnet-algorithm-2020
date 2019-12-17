d0d74f9f6faa6971dca46dc439371cbf410fbbb8
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-12-17 07:12:08 -0800

    Minimum Add to Make Parentheses Valid OJ

--- minAddtoMakeParentheseValid/program.cs
+++ minAddtoMakeParentheseValid/program.cs
@@ -2,18 +2,12 @@ public class Solution {
     public int MinAddToMakeValid(string S) {
         int left = 0, right = 0;
         for (int i = 0; i < S.Length; ++i) {
-            if (S[i] == '(') {
-                right++;
-            } 
+            if (S[i] == '(') right++;
             // s[i] == ")" and has "(" already
-            else if (right > 0) {
-                right--;
-            } 
+            else if (right > 0) right--;
             // if valid ")" right--
             // invalid ")" with no "(" in front 
-            else {
-                left++;
-            }
+            else left++;
         }
         return left + right;
     }
