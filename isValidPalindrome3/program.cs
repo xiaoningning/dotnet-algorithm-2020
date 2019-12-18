0c1ba850673dc083f91cae442087eb497189b01c
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-12-18 02:30:41 -0800

    Valid Palindrome III OJ

--- isValidPalindrome3/program.cs
+++ isValidPalindrome3/program.cs
@@ -1,6 +1,17 @@
 public class Solution {
     public bool IsValidPalindrome(string s, int k) {
         int n = s.Length;
+        int[,] dp = new int[n,n];
+        for (int i = n-1; i >= 0; i--) {
+            for (int j = i+1; j < n; j++) {
+                if (s[j] == s[i]) dp[i,j] = dp[i+1,j-1];
+                else dp[i,j] = 1+ Math.Min(dp[i + 1,j], dp[i,j - 1]);
+            }
+        }
+        return dp[0,n-1] <= k;
+    }
+    public bool IsValidPalindrome1(string s, int k) {
+        int n = s.Length;
         // steps to be palindrome
         int[,] m = new int[n,n];
         return helper(s, 0, n-1, m) <= k;
@@ -9,7 +20,7 @@ public class Solution {
         if (l == r) return 0;
         if (l > r) return 0;
         if (m[l,r] != 0) return m[l,r];
-        m [l,r] = s[l] == s[r] ? 
+        m[l,r] = s[l] == s[r] ? 
             0 + helper(s, l+1, r-1, m)
             : Math.Min(helper(s, l+1, r, m), helper(s, l, r-1, m)) + 1;
         return m[l,r];
