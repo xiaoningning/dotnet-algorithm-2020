da1254fb66b203c405dcce1b88284e084c5e41b0
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-12-19 22:50:38 -0800

    Shortest Path with Alternating Colors OJ

--- shortestPathAlternatingColors/program.cs
+++ shortestPathAlternatingColors/program.cs
@@ -1,32 +1,34 @@
 public class Solution {
     public int[] ShortestAlternatingPaths(int n, int[][] red_edges, int[][] blue_edges) {
-        var g = new Dictionary<Tuple<int, int>, List<int>>();   
+        var g = new Dictionary<string, List<int>>();   
         // red: 0, blue: 1
         for (int i = 0; i < red_edges.Length; i++) {
-			var key = Tuple.Create(red_edges[i][0], 0);
+			var key =  0 + "-" + red_edges[i][0];
             if (!g.ContainsKey(key)) 
                 g.Add(key, new List<int>());
             g[key].Add(red_edges[i][1]);
         }
         for (int i = 0; i < blue_edges.Length; i++) {
-			var key = Tuple.Create(red_edges[i][0], 1);
+			var key =  1 + "-" + blue_edges[i][0];
             if (!g.ContainsKey(key)) 
                 g.Add(key, new List<int>());
             g[key].Add(blue_edges[i][1]);
         }
-        var q = new Queue<Tuple<int, int>>();
-        q.Enqueue(Tuple.Create(0,0));
-        q.Enqueue(Tuple.Create(0,1));
+        var q = new Queue<string>();
+        q.Enqueue("0-0");
+        q.Enqueue("1-0");
         var res = new int[n, 2];
-        for (int i= 0; i < n; i++) { res[i, 0] = n * 2;  res[i, 1] = n * 2; }
+        // init 0 => 0
+        for (int i= 1; i < n; i++) { res[i, 0] = n * 2;  res[i, 1] = n * 2; }
         while (q.Any()) {
             var t = q.Dequeue();
             if (!g.ContainsKey(t)) continue;
             foreach (var r in g[t]) {
-                // t[1] is color
-                if (res[r, 1 - t.Item2] == n * 2) {
-                    res[r, 1 - t.Item2] = res[t.Item1, t.Item2] + 1;
-                    q.Enqueue(Tuple.Create(r, 1 - t.Item2));
+                int color = Int32.Parse(t.Split("-")[0]);
+                int node  = Int32.Parse(t.Split("-")[1]);
+                if (res[r, 1 - color] == n * 2) {
+                    res[r, 1 - color] = res[node, color] + 1;
+                    q.Enqueue((1-color) + "-" + r);
                 }
             }
         }
