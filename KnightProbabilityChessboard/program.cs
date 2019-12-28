5e4f895950867af0c9f5db7bba274fad38ab5b0c
Ning <1930967+xiaoningning@users.noreply.github.com>
2019-12-28 07:38:05 -0800

    Knight Probability in Chessboard OJ

--- KnightProbabilityChessboard/program.cs
+++ KnightProbabilityChessboard/program.cs
@@ -12,12 +12,12 @@ public class Solution {
             for (int i = 0; i < N; ++i)
                 for (int j = 0; j < N; ++j) 
                     for (int m = 0; m < 8; ++m) {
-                        int x = j + dirs[m,0];
-                        int y = i + dirs[m,1];
+                        int x = j + dirs[m,0], y = i + dirs[m,1];
                         if (x < 0 || y < 0 || x >= N || y >= N) continue;
                         dp1[i,j] += dp0[y,x];
                     }
-            Array.Copy(dp1, 0, dp0, 0, N);
+            // 2D array copy with N*N
+            Array.Copy(dp1, 0, dp0, 0, N*N);
         }
         return dp0[r,c] / Math.Pow(8, K);
     }
