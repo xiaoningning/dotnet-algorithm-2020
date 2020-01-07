5d5d074cc09ac78f6c91b9d09ea75403206886d2
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-01-06 23:49:31 -0800

    Sliding Window Maximum update

--- slidingWindowMax/Program.cs
+++ slidingWindowMax/Program.cs
@@ -1,33 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace slidingWindowMax
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var obj = new Solution();
-            Console.WriteLine("sliding windows max : {0}", string.Join(",", obj.MaxSlidingWindow(new int[]{1,3,-1,-3,5,3,6,7}, 3)));
+public class Solution {
+    public int[] MaxSlidingWindow(int[] nums, int k) {
+        List<int> res = new List<int>();
+        // store the index of max value
+        // in the oder of max to smaller
+        List<int> q = new List<int>();
+        for (int i =0; i < nums.Length; i++) {
+            // i shift to left more than k. out of k window
+            // remove the first index of k window
+            if (q.Any() && q[0] == i - k) q.RemoveAt(0);
+            // remove index whose value < current i value
+            while (q.Any() && nums[q.Last()] < nums[i]) q.RemoveAt(q.Count - 1);
+            q.Add(i);
+            if (i >= k - 1) res.Add(nums[q[0]]);
         }
-    }
-    public class Solution {
-        public int[] MaxSlidingWindow(int[] nums, int k) {
-            List<int> res = new List<int>();
-            // store the index of max values
-            // in the oder of max to smaller
-            List<int> q = new List<int>();
-            for (int i =0; i < nums.Length; i++) {
-                // i shift to left more than k. out of k window
-                // remove the first index of k window
-                if (q.Count != 0 && q[0] == i - k) q.RemoveAt(0);
-                // remove index whose value < current i value
-                // 
-                while (q.Count != 0 && nums[q[q.Count - 1]] < nums[i]) q.RemoveAt(q.Count - 1);
-                q.Add(i);
-                if (i >= k - 1) res.Add(nums[q[0]]);
-            }
-            return res.ToArray();
-        }
+        return res.ToArray();
     }
 }
