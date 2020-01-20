0fc932e87decd7b1a4344a678708304789d2b46e
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-01-20 09:45:38 -0800

    Global and Local Inversions update

--- GlobalandLocalInversions/program.cs
+++ GlobalandLocalInversions/program.cs
@@ -1,5 +1,5 @@
 public class Solution {
-    public bool IsIdealPermutation(int[] A) {
+    public bool IsIdealPermutation1(int[] A) {
         // If the # of global inversions == the # of local inversions,
         // => all global inversions in permutations are local inversions.
         // to place number i, only place i at A[i-1],A[i] or A[i+1]
@@ -7,5 +7,32 @@ public class Solution {
             if (Math.Abs(A[i] - i) > 1) return false;
         }
         return true;
+    }
+    int[] tmp;
+    public bool IsIdealPermutation(int[] A) {
+        int localInv = 0, n = A.Length;
+        tmp = new int[n];
+        for (int i = 0; i < n -1; i++) if (A[i] > A[i+1]) localInv++;
+        int globalInv = MergeSort(A, 0, n - 1);
+        return globalInv == localInv;
+    }
+    int MergeSort(int[] A, int l, int r) { 
+        if (l >= r) return 0;
+        int m = l + (r - l) / 2;
+        int inv = MergeSort(A, l, m) + MergeSort(A, m+1, r);
+        int i = l;
+        int j = m + 1;
+        int k = 0;
+        while (i <= m && j <= r) {
+            if (A[i] <= A[j]) tmp[k++] = A[i++];
+            else {
+                inv += m - i + 1;
+                tmp[k++] = A[j++];
+            }
+        }
+        while (i <= m) tmp[k++] = A[i++];
+        while (j <= r) tmp[k++] = A[j++];
+        Array.Copy(tmp, 0, A, l, k);
+        return inv;
     }
 }
