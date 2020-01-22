9e8b9f282c3a03230bfdd501866cc178404fb34a
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-01-21 17:22:50 -0800

    Find Mode in Binary Search Tree OJ

---
+++ FindModeBinarySearchTree/program.cs
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int[] FindMode(TreeNode root) {
+        InOrder(root);
+        return res.ToArray();
+    }
+    List<int> res = new List<int>();
+    int val = 0;
+    int cnt = 0;
+    int mxCnt = 0;
+    void InOrder(TreeNode root) {
+        if (root == null) return;
+        InOrder(root.left);
+        Visit(root);
+        InOrder(root.right);
+    }
+    void Visit(TreeNode root) {
+        if (cnt > 0 && val == root.val) cnt++;
+        else {
+            val = root.val;
+            cnt = 1;
+        }
+        if (cnt > mxCnt) {
+            mxCnt = cnt;
+            res.Clear();
+        }
+        if (cnt == mxCnt) {
+            res.Add(root.val);
+        }
+    }
+}
