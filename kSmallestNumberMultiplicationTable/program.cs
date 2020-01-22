443b7d8357af3d0f6213fc54a048a3b7f8544a84
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-01-21 22:43:34 -0800

    Kth Smallest Number Multiplication Table update

--- kSmallestNumberMultiplicationTable/program.cs
+++ kSmallestNumberMultiplicationTable/program.cs
@@ -1,6 +1,6 @@
 public class Solution {
     public int FindKthNumber(int m, int n, int k) {
-        int left = 1, right = m * n;
+        int left = 1, right = m * n + 1;
          while (left < right) {
              int mid = left + (right - left) / 2, cnt = 0;
              // each i is n
