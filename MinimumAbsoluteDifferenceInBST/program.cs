1055ddc4637ac30beb2af0c7f22469ae5e2cba29
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-01-21 16:04:40 -0800

    Minimum Absolute Difference in BST OJ

---
+++ MinimumAbsoluteDifferenceInBST/program.cs
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    int res = Int32.MaxValue;
+    int preVal = -1;
+    public int GetMinimumDifference(TreeNode root) {
+        if (root == null) return res;
+        GetMinimumDifference(root.left);
+        if (preVal >= 0) res = Math.Min(res, root.val - preVal);
+        preVal = root.val;
+        GetMinimumDifference(root.right);
+        return res;
+    }
+}
