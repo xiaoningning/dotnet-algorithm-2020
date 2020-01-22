08bbd07864197ed9a3572b91c360adb98a3a6d4b
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-01-21 23:48:23 -0800

    sqrt x update

--- sqrt/Program.cs
+++ sqrt/Program.cs
@@ -1,26 +1,13 @@
-﻿using System;
-
-namespace sqrt
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var obj = new Solution();
-            Console.WriteLine("Sqrt(x) {0}", obj.MySqrt(8));
+public class Solution {
+    public int MySqrt(int x) {
+        if (x <= 1) return x;
+        int right = x, left = 0;
+        while (left <= right){
+            int mid = left + (right - left) / 2;
+            // use divide otherwise, it could be overflow int.
+            if (x / mid >= mid) left = mid + 1;
+            else right = mid - 1;
         }
-    }
-    public class Solution {
-        public int MySqrt(int x) {
-            if (x <= 1) return x;
-            int right = x, left = 0;
-            while (left < right){
-                int mid = left + (right - left) / 2;
-                // use divide otherwise, it could be overflow int.
-                if (x / mid >= mid) left = mid + 1;
-                else right = mid;
-            }
-            return right - 1;
-        }
+        return left - 1;
     }
 }
