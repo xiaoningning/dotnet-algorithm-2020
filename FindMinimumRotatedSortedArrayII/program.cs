ee75ed0723b45bf8b308b80dbc54fb74184bdc68
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-01-22 11:19:31 -0800

    Find Minimum in Rotated Sorted Array II OJ

---
+++ FindMinimumRotatedSortedArrayII/program.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int FindMin(int[] nums) {
+        int l = 0, r = nums.Length - 1;
+        while (l < r) {
+            if (nums[l] < nums[r]) return nums[l];
+            int m = l + (r - l) / 2;
+            // rotated
+            if (nums[m] > nums[r]) l = m + 1;
+            else if (nums[m] < nums[r]) r = m;
+            // duplicated case
+            else r--;
+        }
+        return nums[l];
+    }
+}
