9334459a8e2f840f1c67c8f75e6ff146a321c6cb
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-01-22 01:02:24 -0800

    Dungeon Game OJ

---
+++ DungeonGame/program.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int CalculateMinimumHP(int[][] dungeon) {
+        int m = dungeon.GetLength(0), n = dungeon[0].GetLength(0);
+        Console.WriteLine($"{m}, {n}");
+        // dp[y][x]: min health point required to reach bottom right (P).
+        int[] dp = new int[n+1];
+        Array.Fill(dp, Int32.MaxValue);
+        dp[n - 1] = 1; //n -1 at least 1 to reach [m,n]
+        for (int i = m - 1; i >= 0; --i)
+            for (int j = n - 1; j >= 0; --j)
+                dp[j] = Math.Max(1, Math.Min(dp[j], dp[j + 1]) - dungeon[i][j]);
+        return dp[0];
+    }
+}
