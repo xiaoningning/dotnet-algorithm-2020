d540772b1efa933eb8f03c8a4c10b2cbd13a4e95
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-01-24 17:32:12 -0800

    time based key value update

--- timeBasedKeyValueStore/program.cs
+++ timeBasedKeyValueStore/program.cs
@@ -14,7 +14,7 @@ public class TimeMap {
         if (!map.ContainsKey(key)) return "";
         return BinarySearch(map[key], timestamp);
     }
-    // linq upper bound TLE
+    // linq upper bound TLE is not binary search
     string BinarySearch1(List<Data> list, int timestamp) {
         var res = list.Where(d => d.time <= timestamp).OrderBy(d => d.time);
         if (res.Any()) return res.Last().val;
