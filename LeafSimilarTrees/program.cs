939907daa86ab798f1b65138b3dc100475a95bf3
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-01-26 17:53:22 -0800

    Leaf-Similar Trees

---
+++ LeafSimilarTrees/program.cs
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public bool LeafSimilar(TreeNode root1, TreeNode root2) {
+        var leaf1 = new List<int>();
+        var leaf2 = new List<int>();
+        GetLeaf(root1, leaf1);
+        GetLeaf(root2, leaf2);
+        // c# equal is object level, not element value level
+        return leaf1.SequenceEqual(leaf2);
+    }
+    void GetLeaf(TreeNode root, List<int> leaf) {
+        if (root == null) return;
+        if (root.left == null && root.right == null) leaf.Add(root.val);
+        GetLeaf(root.left, leaf);
+        GetLeaf(root.right, leaf);
+    }
+}
