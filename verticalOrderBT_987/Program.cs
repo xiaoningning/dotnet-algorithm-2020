24f1762592c04f8e9b09dfe86b575e64e8dfab4e
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-01-26 17:42:03 -0800

    Vertical Order Traversal of a Binary Tree Update

--- verticalOrderBT_987/Program.cs
+++ verticalOrderBT_987/Program.cs
@@ -9,6 +9,35 @@
  */
 public class Solution {
     public IList<IList<int>> VerticalTraversal(TreeNode root) {
+        var m = new SortedDictionary<int, SortedDictionary<int, List<int>>>();
+        dfs(root, 0, 0, m);
+        var res = new List<IList<int>>();
+        foreach (var kv in m) {
+            var tl = new List<int>();
+            foreach (var tkv in kv.Value.Reverse()) {
+                tkv.Value.Sort();
+                tl.AddRange(tkv.Value);
+            }
+            res.Add(tl);
+        }
+        return res;
+    }
+    void dfs(TreeNode r, 
+             int y, 
+             int level, 
+             SortedDictionary<int, SortedDictionary<int, List<int>>> m) {
+        if (r != null) {
+            if (!m.ContainsKey(y)) 
+                m.Add(y, new SortedDictionary<int, List<int>>());
+            if (!m[y].ContainsKey(level))
+                m[y].Add(level, new List<int>());
+            m[y][level].Add(r.val);
+            dfs(r.left, y - 1, level - 1, m);
+            dfs(r.right, y + 1, level - 1, m);
+        }
+    }
+    
+    public IList<IList<int>> VerticalTraversal1(TreeNode root) {
         var res = new List<IList<int>>();
         if (root == null) return res;
         var m = new SortedDictionary<int, SortedDictionary<int, List<int>>>();
