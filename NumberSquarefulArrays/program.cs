5c1df69f9f833a1c369948272db6c145366ae82f
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-01-27 14:49:45 -0800

    Number of Squareful Arrays Update OJ

--- NumberSquarefulArrays/program.cs
+++ NumberSquarefulArrays/program.cs
@@ -1,8 +1,36 @@
 public class Solution {
+    public int NumSquarefulPerms(int[] A) { 
+        // avoid duplicates in A
+        Array.Sort(A);
+        var used = new bool[A.Length];
+        Permutation(A, new List<int>(), used);
+        return res;
+    }
+    void Permutation(int[] A, List<int> tmp, bool[] used) {
+        if (tmp.Count == A.Length) { 
+            res++;
+            return;
+        }
+        for (int i = 0; i < A.Length; i ++) {
+            // like permutation II
+            if (used[i] || (i > 0 && A[i] == A[i - 1] && !used[i - 1])) continue;
+            if (tmp.Count == 0 || IsSquareful(A[i], tmp.Last())) {
+                used[i] = true;
+                tmp.Add(A[i]);
+                Permutation(A, tmp, used);
+                tmp.RemoveAt(tmp.Count - 1);
+                used[i] = false;
+            }
+        }
+    }
+    bool IsSquareful(int x, int y) {
+        var s = (int) Math.Sqrt(x + y);
+        return s * s == x + y;
+    }
     Dictionary<int, int> count = new Dictionary<int, int>();
     Dictionary<int, HashSet<int>> cand = new Dictionary<int, HashSet<int>>();
     int res = 0;
-    public int NumSquarefulPerms(int[] A) {
+    public int NumSquarefulPerms1(int[] A) {
         foreach (int a in A) count[a] = count.GetValueOrDefault(a,0) + 1;
         foreach (var i in count) {
             foreach (var j in count) {
