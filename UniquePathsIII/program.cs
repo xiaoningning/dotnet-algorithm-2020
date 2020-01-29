9785c7152f6f20c32aeff1be84220806e7cf9bcc
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-01-29 12:37:09 -0800

    Unique Paths III update

--- UniquePathsIII/program.cs
+++ UniquePathsIII/program.cs
@@ -2,12 +2,12 @@ public class Solution {
     Dictionary<int, Dictionary<int, int>> cache = new Dictionary<int, Dictionary<int, int>>();
     int m;
     int n;
-    public int UniquePathsIII1(int[][] grid) {
+    public int UniquePathsIII(int[][] grid) {
         m = grid.Length; n = grid[0].Length;
         int state = 0;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) { 
-                if (grid[i][j] == 0 || grid[i][j] == 2) state |= 1 << (i * m + j);
+                if (grid[i][j] == 0 || grid[i][j] == 2) state |= 1 << (i * n + j);
                 else if (grid[i][j] == 1) {
                     sx = i; sy = j;    
                 }
@@ -16,8 +16,8 @@ public class Solution {
         return dfs(grid, sx, sy, state);
     }
     int dfs(int[][] grid, int x, int y, int state) {
-        if (cache.ContainsKey(x * m + y) && cache[x * m + y].ContainsKey(state))
-            return cache[x * m + y][state];
+        if (cache.ContainsKey(x * n + y) && cache[x * n + y].ContainsKey(state))
+            return cache[x * n + y][state];
         if (grid[x][y] == 2) return state == 0 ? 1 : 0; 
         var dirs = new int[,]{{-1,0}, {0,-1}, {1,0}, {0,1}};
         int cnt = 0;
@@ -25,17 +25,17 @@ public class Solution {
             int tx = x + dirs[i, 0];
             int ty = y + dirs[i, 1];
             if (tx < 0 || tx >= m || ty < 0 || ty >= n || grid[tx][ty] == -1) continue;
-            if ((state & (1 * tx + ty)) == 0) continue;
-            cnt += dfs(grid, tx, ty, state ^ (1 * tx + ty));            
+            if ((state & (1 << (tx *n + ty))) == 0) continue;
+            cnt += dfs(grid, tx, ty, state ^ (1 << (tx * n + ty)));            
         }
-        if (!cache.ContainsKey(x * m + y)) cache.Add(x * m + y, new Dictionary<int,int>());
-        if (!cache[x * m + y].ContainsKey(state)) cache[x * m + y].Add(state, 0);
-        cache[x * m + y][state] = cnt;
-        return cache[x * m + y][state];
+        if (!cache.ContainsKey(x * n + y)) cache.Add(x * n + y, new Dictionary<int,int>());
+        if (!cache[x * n + y].ContainsKey(state)) cache[x * n + y].Add(state, 0);
+        cache[x * n + y][state] = cnt;
+        return cache[x * n + y][state];
     }
     
     int res = 0, empty = 1, sx, sy, ex, ey;
-    public int UniquePathsIII(int[][] grid) {
+    public int UniquePathsIII1(int[][] grid) {
         int m = grid.Length, n = grid[0].Length;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
