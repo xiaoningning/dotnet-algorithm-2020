57aa81bccb539a710f533824c8bbf1e49ffc6aa5
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-02-07 00:04:52 -0800

    Sentence Similarity II OJ

--- sentenceSimilarityII/program.cs
+++ sentenceSimilarityII/program.cs
@@ -2,6 +2,15 @@ public class Solution {
     Dictionary<string, string> roots = new Dictionary<string, string>();
     public bool AreSentencesSimilarTwo(string[] words1, string[] words2, IList<IList<string>> pairs) {
         if (words1.Length != words2.Length) return false;
+        foreach (var w in words1)
+            if (!roots.ContainsKey(w)) roots.Add(w, w);
+        foreach (var w in words2)
+            if (!roots.ContainsKey(w)) roots.Add(w, w);
+        foreach (var p in pairs) {
+            if (!roots.ContainsKey(p[0])) roots.Add(p[0], p[0]);
+            if (!roots.ContainsKey(p[1])) roots.Add(p[1], p[1]);
+        }
+            
         foreach (var p in pairs) {
             string r1 = FindRoot(roots, p[0]);
             string r2 = FindRoot(roots, p[1]);
@@ -16,12 +25,7 @@ public class Solution {
         return true;
     }
     string FindRoot(Dictionary<string, string> roots, string i){
-        if (!roots.ContainsKey(i)) roots.Add(i, i);
-        while (i != roots[i]) {
-            // path compression
-            // roots[i] = roots[roots[i]];
-            i = roots[i];
-        }
-        return i;
+        // path compression
+        return i == roots[i] ? i : FindRoot(roots, roots[i]);
     }
 }
