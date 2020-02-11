16acfc3e53ad379ff11f6951557898c329cabbfb
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-02-10 17:01:55 -0800

    Copy List with Random Pointer Update space o(1)

--- copyListWithRandomPointer/Program.cs
+++ copyListWithRandomPointer/Program.cs
@@ -13,7 +13,37 @@ public class Node {
 }
 */
 public class Solution {
-    public Node CopyRandomList(Node head) {
+    public Node CopyRandomList(Node head) { 
+        if (head == null) return head;
+        var cur = head;
+        // copy all nodes
+        while (cur != null) {
+            var t = new Node(cur.val, null, null);
+            t.next = cur.next;
+            cur.next = t;
+            cur = t.next;
+        }
+        cur = head;
+        // assign random
+        while (cur != null) {
+            // the next is the copy of cur/cur.random
+            if (cur.random != null) cur.next.random = cur.random.next;
+            cur = cur.next.next;
+        }
+        // remove old node
+        cur = head;
+        var res = cur.next;
+        while (cur != null) {
+            var t = cur.next;
+            cur.next = t.next;
+            if (t.next != null) t.next = t.next.next;
+            cur = cur.next;
+        }
+        // time: O(n) space: O(1)
+        return res;
+    }
+    
+    public Node CopyRandomList1(Node head) {
         var m = new Dictionary<Node, Node>();
         return Helper(head, m);
     }
