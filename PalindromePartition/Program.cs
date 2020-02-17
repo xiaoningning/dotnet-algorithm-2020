cfb4a6ea4d7772aa13700cfe600d5dd80d0c5827
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-02-16 17:20:59 -0800

    Palindrome Partitioning Update

--- PalindromePartition/Program.cs
+++ PalindromePartition/Program.cs
@@ -1,86 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace PalindromePartition
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string s = "aab";
-            var res = Partition(s);
-            foreach(var r in res){
-                Console.WriteLine("result list: {0}", string.Join(',', r));
-            }
-        }
-        static IList<IList<string>> Partition(string s) {
-            List<IList<string>> res = new List<IList<string>>();
-            BackTrack(s, 0, new List<string>(), res);        
-            return res;
-        }
-        static void BackTrack(string s, int start, List<string> temp, List<IList<string>> res){
-            if (start == s.Length) res.Add(new List<string>(temp));
-            else {
-                for(int i = start; i < s.Length; i++){
-                    if(IsPalindrome(s, start, i)){
-                        temp.Add(s.Substring(start, i - start + 1));
-                        BackTrack(s, i + 1, temp, res);
-                        temp.RemoveAt(temp.Count -1);
-                    }                    
-                }            
-            }
-        }
-        static bool IsPalindrome(string s, int low, int high){
-            while(low < high)
-                if(s[low++] != s[high--]) return false;
-            return true;
-        } 
+public class Solution {
+    public IList<IList<string>> Partition1(string s) {
+        List<IList<string>> res = new List<IList<string>>();
+        dfs(s, 0, new List<string>(), res);    
+        // O(2^n)
+        return res;        
     }
-    
-    public class Solution {
-        public IList<IList<string>> Partition(string s) {
-            List<IList<string>> res = new List<IList<string>>();
-            BackTrack(s, 0, new List<string>(), res);        
-            return res;        
+    void dfs(string s, int start, List<string> temp, List<IList<string>> res){
+        if (start == s.Length) res.Add(new List<string>(temp));
+        else {
+            for(int i = start; i < s.Length; i++){
+                if(IsPalindrome(s, start, i)){
+                    temp.Add(s.Substring(start, i - start + 1));
+                    dfs(s, i + 1, temp, res);
+                    temp.RemoveAt(temp.Count -1);
+                }                    
+            }            
         }
-        void BackTrack(string s, int start, List<string> temp, List<IList<string>> res){
-            if (start == s.Length) res.Add(new List<string>(temp));
-            else {
-                for(int i = start; i < s.Length; i++){
-                    if(IsPalindrome(s, start, i)){
-                        temp.Add(s.Substring(start, i - start + 1));
-                        BackTrack(s, i + 1, temp, res);
-                        temp.RemoveAt(temp.Count -1);
-                    }                    
-                }            
-            }
-        }
-        public bool IsPalindrome(string s, int low, int high){
-            while(low < high)
-                if(s[low++] != s[high--]) return false;
-            return true;
-        } 
-        // output is not right
-        public IList<IList<string>> Partition2(string s){
-            int n = s.Length;
-            var res = new List<IList<string>>[n+1];
-            for(int i = 0; i < n+1; i++) 
-                res[i]= new List<IList<string>>(){new List<string>(){}};
-            bool[,] dp = new bool[n,n];
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j <= i; j++) {
-                    if (s[j] == s[i] && (i - j <= 2 || dp[j + 1,i - 1])) {
-                        dp[j,i] = true;
-                        string cur = s.Substring(j, i-j+1);
-                        for(int m = 0; m < res[j].Count; m++) {
-                            res[j][m].Add(cur);
-                            res[j+1].Add(new List<string>(res[j][m]));
-                            // Console.WriteLine(string.Join(",",res[j][m]));
-                        }
-                    }  
+    }
+    public bool IsPalindrome(string s, int low, int high){
+        while(low < high)
+            if(s[low++] != s[high--]) return false;
+        return true;
+    } 
+    // output is not right
+    public IList<IList<string>> Partition(string s){
+        int n = s.Length;
+        var dp = new List<IList<string>>[n+1]; // res of str[0:i]
+        for(int i = 0; i <= n; i++) dp[i]= new List<IList<string>>();
+        for (int len = 1; len <= n; len++) {
+            for (int i = 0; i < len; ++i) {
+                string right = s.Substring(i, len - i);
+                if (!IsPalindrome(right, 0, right.Length - 1)) continue;
+                if (i == 0) dp[len].Add(new List<string>(){right});
+                foreach (var p in dp[i]) {
+                    var t = p.ToList();// copy of new obj
+                    t.Add(right);
+                    dp[len].Add(t);
                 }
             }
-            return res[n];
         }
+        // O(2^n)
+        return dp[n];
     }
 }
