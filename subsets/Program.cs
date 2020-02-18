5c8478d7a293f600464a96ccc9f188691a0155e9
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-02-18 00:51:04 -0800

    subsets update

--- subsets/Program.cs
+++ subsets/Program.cs
@@ -1,32 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace subsets
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var obj = new Solution();
-            int[] nums = new int[]{1,2,3};
-            var res = obj.Subsets(nums);
-            Console.WriteLine("Subsets");
-            foreach(var r in res) Console.WriteLine(string.Join(",",r));
-        }
+public class Solution {
+    public IList<IList<int>> Subsets(int[] nums) {
+        var res = new List<IList<int>>();
+        // assume no duplication
+        dfs(nums, 0, new List<int>(), res);
+        return res;
     }
-    public class Solution {
-        public IList<IList<int>> Subsets(int[] nums) {
-            List<IList<int>> res = new List<IList<int>>();
-            Backtrack(nums, 0, new List<int>(), res);
-            return res;
-        }
-        void Backtrack(int[] nums, int pos, List<int> tmp, List<IList<int>> res){
-            res.Add(new List<int>(tmp));
-            for (int i = pos; i < nums.Length; ++i) {
-                tmp.Add(nums[i]);
-                Backtrack(nums, i+1, tmp, res);
-                tmp.RemoveAt(tmp.Count -1);  
-            }
+    void dfs(int[] nums, int start, List<int> tmp, List<IList<int>> res){
+        res.Add(new List<int>(tmp));
+        for (int i = start; i < nums.Length; ++i) {
+            tmp.Add(nums[i]);
+            dfs(nums, i+1, tmp, res);
+            tmp.RemoveAt(tmp.Count -1);  
         }
     }
 }
