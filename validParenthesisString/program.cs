0f27b8ec1eddf21a9f82a1a108526f4df9b4b094
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-02-19 03:44:53 -0800

    Valid Parenthesis String Update

--- validParenthesisString/program.cs
+++ validParenthesisString/program.cs
@@ -1,5 +1,17 @@
 public class Solution {
     public bool CheckValidString(string s) {
+        int open = 0, close = 0;
+        foreach (var c in s) {
+            if (c == '(') open++; 
+            else open--;
+            if (c == ')') close++; 
+            else close--;
+            if (close > 0) return false;
+            open = Math.Max(0,open);
+        }
+        return open == 0;
+    }
+    public bool CheckValidString1(string s) {
         return Helper(s, 0, 0);
     }
     bool Helper(string s, int i, int opened) {
