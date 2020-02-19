3a50266ae73690f7aa9878d4b922f03b3d6a4db2
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-02-19 02:32:23 -0800

    Generate Parentheses Update

--- generateParentheses/Program.cs
+++ generateParentheses/Program.cs
@@ -1,16 +1,30 @@
 public class Solution {
     public IList<string> GenerateParenthesis(int n) {
+        if (n == 0) return new List<string>(){""};
+        var st = new HashSet<string>();
+        var pre = GenerateParenthesis(n-1);
+        foreach (var str in pre) {
+            for (int i = 0; i < str.Length; i++) {
+                if (str[i] == '(') 
+                    st.Add(str.Substring(0, i+1) +"()" + str.Substring(i+1));
+            }
+            st.Add("()" + str);
+        }
+        return st.ToList();
+    }
+    public IList<string> GenerateParenthesis1(int n) {
         var res = new List<string>();
-        Helper(n, n, "", res);
+        dfs(n, n, "", res);
+        // O(2^n)
         return res;
     }
-    void Helper(int l, int r, string s, List<string> res) {
+    void dfs(int l, int r, string s, List<string> res) {
         // if l > r, )(
         if (l > r) return;
         if (l == 0 && r == 0) res.Add(s);
         else {
-            if (l>0) Helper(l-1, r, s+"(", res);
-            if (r>0) Helper(l, r-1, s+")", res);
+            if (l>0) dfs(l-1, r, s+"(", res);
+            if (r>0) dfs(l, r-1, s+")", res);
         }
     }
 }
