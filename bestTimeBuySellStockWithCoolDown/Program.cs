f46a262662c5538bf98e035b362e45a4a17ae41d
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-02-21 17:29:20 -0800

    Best Time to Buy and Sell Stock with Cooldown Update

--- bestTimeBuySellStockWithCoolDown/Program.cs
+++ bestTimeBuySellStockWithCoolDown/Program.cs
@@ -1,42 +1,28 @@
-﻿using System;
-
-namespace bestTimeBuySellStockWithCoolDown
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("price array: {0}", args[0]);
-            int[] princes = Array.ConvertAll(args[0].Split(','), s => int.Parse(s));
-            Console.WriteLine("Max profit: {0}", MaxProfit(princes));
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        if(prices == null || !prices.Any()) return 0;
+        int sell = 0, buy = -prices[0], rest = 0;
+        foreach(var p in prices){
+            int prev_sell = sell;
+            sell = buy + p;
+            buy = Math.Max(buy, rest - p); 
+            rest = Math.Max(rest, prev_sell); // rest a day
         }
-        
-        static int MaxProfit(int[] prices) {
-            if(prices == null || prices.Length <= 1) return 0;
+        return Math.Max(rest, sell);
+    }
+    public int MaxProfit1(int[] prices) {
+        if(prices == null || prices.Length <= 1) return 0;
 
-            // After you sell your stock, you cannot buy stock on next day
-            // one day rest in between buy and sell
-            // it must be buy rest sell
-            // can not be buy rest buy
-            // buy[i] <= rest[i] => rest[i] = max(sell[i-1], rest[i-1])
-            /*
-                buy[i]  = max(rest[i-1] - price, buy[i-1]) 
-                sell[i] = max(buy[i-1] + price, sell[i-1])
-                rest[i] = max(sell[i-1], buy[i-1], rest[i-1])
-
-                rest[i] = sell[i-1] =>
-                buy[i]  = max(sell[i-2] - price, buy[i-1]) 
-                sell[i] = max(buy[i-1] + price, sell[i-1])
-             */
-            int sell = 0, prev_sell = 0, buy = -prices[0], prev_buy;
-            for (int i = 0; i < prices.Length; i++) {
-                prev_buy = buy;
-                buy = Math.Max(buy, prev_sell - prices[i]);
-                prev_sell = sell;
-                sell = Math.Max(sell, prev_buy + prices[i]);
-                
-            }
-            return sell;
+        // one day rest in between buy and sell
+        // buy[i]  = max(sell[i-2] - price, buy[i-1]) 
+        // sell[i] = max(buy[i-1] + price, sell[i-1])
+        int sell = 0, prev_sell = 0, buy = -prices[0], prev_buy;
+        for (int i = 0; i < prices.Length; i++) {
+            prev_buy = buy;
+            buy = Math.Max(buy, prev_sell - prices[i]);
+            prev_sell = sell;
+            sell = Math.Max(sell, prev_buy + prices[i]);
         }
+        return sell;
     }
 }
