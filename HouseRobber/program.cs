6d3377803dc4a16004b59f28407af8876650f0bc
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-02-21 17:10:24 -0800

    house robber update

--- HouseRobber/program.cs
+++ HouseRobber/program.cs
@@ -1,6 +1,16 @@
 public class Solution {
     public int Rob(int[] nums) {
         if (!nums.Any()) return 0;
+        int n = nums.Length, rob = 0, notRob = 0;
+        for (int i = 0; i < n; ++i) {
+            int preRob = rob, preNotRob = notRob;
+            rob = preNotRob + nums[i];
+            notRob = Math.Max(preRob, preNotRob);
+        }
+        return Math.Max(rob, notRob);
+    }
+    public int Rob1(int[] nums) {
+        if (!nums.Any()) return 0;
         int n = nums.Length;
         var dp = new int[n];
         for (int i = 0; i < n; i++) {
