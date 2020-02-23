5fda79f859dcd45ae7e32286621b9895c495c602
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-02-23 15:44:43 -0800

    race car update

--- raceCar/program.cs
+++ raceCar/program.cs
@@ -1,6 +1,6 @@
 public class Solution {
     // BFS + memo
-    public int Racecar(int target) {
+    public int Racecar1(int target) {
         int res = 0;
         var q = new Queue<int[]>(); q.Enqueue(new int[]{0, 1});
         var visited = new HashSet<string>(){"0,1"};
@@ -25,19 +25,20 @@ public class Solution {
             }
             ++res;
         }
+        // O(2^D)
         return -1;
     }
     // DP
     // 0 -> 1 -> 3 -> 7 -> 15 -> 31
     // target = 2^cnt - 1
-    public int Racecar1(int target) {
+    public int Racecar(int target) {
         int[] dp = new int[target + 1];
         for (int i = 1; i <= target; i++) {
             dp[i] = Int32.MaxValue;
             int j = 1, cnt1 = 1;
             // before i, acclerate
             for (; j < i; j = (1 << ++cnt1) - 1) {
-                // reverse back and then reverse forward again 
+                // revers 1
                 for (int k = 0, cnt2 = 0; k < j; k = (1 << ++cnt2) - 1) {
                     dp[i] = Math.Min(dp[i], cnt1 + 1 + cnt2 + 1 + dp[i - (j - k)]);
                 }
@@ -45,6 +46,7 @@ public class Solution {
             // if  pass i, reverse, otherwise cnt1
             dp[i] = Math.Min(dp[i], cnt1 + (i == j ? 0 : 1 + dp[j - i]));
         }
+        // O(TlogT)
         return dp[target];
     }
 }
