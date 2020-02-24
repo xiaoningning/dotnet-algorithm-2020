7d8e8afae43e8f6a3b6c7e5a1627ec12949157db
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-02-23 16:12:53 -0800

    Number of Longest Increasing Subsequence update

--- FindNumberOfLIS/Program.cs
+++ FindNumberOfLIS/Program.cs
@@ -1,41 +1,29 @@
-﻿using System;
-
-namespace FindNumberOfLIS
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("array: {0}", args[0]);
-            int[] nums = Array.ConvertAll(args[0].Split(','), s => int.Parse(s));
-            Console.WriteLine("the number of LCIS: {0}", FindNumberOfLIS(nums));
+public class Solution {
+    public int FindNumberOfLIS(int[] nums) {
+        int res = 0, maxLength = 0;
+        int length = nums.Length;
+        int[] len = new int[length], cnt = new int[length];
+        for (int i = 0; i < length; i++){
+            len[i] = 1;
+            cnt[i] = 1;
         }
-        
-        static int FindNumberOfLIS(int[] nums) {
-            int res = 0, maxLength = 0;
-            int length = nums.Length;
-            int[] len = new int[length], cnt = new int[length];
-            for (int i = 0; i < length; i++){
-                len[i] = 1;
-                cnt[i] = 1;
+        for (int i = 0; i < nums.Length; i++){
+            for (int j = 0; j < i; j++){
+                if (nums[i] > nums[j]){
+                    // num[j] is part of seq with num[i]
+                    if (len[i] == len[j] + 1) cnt[i] += cnt[j]; 
+                    else if (len[i] < len[j] + 1) // new sequence
+                    {
+                       len[i] = len[j] + 1;
+                       cnt[i] = cnt[j];
+                    }
+                }                    
             }
-            for (int i = 0; i < nums.Length; i++){
-                for (int j = 0; j < i; j++){
-                    if (nums[i] > nums[j]){
-                        if (len[i] == len[j] + 1) cnt[i] += cnt[j];
-                        else if (len[i] < len[j] + 1)
-                        {
-                           len[i] = len[j] + 1;
-                           cnt[i] = cnt[j];
-                        }
-                    }                    
-                }
-                maxLength = Math.Max(len[i], maxLength);
-            }
-            for (int i = 0; i <  nums.Length; ++i) {
-                if (maxLength == len[i]) res += cnt[i];
-            }
-            return res;
+            maxLength = Math.Max(len[i], maxLength);
         }
+        for (int i = 0; i <  nums.Length; ++i) {
+            if (maxLength == len[i]) res += cnt[i];
+        }
+        return res;
     }
 }
