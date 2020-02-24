9adc0a88b75155f9d1f0d21e6036700b288c3dee
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-02-24 04:02:31 -0800

    Longest Increasing Subsequence binary search update

--- longestIncreasingSubseq/Program.cs
+++ longestIncreasingSubseq/Program.cs
@@ -1,4 +1,54 @@
 public class Solution {
+    public int LengthOfLIS(int[] nums) {
+        if (nums.Length == 0) return 0;
+        List<int> dp = new List<int>();
+        foreach (var n in nums) {
+            // If value is not found and value is 
+            // less than one or more elements in array, 
+            // the negative number returned is the bitwise complement of 
+            // the index of the first element that is larger than value. 
+            int idx = dp.BinarySearch(n);
+            idx = idx < 0 ? ~idx : idx;
+            if (idx >= dp.Count) dp.Add(n);
+            else dp[idx] = n;
+        }
+        // O(nlogn)
+        return dp.Count();
+    }
+    
+    public int LengthOfLIS3(int[] nums) {
+        if (nums.Length == 0) return 0;
+        List<int> dp = new List<int>();
+        for (int i = 0; i < nums.Length; i++){
+            int left = 0, right = dp.Count();
+            while (left < right) {
+                int mid = left + (right - left) /2;
+                // binary search to find 
+                // 1st one >= nums[i]
+                if (dp[mid] < nums[i]) left = mid +1;
+                else right = mid;
+            }
+            if (right >= dp.Count) dp.Add(nums[i]);
+            else dp[right] = nums[i];
+        }
+        // O(nlogn)
+        return dp.Count();
+    }
+    public int LengthOfLIS2(int[] nums) { 
+        int res = 0;
+        int[] dp = new int[nums.Length];
+        for (int i = 0; i < nums.Length; ++i) dp[i] = 1;
+        for (int i = 0; i < nums.Length; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.Max(dp[i], dp[j] + 1);
+                }
+            }
+            res = Math.Max(res, dp[i]);
+        }
+        // O(n*n)
+        return res;
+    }
     public int LengthOfLIS1(int[] nums) {
         if (nums.Length == 0) return 0;
         List<int> ends = new List<int>(){nums[0]};
@@ -19,39 +69,5 @@ public class Solution {
         }
         // O(nlogn)
         return ends.Count();
-    }
-    
-    public int LengthOfLIS2(int[] nums) {
-        if (nums.Length == 0) return 0;
-        List<int> dp = new List<int>();
-        for (int i = 0; i < nums.Length; i++){
-            int left = 0, right = dp.Count();
-            while (left < right) {
-                int mid = left + (right - left) /2;
-                // binary search to find 
-                // 1st one >= nums[i]
-                if (dp[mid] < nums[i]) left = mid +1;
-                else right = mid;
-            }
-            if (right >= dp.Count()) dp.Add(nums[i]);
-            else dp[right] = nums[i];
-        }
-        // O(nlogn)
-        return dp.Count();
-    }
-    public int LengthOfLIS(int[] nums) { 
-        int res = 0;
-        int[] dp = new int[nums.Length];
-        for (int i = 0; i < nums.Length; ++i) dp[i] = 1;
-        for (int i = 0; i < nums.Length; ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (nums[i] > nums[j]) {
-                    dp[i] = Math.Max(dp[i], dp[j] + 1);
-                }
-            }
-            res = Math.Max(res, dp[i]);
-        }
-        // O(n*n)
-        return res;
     }
 }
