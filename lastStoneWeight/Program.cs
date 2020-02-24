f2995f0cd00116887478c57a1f75d403390fca1e
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-02-23 16:25:44 -0800

    last stone update

--- lastStoneWeight/Program.cs
+++ lastStoneWeight/Program.cs
@@ -1,5 +1,17 @@
 public class Solution {
     public int LastStoneWeight(int[] stones) {
+        var q = new List<int>(stones);
+        q = q.OrderBy(s => s).ToList();
+        while (q.Count > 1) {
+            int x = q.Last(); q.RemoveAt(q.Count - 1);
+            int y = q.Last(); q.RemoveAt(q.Count - 1);
+            if (x == y) continue;
+            q.Add(Math.Abs(x - y));
+            q = q.OrderBy(s => s).ToList();
+        }
+        return q.Any() ? q.Last() : 0;
+    }
+    public int LastStoneWeight1(int[] stones) {
         // 1 <= stones[i] <= 1000
         var m = new int [1001];
         foreach (var s in stones) m[s]++;
