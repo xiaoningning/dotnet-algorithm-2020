d25e5abae7f14251f56018d156ee8dd931294096
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-02-23 16:46:32 -0800

    Filling Bookcase Shelves Update

--- FillingBookcaseShelves/program.cs
+++ FillingBookcaseShelves/program.cs
@@ -7,6 +7,8 @@ public class Solution {
         for (int i = 0; i < n; i++) {
             int h = 0, w = 0;
             for (int j = i; j < n; j++) {
+                // the order of the books we place is 
+                // the same order as the given seq of books
                 if ((w += books[j][0]) > shelf_width) break;
                 h = Math.Max(h, books[j][1]);
                 dp[j] = Math.Min(dp[j], (i == 0 ? 0 : dp[i-1]) + h);
