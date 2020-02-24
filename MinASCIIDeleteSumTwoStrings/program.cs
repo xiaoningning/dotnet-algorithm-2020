c7ad0935422955e01d18c470d6ecae8eba7e5882
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-02-23 17:47:03 -0800

    Minimum ASCII Delete Sum for Two Strings update

--- MinASCIIDeleteSumTwoStrings/program.cs
+++ MinASCIIDeleteSumTwoStrings/program.cs
@@ -3,9 +3,9 @@ public class Solution {
         int l1 = s1.Length, l2 = s2.Length;
         var dp = new int[l1+1, l2+1];
         // similar to edit distance
+        for (int i = 1; i <= l1; ++i) dp[i,0] = dp[i-1,0] + s1[i-1];
         for (int j = 1; j <= l2; j++) dp[0,j] = dp[0,j-1] + s2[j-1];
         for (int i = 1; i <= l1; ++i) {
-            dp[i,0] = dp[i-1,0] + s1[i-1];
             for (int j = 1; j <= l2; ++j) {
                 dp[i,j] = (s1[i - 1] == s2[j - 1]) ? 
                     dp[i-1,j-1]
