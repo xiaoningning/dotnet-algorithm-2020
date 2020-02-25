863b4e65a3a01fda183d772ef6e3028306e1d9bf
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-02-25 12:18:59 -0800

    Burst Balloons OJ update

--- burstBalloons/program.cs
+++ burstBalloons/program.cs
@@ -1,5 +1,23 @@
 public class Solution {
+    int[,] m;
     public int MaxCoins(int[] nums) {
+        int n = nums.Length;
+        var l = nums.ToList();
+        // 1 to boarder for calculation
+        l.Insert(0, 1);
+        l.Add(1);
+        m = new int[n+2, n+2];
+        return helper(l, 1, n);
+    }
+    int helper(List<int> nums, int i, int j) {
+        if (i > j) return 0;
+        if (m[i,j] != 0) return m[i,j];
+        int res = 0;
+        for (int k = i; k <= j; k++)
+            res = Math.Max(res, nums[i-1]*nums[k]*nums[j+1] + helper(nums, i, k-1) + helper(nums, k+1, j));       
+        return m[i,j] = res;
+    }
+    public int MaxCoins1(int[] nums) {
         int n = nums.Length;
         var l = nums.ToList();
         // 1 to boarder for calculation
@@ -12,7 +30,7 @@ public class Solution {
                 int j = i + len - 1;
                 for (int k = i; k <= j; ++k) {
                     // i,j is len, if len > 1, k neighbors are bursted already
-                    // => l[i - 1] * nums[k] * l[j + 1]
+                    // => nums[i - 1] * nums[k] * nums[j + 1]
                     dp[i,j] = Math.Max(dp[i,j], l[i - 1] * l[k] * l[j + 1] + dp[i,k - 1] + dp[k + 1,j]);
                 }
             }
