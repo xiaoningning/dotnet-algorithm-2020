bf13bac41ee1bb426f963f61878e86f1a0cdeb21
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-02-27 12:43:37 -0800

    Reverse Substrings Between Each Pair of Parentheses update

--- reverseSubstrBetweenPairParentheses/program.cs
+++ reverseSubstrBetweenPairParentheses/program.cs
@@ -1,5 +1,5 @@
 public class Solution {
-    public string ReverseParentheses(string s) {
+    public string ReverseParentheses1(string s) {
         int n = s.Length;
         int[] pair = new int[n];
         var st = new Stack<int>();
@@ -20,6 +20,28 @@ public class Solution {
                 res += s[i];
             }
         }
+        // O(n)
         return res;
+    }
+    public string ReverseParentheses(string s) {
+        var st = new Stack<string>();
+        st.Push("");
+        foreach (var c in s) {
+            if (c == '(') st.Push("");
+            else if (c != ')') {
+                var t = st.Pop();
+                t += c;
+                st.Push(t);
+            }
+            else {
+                var t1 = st.Pop();
+                t1 = new string(t1.ToCharArray().Reverse().ToArray());
+                var t2 = st.Pop();
+                t2 += t1;
+                st.Push(t2);
+            }            
+        }
+        // O(n^2)
+        return st.Peek();
     }
 }
