07221a657e571e9ea9ee07c41369a49c52161e81
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-03-01 15:20:23 -0800

    Factor Combinations update

--- FactorCombinations/program.cs
+++ FactorCombinations/program.cs
@@ -1,6 +1,25 @@
 public class Solution {
     public IList<IList<int>> GetFactors(int n) {
         var res = new List<IList<int>>();
+        helper(n, 2, new List<int>(), res);
+        return res;
+    }
+    void helper(int n, int s, List<int> t, List<IList<int>> res){
+        if (n == 1) { 
+            if (t.Count > 1) res.Add(new List<int>(t));
+        }
+        else {
+            for (int i = s; i <= n; i++) {
+                if (n % i == 0) {
+                    t.Add(i);
+                    helper(n / i, i, t, res);
+                    t.RemoveAt(t.Count - 1);
+                }
+            }
+        }
+    }
+    public IList<IList<int>> GetFactors1(int n) {
+        var res = new List<IList<int>>();
         for (int i = 2; i * i <= n; i++) {
             if (n % i == 0) {
                 var t = GetFactors(n / i);
