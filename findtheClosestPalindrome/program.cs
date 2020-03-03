2bbdbe4a588e5a3aef964c7dd0039574af895715
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-03-03 05:08:05 -0800

    Find the Closest Palindrome update

--- findtheClosestPalindrome/program.cs
+++ findtheClosestPalindrome/program.cs
@@ -9,7 +9,7 @@ public class Solution {
         s.Add((long)Math.Pow(10, len - 1) - 1);
         // left half since closest integer
         long prefix = Int64.Parse(n.Substring(0, (len + 1) / 2));
-        // i = 0 then it can be itself.
+        // i = 0 then it might be itself.
         for (long i = -1; i <= 1; ++i) {
             string pre = (prefix + i).ToString();
             // reverse left half for palindrome
@@ -18,7 +18,7 @@ public class Solution {
             string str = pre + new string(t);
             s.Add(Int64.Parse(str));
         }
-        s.Remove(num);
+        s.Remove(num); // try to remove itself
         foreach (var a in s) {
             long diff = Math.Abs(a - num);
             if (diff < minDiff) {
