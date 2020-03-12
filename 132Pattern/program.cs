e6707fe54a4ee72c5fe77849c2f727e2b53d3484
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-03-11 22:51:49 -0700

    132 Pattern OJ

---
+++ 132Pattern/program.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public bool Find132pattern(int[] nums) {
+        int third = Int32.MinValue;
+        var st = new Stack<int>(); // track 3rd
+        for (int i = nums.Length - 1; i >= 0; i--) {
+            if (nums[i] < third) return true; // find 1st
+            while (st.Any() && st.Peek() < nums[i] ) third = st.Pop();
+            st.Push(nums[i]);
+        }
+        return false;
+    }
+}
