ef778b7a3837301111920650ce531d553b26fa75
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-03-11 23:10:24 -0700

    Boundary of Binary Tree OJ

---
+++ BoundaryBinaryTree/program.cs
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public IList<int> BoundaryOfBinaryTree(TreeNode root) {
+        var res = new List<int>();
+        if (root == null) return res;
+        res.Add(root.val);
+        Helper(root.left, true, false, res);
+        Helper(root.right, false, true, res);
+        return res;
+    }
+    void Helper(TreeNode node, bool leftb, bool rightb, List<int> res) {
+        if (node == null) return;
+        // leave
+        if (node.left == null && node.right == null)  {
+            res.Add(node.val);
+            return;
+        }
+        if (leftb) res.Add(node.val);
+        Helper(node.left, leftb && (node.left != null), rightb && (node.right == null), res);
+        Helper(node.right, leftb && (node.left == null), rightb && (node.right != null), res);
+        if (rightb) res.Add(node.val);
+    }
+}
