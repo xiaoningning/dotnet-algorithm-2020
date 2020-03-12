11fd12a86499259dbc8383bb329c796293c7ad60
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-03-11 22:53:42 -0700

    132 Pattern OJ

--- 132Pattern/program.cs
+++ 132Pattern/program.cs
@@ -4,6 +4,7 @@ public class Solution {
         var st = new Stack<int>(); // track 3rd
         for (int i = nums.Length - 1; i >= 0; i--) {
             if (nums[i] < third) return true; // find 1st
+            // find 2nd
             while (st.Any() && st.Peek() < nums[i] ) third = st.Pop();
             st.Push(nums[i]);
         }
