d4fead9e99ce79eb2ba1a65ccc0d2e5cce7b5847
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-03-13 17:10:52 -0700

    target num update

--- TargetSum/Program.cs
+++ TargetSum/Program.cs
@@ -1,69 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace TargetSum
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int[] nums = new int[]{1,1,1,1,1};
-            int S = 3;
-            var obj = new Solution();
-            Console.WriteLine("Find the ways of target sum: {0}", string.Join(",", nums));
-            Console.WriteLine("The target sum: {0}", S);
-            Console.WriteLine("the ways of target sum: {0}", obj.FindTargetSumWays(nums, S));
-        }
+public class Solution {
+    /*
+    sum(P) - sum(N) = target
+    sum(P) + sum(N) + sum(P) - sum(N) = target + sum(P) + sum(N)
+    2 * sum(P) = target + sum(nums)
+    */
+    public int FindTargetSumWays3(int[] nums, int S) {
+        int sum = nums.Sum();
+        int s = Math.Abs(S); // S can be negative
+        if (sum < S || (sum + S) % 2 != 0) return 0;
+        int target = (sum + s) / 2;
+        var dp = new int[target + 1];
+        dp[0] = 1;
+        foreach (var n in nums)
+            for (int i = target; i >= n; i--)
+                dp[i] += dp[i-n];
+        // O(n*sum)
+        return dp.Last();
     }
     
-    public class Solution {
-        public int FindTargetSumWays(int[] nums, int S) {
-            // reduce space
-            var dp = new Dictionary<int, int>();
-            dp[0] = 1;
-            foreach (int num in nums) {
-                var t = new Dictionary<int, int>();
-                foreach (var a in dp) {
-                    int sum = a.Key, cnt = a.Value;
-                    t[sum + num] = t.ContainsKey(sum + num) ? t[sum + num] + cnt : cnt;
-                    t[sum - num] = t.ContainsKey(sum - num) ? t[sum - num] + cnt : cnt;
-                }
-                dp = t;
+    public int FindTargetSumWays(int[] nums, int S) { 
+        var dp = new Dictionary<int,int>();
+        dp.Add(0,1);
+        foreach (var n in nums) {
+            var t = new Dictionary<int,int>();
+            foreach (var kv in dp) {
+                int k = kv.Key, cnt = kv.Value;
+                t[k - n] = t.ContainsKey(k - n) ? t[k - n] + cnt : cnt;
+                t[k + n] = t.ContainsKey(k + n) ? t[k + n] + cnt : cnt;
             }
-            return dp.ContainsKey(S) ? dp[S] : 0;
+            dp = t;
         }
-        
-        public int FindTargetSumWays2(int[] nums, int S) {
-            int res = 0;
-            Helper(nums, S, 0, ref res);
-            return res;
-        }
-
-        void Helper(int[] nums, int S, int start, ref int res) {
-            if (start == nums.Length) {
-                if (S == 0) res++;
-                return;
-            }
-            Helper(nums, S + nums[start], start+1, ref res);
-            Helper(nums, S - nums[start], start+1, ref res);
-        }
-
-        public int FindTargetSumWays1(int[] nums, int S) {
-            int n = nums.Length;
-            // index of list is ith nums
-            List<Dictionary<int, int>> dp = new List<Dictionary<int, int>>();
-            // dict: key -> sum, value -> total cnt to get sum
-            dp.Add(new Dictionary<int, int>());
-            dp[0].Add(0,1);
-            for (int i = 0; i < n; ++i) {
-                dp.Add(new Dictionary<int, int>());
-                foreach (var a in dp[i]) {
-                    int sum = a.Key, cnt = a.Value;
-                    dp[i + 1][sum + nums[i]] = dp[i+1].ContainsKey(sum + nums[i])? dp[i + 1][sum + nums[i]] + cnt : cnt;
-                    dp[i + 1][sum - nums[i]] = dp[i+1].ContainsKey(sum - nums[i])? dp[i + 1][sum - nums[i]] + cnt : cnt;
-                }
-            }
-            return dp[n].ContainsKey(S) ? dp[n][S] : 0; 
-        }
+        return dp.ContainsKey(S) ? dp[S] : 0;
     }
+    
+    public int FindTargetSumWays1(int[] nums, int S) {
+        int n = nums.Length;
+        List<Dictionary<int, int>> dp = new List<Dictionary<int, int>>();
+        // dict: key -> sum, value -> total cnt to get sum
+        dp.Add(new Dictionary<int, int>());
+        dp[0].Add(0,1);
+        for (int i = 0; i < n; ++i) {
+            // i + 1
+            dp.Add(new Dictionary<int, int>());
+            foreach (var a in dp[i]) {
+                int sum = a.Key, cnt = a.Value;
+                dp[i + 1][sum + nums[i]] = dp[i+1].ContainsKey(sum + nums[i])? dp[i + 1][sum + nums[i]] + cnt : cnt;
+                dp[i + 1][sum - nums[i]] = dp[i+1].ContainsKey(sum - nums[i])? dp[i + 1][sum - nums[i]] + cnt : cnt;
+            }
+        }
+        // O(2^n)
+        return dp[n].ContainsKey(S) ? dp[n][S] : 0; 
+    }
+    
+    public int FindTargetSumWays2(int[] nums, int S) {
+        int res = 0;
+        Helper(nums, S, 0, ref res);
+        return res;
+    }
+
+    void Helper(int[] nums, int S, int start, ref int res) {
+        if (start == nums.Length) {
+            if (S == 0) res++;
+            return;
+        }
+        Helper(nums, S + nums[start], start+1, ref res);
+        Helper(nums, S - nums[start], start+1, ref res);
+    }   
 }
