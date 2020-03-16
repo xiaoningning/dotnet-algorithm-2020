c3430f5ad1c7b5e40b58e84e971820f1312c3626
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-03-16 15:20:34 -0700

    Combinations update

--- combinations/program.cs
+++ combinations/program.cs
@@ -1,5 +1,14 @@
 public class Solution {
     public IList<IList<int>> Combine(int n, int k) {
+        if (k > n || k < 0) return new List<IList<int>>();
+        if (k == 0) return new List<IList<int>>(){new List<int>()};
+        // C(n, k) = C(n-1, k-1) + C(n-1, k)
+        var res = Combine(n-1, k -1);
+        foreach (var a in res) a.Add(n);
+        foreach (var a in Combine(n-1, k)) res.Add(a);
+        return res;
+    }
+    public IList<IList<int>> Combine1(int n, int k) {
         var res = new List<IList<int>>();
         backtrack(res, new List<int>(), 1, n, k);
         return res;
