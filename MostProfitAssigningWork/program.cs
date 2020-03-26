d8809965c522e965c604824e186a11e7317f5cd2
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-03-26 14:51:27 -0700

    Most Profit Assigning Work OJ

---
+++ MostProfitAssigningWork/program.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int MaxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+        var dp = new int[100001]; // difficulty -> profit
+        int n = difficulty.Length, res = 0; 
+        for (int i = 0; i < n; i++) dp[difficulty[i]] = Math.Max(dp[difficulty[i]], profit[i]);
+        for (int i = 1; i < dp.Length; i++) dp[i] = Math.Max(dp[i], dp[i-1]);
+        foreach(var a in worker) res += dp[a];
+        return res; 
+    }
+}
