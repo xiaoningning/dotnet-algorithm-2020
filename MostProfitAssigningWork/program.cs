52a8c1f71da2cb53ada4bf98838339b7b3b296e7
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-03-26 14:52:21 -0700

    Most Profit Assigning Work update

--- MostProfitAssigningWork/program.cs
+++ MostProfitAssigningWork/program.cs
@@ -1,6 +1,6 @@
 public class Solution {
     public int MaxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
-        var dp = new int[100001]; // difficulty -> profit
+        var dp = new int[100001]; // difficulty -> profit and sorted
         int n = difficulty.Length, res = 0; 
         for (int i = 0; i < n; i++) dp[difficulty[i]] = Math.Max(dp[difficulty[i]], profit[i]);
         for (int i = 1; i < dp.Length; i++) dp[i] = Math.Max(dp[i], dp[i-1]);
