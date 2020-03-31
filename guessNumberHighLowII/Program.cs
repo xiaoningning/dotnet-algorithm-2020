d769e93dd763a011e29ba59b9aadb5f76e9c4b70
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-03-31 15:58:51 -0700

    Guess Number Higher or Lower II Update

--- guessNumberHighLowII/Program.cs
+++ guessNumberHighLowII/Program.cs
@@ -1,8 +1,24 @@
 public class Solution {
     public int GetMoneyAmount(int n) {
+        var dp = new int[n+1,n+1];
+        for (int i = 2; i <= n; i++) {
+            for (int j = i - 1; j > 0; j--) {
+                int global_min = Int32.MaxValue;
+                for (int k = j + 1; k < i; k++) {
+                    int local_max = k + Math.Max(dp[j,k-1], dp[k+1,i]);
+                    global_min = Math.Min(global_min, local_max);
+                }
+                // j + 1 == i edge case b/s no k
+                dp[j,i] = j + 1 == i ? j : global_min; 
+            }
+        }
+        return dp[1, n];
+    }
+    public int GetMoneyAmount1(int n) {
         int[,] memo = new int[n+1,n+1];
         return helper(1, n, memo);
     }
+    
     int helper(int start, int end, int[,] memo){
         if (start >= end) return 0;
         if(memo[start, end] != 0) return memo[start, end];
