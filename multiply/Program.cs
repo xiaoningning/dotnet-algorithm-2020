63a2c0543a92f2d5fde03b0120506f0eb21c23c3
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-04-14 08:22:53 -0700

    Multiply Strings update

--- multiply/Program.cs
+++ multiply/Program.cs
@@ -27,12 +27,12 @@ namespace multiply
                 }
             }
 
-            StringBuilder sb = new StringBuilder();  
+            var res = "";  
             foreach(int p in pos){
                 // ignore the first 0
-                if (!(p == 0 && sb.Length == 0)) sb.Append(p.ToString());
+                if (!(p == 0 && res.Length == 0)) res += p.ToString();
             }
-            return sb.Length == 0 ? "0" : sb.ToString();
+            return res.Length == 0 ? "0" : res;
         }
     }
 }
