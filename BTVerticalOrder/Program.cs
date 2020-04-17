94caa22f02ee1cfb1b3f7e4732a7e891be558083
Ning <1930967+xiaoningning@users.noreply.github.com>
2020-04-17 00:50:13 -0700

    Binary Tree Vertical Order Traversal Update

--- BTVerticalOrder/Program.cs
+++ BTVerticalOrder/Program.cs
@@ -1,67 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace BTVerticalOrder
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var obj = new Solution();
-            TreeNode root = new TreeNode(3);
-            root.left = new TreeNode(9);
-            root.right = new TreeNode(8);
-            root.left.left = new TreeNode(4);
-            root.left.right = new TreeNode(0);
-            var res = obj.VerticalOrder(root);
-            foreach(var r in res){
-                Console.WriteLine("result list: {0}", string.Join(',', r));
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public IList<IList<int>> VerticalOrder(TreeNode root) { 
+        var res = new List<IList<int>>();
+        if (root == null) return res;
+        var m = new SortedDictionary<int, List<int>>();
+        var q = new Queue<Tuple<int, TreeNode>>();
+        q.Enqueue(new Tuple<int, TreeNode>(0, root));
+        while (q.Any()) {
+            var t = q.Dequeue();
+            if (!m.ContainsKey(t.Item1)) m.Add(t.Item1, new List<int>());
+            m[t.Item1].Add(t.Item2.val);
+            if (t.Item2.left != null) 
+                q.Enqueue(new Tuple<int, TreeNode>(t.Item1 - 1, t.Item2.left));
+            if (t.Item2.right != null) 
+                q.Enqueue(new Tuple<int, TreeNode>(t.Item1 + 1, t.Item2.right));
+        }
+        foreach (var kv in m) res.Add(kv.Value);
+        return res;
+    }
+    public IList<IList<int>> VerticalOrder1(TreeNode root) {
+        List<IList<int>> res = new List<IList<int>>();
+        if(root == null) return res;
+        
+        Dictionary<int,List<int>> map = new Dictionary<int,List<int>>();
+        Queue<TreeNode> qNode = new Queue<TreeNode>();
+        Queue<int> qCol = new Queue<int>();
+        int min = 0;
+        int max = 0;
+        
+        qNode.Enqueue(root);
+        qCol.Enqueue(0);
+        
+        while(qNode.Count != 0){
+            var t = qNode.Dequeue();
+            int col = qCol.Dequeue();
+            if (!map.ContainsKey(col)) {
+                map.Add(col, new List<int>());
+            }
+            map[col].Add(t.val);    
+            
+            if(t.left != null){
+                qNode.Enqueue(t.left);
+                qCol.Enqueue(col - 1);
+                min = Math.Min(min, col - 1);
+            }
+            if(t.right != null){
+                qNode.Enqueue(t.right);
+                qCol.Enqueue(col + 1);
+                max = Math.Max(max, col + 1);
             }
         }
-    }
-    public class TreeNode {
-        public int val;
-        public TreeNode left;
-        public TreeNode right;
-        public TreeNode(int x) { val = x; }
-    }
-    public class Solution {
-        public IList<IList<int>> VerticalOrder(TreeNode root) {
-            List<IList<int>> res = new List<IList<int>>();
-            if(root == null) return res;
-            
-            Dictionary<int,List<int>> map = new Dictionary<int,List<int>>();
-            Queue<TreeNode> qNode = new Queue<TreeNode>();
-            Queue<int> qCol = new Queue<int>();
-            int min = 0;
-            int max = 0;
-            
-            qNode.Enqueue(root);
-            qCol.Enqueue(0);
-            
-            while(qNode.Count != 0){
-                var t = qNode.Dequeue();
-                int col = qCol.Dequeue();
-                if (!map.ContainsKey(col)) {
-                    map.Add(col, new List<int>());
-                }
-                map[col].Add(t.val);    
-                
-                if(t.left != null){
-                    qNode.Enqueue(t.left);
-                    qCol.Enqueue(col - 1);
-                    min = Math.Min(min, col - 1);
-                }
-                if(t.right != null){
-                    qNode.Enqueue(t.right);
-                    qCol.Enqueue(col + 1);
-                    max = Math.Max(max, col + 1);
-                }
-            }
-            for (int i = min; i <= max; i++) {
-                if (map.ContainsKey(i)) res.Add(map[i]);
-            }
-            return res;
+        for (int i = min; i <= max; i++) {
+            if (map.ContainsKey(i)) res.Add(map[i]);
         }
+        return res;
     }
 }
