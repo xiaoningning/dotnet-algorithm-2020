49ba4a23ad6c5632a89f422384fee51b7d4dd42f
Ning <1930967+xiaoningning@users.noreply.github.com>
2021-07-15 15:26:06 -0700

    Maximum XOR of Two Numbers in an Array

---
+++ MaximumXORTwoNumbersInArray/program.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int FindMaximumXOR(int[] nums) {
+        int maxXor = 0, mask = 0;
+        for (int i = 31; i >= 0; i--) {
+            mask |= 1 << i;
+            var s = new HashSet<int>();
+            foreach (int n in nums) s.Add(n & mask);
+            int tmp = maxXor | (1 << i);
+            foreach (var prefix in s) {
+                if (s.Contains(tmp ^ prefix)) {
+                    maxXor = tmp;
+                    break;
+                }
+            }
+        }
+        return maxXor;
+    }
+}
