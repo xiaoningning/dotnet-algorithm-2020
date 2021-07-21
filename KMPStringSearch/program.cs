05ed20c0a611224bf74e31c60e79e049c1fbc2c0
Ning <1930967+xiaoningning@users.noreply.github.com>
2021-07-20 18:23:45 -0700

    KMP string search

---
+++ KMPStringSearch/program.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine(KMP("BBC_ABCDAB_ABCDABCDABDE", "ABCDABD"));
+	}
+	
+	static int[] GetNext(string p) {
+		int n = p.Length;
+		int[] next = new int[n];
+		for (int i = 0; i < n; i++) next[i] = -1;
+		int k = -1, j = 0;
+		while (j < n - 1) {
+			Console.WriteLine(p[j] + ":" + j + ":" + k);
+			if (k == -1 || p[k] == p[j]) {
+				k++;j++;
+				Console.WriteLine(p[j] + ":" + j + ":" +p[k]+":"+ k + "#");
+				next[j] = (p[k] != p[j]) ? k : next[k];
+			}
+			else k = next[k];
+		}
+		return next;
+	}
+	static int KMP(string s, string p) {
+		int m = s.Length, n = p.Length, i = 0, j = 0;
+		var next = GetNext(p);
+		while(i < m && j < n) {
+			if (j == -1 || s[i] == p[j]) {
+				i++;j++;
+			}
+			else j = next[j];
+		}
+		return j == n ? i - j : -1;
+	}
+}
