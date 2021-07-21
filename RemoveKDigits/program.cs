e3eefb45c0adf16ec744425eb1eec64cb0e766b8
Ning <1930967+xiaoningning@users.noreply.github.com>
2021-07-21 14:50:59 -0700

    Add Remove K Digits

---
+++ RemoveKDigits/program.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public string RemoveKdigits(string num, int k) {
+        var res = "";
+        foreach (var c in num) {
+            while (k > 0 && res.Length > 0 && res.Last() > c) {
+                res = res.Remove(res.Length - 1, 1); k--;
+            }
+            res += c;
+        }
+        while (res.Any() && k-- > 0) res = res.Remove(res.Length - 1, 1);
+        while (res.Any() && res.First() == '0') res = res.Remove(0,1);
+        return res.Length == 0 ? "0" : res;
+    }
+}
