4d77bd9c4ce31971ad157d92ec34699401fac0cf
Ning <1930967+xiaoningning@users.noreply.github.com>
2021-07-22 23:15:02 -0700

    update Max Consecutive Ones II

--- maxConsecutiveOns2/Program.cs
+++ maxConsecutiveOns2/Program.cs
@@ -1,5 +1,20 @@
 public class Solution {
     public int FindMaxConsecutiveOnes(int[] nums) {
+        int res = 0, right = 0, left = 0, cnt0 = 0, n = nums.Length;
+        while (right < n) {
+            if (nums[right] == 0) cnt0++;
+            while (cnt0 == 2) {
+                res = Math.Max(res, right - left);
+                if (nums[left++] == 0) cnt0--;
+            }
+            right++;
+        }
+        // case: only < 1 zero 
+        if (cnt0 < 2) res = Math.Max(res, right - left);
+        return res;
+    }
+    
+    public int FindMaxConsecutiveOnes1(int[] nums) {
         int res = 0, sum = 0, cur = 0;
         foreach (int n in nums) {
             sum += n;
