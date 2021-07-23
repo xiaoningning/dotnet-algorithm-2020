15b57fb1bd01e420ec8fb0e138451234e88aa202
Ning <1930967+xiaoningning@users.noreply.github.com>
2021-07-22 18:59:06 -0700

    update paint house

--- paintHouse/Program.cs
+++ paintHouse/Program.cs
@@ -1,33 +1,13 @@
-﻿using System;
-
-namespace paintHouse
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var obj = new Solution();            
-            Console.WriteLine("paint house: {0}", obj.MinCost(new int[,]{}));
-            int[,] costs = new int[,]{
-                {1,3,2},
-                {2,1,2}
-             };
-            Console.WriteLine("paint house: {0}", obj.MinCost(costs));
-        }
-    }
-    public class Solution {
-        public int MinCost(int[,] costs) {
-            // length is total # of elements
-            // GetLength is dimisional size
-            if(costs.Length == 0) return 0;
-            int[,] dp = costs;
-            for(int i = 1; i < costs.GetLength(0); i++){
-                // total three colors
-                dp[i,0] += Math.Min(dp[i-1,1], dp[i-1,2]);
-                dp[i,1] += Math.Min(dp[i-1,0], dp[i-1,2]);
-                dp[i,2] += Math.Min(dp[i-1,1], dp[i-1,0]);
+public class Solution {
+    public int MinCost(int[][] costs) {
+        if (costs.Length == 0 || costs[0].Length == 0 ) return 0;
+        var n = costs.Length;
+        var dp = costs;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < 3; j++) {
+                dp[i][j] += Math.Min(dp[i-1][(j + 1) % 3], dp[i-1][(j + 2) % 3]);
             }
-            return Math.Min(Math.Min(dp[costs.GetLength(0)-1, 2], dp[costs.GetLength(0)-1,1]), dp[costs.GetLength(0)-1,0]);
         }
+        return new int[]{dp[n-1][0], dp[n-1][1], dp[n-1][2]}.Min();
     }
 }
