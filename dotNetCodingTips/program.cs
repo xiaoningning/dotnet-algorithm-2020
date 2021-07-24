a661dbe85a65e6f91e4429fea947f5bb4aabfa12
Ning <1930967+xiaoningning@users.noreply.github.com>
2021-07-24 01:57:29 -0700

    update c# coding tips tuple as key for dict

--- dotNetCodingTips/program.cs
+++ dotNetCodingTips/program.cs
@@ -11,19 +11,34 @@ public class Program
 		var sarray = Enumerable.Repeat("a", 5);
 		Console.WriteLine("enumerable repeat:"+string.Join(',', sarray));
 		
-		// get tuple as return of func
 		var (l, s) = GetIt("aefbgc");
 		Console.WriteLine("string length:"+l);
+		var res = GetIt("aefbgc");
+		Console.WriteLine(res.vs);
 		s = new string(s.OrderBy(a => a).ToArray());
 		Console.WriteLine("orderby:" + s);
 		s = s.Remove(1,1);
 		Console.WriteLine("remove string at 1:"+s);
 		
 		// array as key of dictionary
-		var d = new Dictionary<(int,string), string>(){{(1,"1"), "abcd"}};
-		Console.WriteLine(d[(1,"1")]);
+		var d = new Dictionary<(int,string), string>(){{(1,"11"), "abcd"}, {(2,"22"),"xyz"}};
+		Console.WriteLine(d[(1,"11")]);
 		Console.WriteLine(d.ContainsKey((2,"3")));
-		Console.WriteLine(d.ContainsKey((1,"1")));
+		Console.WriteLine(d.ContainsKey((1,"11")));
+		foreach (var kv in d){
+			Console.WriteLine("tuple as key of dict: "+ kv.Key.Item2);
+		}
+		var limitsLookup = new Dictionary<int, (int Min, int Max)>()
+		{
+			[2] = (4, 10),
+			[4] = (10, 20),
+			[6] = (0, 23)
+		};
+
+		if (limitsLookup.TryGetValue(4, out (int Min, int Max) limits))
+		{
+			Console.WriteLine($"Found limits: min is {limits.Min}, max is {limits.Max}");
+		}
 		
 		var lst = new List<string>(){"a","b","v"};
 		// Get index of list
@@ -31,7 +46,8 @@ public class Program
 			Console.WriteLine(idx + ": " + v);
 		}
 	}
-	static (int, string) GetIt(string s) {
+	
+	static (int ln, string vs) GetIt(string s) {
 		return (s.Length, s);
 	}
 }
